e245ab8b32869d630f4c75638c7bb4626285bf62
unknown <devnull@localhost>
2004-10-07 09:36:00 +0000

    added line ending tests

---
+++ run/line_ending_dos.d
@@ -0,0 +1,42 @@
+// encoding: utf-8
+// line ending: dos
+int main(){
+	try{
+		assert(0);
+	}catch(Exception e){
+		checkLineNumber(e);
+		return 0;
+	}catch(Error e){
+		checkLineNumber(e);
+		return 0;
+	}
+
+	assert(-1);
+	return 1;
+}
+
+/* 
+ * WARNING: this code depends on the phobos implementation.
+ * char[]s returned by wrong assertions have to look like:
+ *	 "blah blah (linenumber) blah blah"
+ */
+void checkLineNumber(Object o){
+	char[] string=o.toString();
+	
+	int start;
+	for(start=0; start<string.length; start++){
+		if(string[start]=='('){
+			break;
+		}
+	}
+
+	int end;
+	for(end=string.length-1; end>start; end--){
+		if(string[end]==')'){
+			break;
+		}
+	}
+
+	assert(end-start==2);
+	assert(string[start+1]=='5');
+}
