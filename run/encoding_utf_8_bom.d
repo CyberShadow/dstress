928188e0a883f580014e27fcc4b36a22445bf4ad
unknown <devnull@localhost>
2004-10-07 09:35:10 +0000

    added enconding tests

---
+++ run/encoding_utf_8_bom.d
@@ -0,0 +1,11 @@
+﻿// encoding :utf-8-bom
+int main(){
+	assert('y'== 0x79);
+	assert(''== 0x80);
+	assert('ޙ'== 0x799);
+	assert('ࠀ'== 0x800);
+	assert('￺'== 0xFFFA);
+	assert('𐀀'== 0x10000);
+	assert('𐿽'== 0x10FFD);
+	return 0;
+}
