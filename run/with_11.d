1cd0c574694d2ace93f2850027c2fc1f56c69527
thomask <devnull@localhost>
2004-10-08 21:14:44 +0000

    added digitalmars.com digitalmars.D.bugs:2042

---
+++ run/with_11.d
@@ -0,0 +1,14 @@
+// @author@	Nick <Nick_member@pathlink.com>
+// @date@	2004-10-07
+
+auto class AutoClass{
+}
+
+int main(){
+	auto AutoClass ac = new AutoClass();
+
+	with(ac){
+	}
+
+	return 0;
+}
