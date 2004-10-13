30e79f739294de33f7c5e763529d517d9d00b604
thomask <devnull@localhost>
2004-10-13 07:18:55 +0000

    fixed align_06 test

--- run/align_06.d
+++ run/align_06.d
@@ -1,21 +1,18 @@
-align(1) struct SmallStruct{
+struct SmallStruct{
 	align(1) byte a;
 	align(1) byte b;
 }
 
-align(4) struct MidStruct{
-	align(4) byte c;
-	align(4) byte d;
-}
-
-align(64) struct BigStruct{
-	align(64) byte e;
-	align(64) byte f;
+struct BigStruct{
+	align(4) byte e;
+	align(4) byte f;
 }
 
 int main(){
-	assert(SmallStruct.sizeof < MidStruct.sizeof);
-	assert(MidStruct.sizeof < BigStruct.sizeof);
+	SmallStruct s;
+	BigStruct S;
+	assert(&s.b-&s.a==1);
+	assert(&S.f-&S.e==4);
 	
 	return 0;
 }
