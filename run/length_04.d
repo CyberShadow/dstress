7bbc9522d2e848acc0350e511faa1917bfba8f97
thomask <devnull@localhost>
2004-10-23 10:12:27 +0000

    added length_04 (<opsfwrktdm44buww@simon.mshome.net> / digitalmars.D.bugs:2126)

---
+++ run/length_04.d
@@ -0,0 +1,12 @@
+// @author@	Buchan <kbuchan@xtra.co.nz>
+// @date@	2004-10-18
+// @uri@	news://opsf15ngwe44buww@simon.mshome.net
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2126
+
+module dstress.run.length_04;
+
+int main(){
+	const int[] array = [1, 2, 3];
+	assert((array).length==3);
+	return 0;
+}
