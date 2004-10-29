08893947c452a2a5ae6a2c018c9436fe428f4391
thomask <devnull@localhost>
2004-10-29 05:47:48 +0000

    Ivan Senji <ivan.senji@public.srce.hr> / news://ckpj76r3m@digitaldaemon.com / nttp://digitalmars.com/digitalmars.D.bugs:2117

---
+++ nocompile/bug_20041016_A.d
@@ -0,0 +1,27 @@
+// @author@	Ivan Senji <ivan.senji@public.srce.hr>
+// @date@	2004-10-16
+// @uri@	news://ckpj76$2r3m$1@digitaldaemon.com
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2117
+
+module dstress.nocompile.bug_20041016_A;
+
+class Conversion(T,U){
+	typedef char Small;
+	class Big{
+		char[2] dummy;
+	}
+	static Small Test(U u);
+	static Big Test(...);
+	static T MakeT();
+	enum {
+		exists = (Test(MakeT())).sizeof == (Small).sizeof
+	}
+}
+
+void variadicDummy(...){
+}
+
+int main (){
+	variadicDummy(Conversion!(double,int).exists);
+	return 0;
+}
---
+++ nocompile/bug_20041016_B.d
@@ -0,0 +1,27 @@
+// @author@	Ivan Senji <ivan.senji@public.srce.hr>
+// @date@	2004-10-16
+// @uri@	news://ckpj76$2r3m$1@digitaldaemon.com
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2117
+
+module dstress.nocompile.bug_20041016_B;
+
+int getch(){
+	return 0;
+}
+
+void writefln(...){
+}
+
+class Cout{
+	Cout set(int x){
+		return this;
+	}
+	alias set opShl;
+}
+
+int main ( ){
+	Cout cout = new Cout;
+	cout << 5 << 4;
+	writefln,getch;
+	return 0;
+}
