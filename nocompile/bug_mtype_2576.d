bc40ed1ad992305a2806cc07aa2b2af5614bb7ed
thomask <devnull@localhost>
2004-10-29 05:46:43 +0000

    Buchan <kbuchan@xtra.co.nz> / news://opsfwrktdm44buww@simon.mshome.net / nttp://digitalmars.com digitalmars.D.bugs:2114

---
+++ nocompile/bug_mtype_2576.d
@@ -0,0 +1,13 @@
+// @author@	Buchan <kbuchan@xtra.co.nz>
+// @date@	2004-10-16
+// @uri@	news://opsfwrktdm44buww@simon.mshome.net
+// @url@	nttp://digitalmars.com digitalmars.D.bugs:2114
+
+module dstress.nocompile.bug_mtype_2576;
+
+const char[] B = ['A'];
+
+int main(){
+	cast(B)1;
+	return 0;
+}
