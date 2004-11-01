0c24cac6a68f3b3d2aba0c20bcc75c88d3dc914c
thomask <devnull@localhost>
2004-11-01 12:44:06 +0000

    shinichiro.h <s31552@mail.ecc.u-tokyo.ac.jp> news://20041101173804.4e16f816.s31552@mail.ecc.u-tokyo.ac.jp nttp://digitalmars.com/D.gnu:829

---
+++ addon/constructor_04_A.d
@@ -0,0 +1,17 @@
+// @author@	shinichiro.h <s31552@mail.ecc.u-tokyo.ac.jp>
+// @date@	2004-11-01
+// @uri@	news://20041101173804.4e16f816.s31552@mail.ecc.u-tokyo.ac.jp
+// @url@	nttp://digitalmars.com/D.gnu:829
+
+// see dstress.run.constructor_04
+module dstress.addon.constructor_04_A;
+
+class Base{
+	this(){
+	}
+}
+
+class Derived : Base{
+	private:
+	// constructor will be wrongly generated here, so it is private.
+}
---
+++ run/constructor_04.d
@@ -0,0 +1,16 @@
+// @author@	shinichiro.h <s31552@mail.ecc.u-tokyo.ac.jp>
+// @date@	2004-11-01
+// @uri@	news://20041101173804.4e16f816.s31552@mail.ecc.u-tokyo.ac.jp
+// @url@	nttp://digitalmars.com/D.gnu:829
+
+// see also dstress.addon.constructor_04_A
+
+// __DSTRESS_DFLAGS__ addon/constructor_04_A.d
+
+module dstress.run.constructor_04;
+import dstress.addon.constructor_04_A;
+
+int main(){
+	Derived c = new Derived();
+	return 0;
+}
