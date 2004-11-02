f2d9a686b01df640f5735597f59c7c99e9295f6f
thomask <devnull@localhost>
2004-11-02 16:34:33 +0000

    added basic creal tests

---
+++ run/creal_01.d
@@ -0,0 +1,18 @@
+module dstress.run.creal_01;
+
+int main(){
+	real x1 = 2.0;
+	real y1 = 3.0;
+	real x2 = 5.0;
+	real y2 = 7.0;
+	ireal i = 1.0i;
+
+	creal a = x1 + y1*i;
+	creal b = x2 + y2*i;
+	creal c = a + b;
+
+	creal d = (x1+x2) + (y1*i+y2*i);
+	assert(c == d);
+
+	return 0;
+}
