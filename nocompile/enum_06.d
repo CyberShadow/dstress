bfb160179811f4cb4503337b1b94763b42dd2ad2
thomask <devnull@localhost>
2004-11-03 19:11:36 +0000

    shinichiro.h <s31552@mail.ecc.u-tokyo.ac.jp> news://20041104013903.548413c0.s31552@mail.ecc.u-tokyo.ac.jp nttp://digitalmars.com/D.gnu:831

---
+++ nocompile/enum_06.d
@@ -0,0 +1,6 @@
+// Enums must have at least one member
+
+module dstress.nocompile.enum_06;
+
+enum{
+}
---
+++ run/enum_08.d
@@ -0,0 +1,14 @@
+// @author@	shinichiro.h <s31552@mail.ecc.u-tokyo.ac.jp>
+// @date@	2004-11-04
+// @uri@	news://20041104013903.548413c0.s31552@mail.ecc.u-tokyo.ac.jp
+// @url@	nttp://digitalmars.com/D.gnu:831
+
+module destress.run.enum_08;
+
+int main(){
+	enum MyEnum{ A=-1, AA, B };
+	MyEnum e = -MyEnum.B;
+	assert(MyEnum.B>0);
+	assert(e<0);
+	return 0;
+}
