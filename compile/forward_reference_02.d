ea56a73687aabab0fffc5ff628077fe879e79caf
thomask <devnull@localhost>
2004-11-03 09:39:38 +0000

    ndove (http://www.dsource.org/forums/profile.php?mode=viewprofile&u=142) http://www.dsource.org/forums/viewtopic.php?t=402

---
+++ compile/forward_reference_02.d
@@ -0,0 +1,23 @@
+// @author@	ndove (http://www.dsource.org/forums/profile.php?mode=viewprofile&u=142)
+// @date@	2004-10-12
+// @url@	http://www.dsource.org/forums/viewtopic.php?t=402
+
+module dstress.compile.forward_reference_02;
+
+struct Bounds{
+   int Bottom = 0;
+   int Left   = 0;
+   int Right  = 0;
+   int Top    = 0;
+}
+
+struct Position{
+   int X = 0;
+   int Y = 0;
+}
+
+class Something{
+   Bounds   bounds;
+   Position position;
+}
+
