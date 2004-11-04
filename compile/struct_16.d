a179ee2a753bb95ffcfbfa3c1c2dab8736f40b84
thomask <devnull@localhost>
2004-11-04 13:49:58 +0000

    Thomas Kuehne <thomas-dloop@kuehne.cn> news://cmdbq1$1a49$3@digitaldaemon.com nttp://digitalmars.com/digitalmars.D.bugs:2194

---
+++ compile/struct_16.d
@@ -0,0 +1,13 @@
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2004-11-04
+// @uri@	news://cmdbq1$1a49$3@digitaldaemon.com
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2194
+
+module dstress.compile.struct_16;
+
+struct MyStruct{
+	StructAlias* MyStruct(){
+		return null;
+	}
+}
+alias MyStruct StructAlias;
