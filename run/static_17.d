012d5033fe9e6a3534fb6170a5da304e98d0c899
thomask <devnull@localhost>
2004-11-09 11:04:36 +0000

    Stewart Gordon <smjg_1998@yahoo.com> news://cmq61m$5rn$1@digitaldaemon.com nttp://digitalmars.com/digitalmars.D.bugs:2218

---
+++ run/static_17.d
@@ -0,0 +1,23 @@
+// @author@ 	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2004-11-09
+// @uri@	news://cmq61m$5rn$1@digitaldaemon.com
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2218
+
+module dstress.run.static_17;
+
+class MyClass{
+	static{
+		int foo(){
+			return 1;
+		}
+	}
+
+	static int bar(){
+		return foo()+3;
+	}
+}
+
+int main(){
+	assert(MyClass.bar()==4);
+	return 0;
+}
---
+++ run/static_18.d
@@ -0,0 +1,25 @@
+// @author@ 	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2004-11-09
+// @uri@	news://cmq61m$5rn$1@digitaldaemon.com
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2218
+
+// __DSTRESS_DFLAGS__ -d
+
+module dstress.run.static_18;
+
+class MyClass{
+	static{
+		deprecated int foo(){
+			return 1;
+		}
+	}
+
+	static deprecated int bar(){
+		return foo()+3;
+	}
+}
+
+int main(){
+	assert(MyClass.bar()==4);
+	return 0;
+}
