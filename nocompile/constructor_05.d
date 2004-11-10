6a189501390083530b79d6616b8b68d3deba1c84
thomask <devnull@localhost>
2004-11-10 06:50:33 +0000

    Ilya Zaitseff <sark7@mail333.com> news://opsg7zgbjdaaezs2@ilya.tec.amursk.ru nttp://digitalmars.com/digitalmars.D.bugs:2238

---
+++ nocompile/constructor_05.d
@@ -0,0 +1,18 @@
+// @author@	Ilya Zaitseff <sark7@mail333.com>
+// @date@	2004-11-10
+// @uri@	news://opsg7zgbjdaaezs2@ilya.tec.amursk.ru
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2238
+
+module dstress.nocompile.constructor_05;
+
+class Class{
+	template Template(){
+		this() {
+		}
+	}
+}
+
+int main(){
+	Class c = new Class.Template!();
+	return 0;
+}
