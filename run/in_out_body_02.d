38262fc7a2eec9ee04572fb1f808929368aafb93
thomask <devnull@localhost>
2004-11-14 18:27:03 +0000

    Buchan <kbuchan@xtra.co.nz> news://opshfzxz0b44buww@simon.homenet nttp://digitalmars.com/digitalmars.D.bugs:2262

---
+++ run/in_out_body_02.d
@@ -0,0 +1,33 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Buchan <kbuchan@xtra.co.nz>
+// @date@	2004-11-14
+// @uri@	news://opshfzxz0b44buww@simon.homenet
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2262
+
+module dstress.run.in_out_body_02;
+
+struct MyStruct{
+	int i;
+}
+
+void outer(){
+	MyStruct inner()
+	in{
+		assert(1);
+	}out (result){
+		assert(result.i==1);
+	}body{
+		MyStruct s;
+		s.i = 1;
+		return s;
+	}
+	assert(inner.i==1);
+}
+
+int main(){
+	outer();
+	return 0;
+}
