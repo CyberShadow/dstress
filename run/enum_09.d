d32caaaee8149f2eaa5b9e591c2c36e0721aa506
thomask <devnull@localhost>
2004-11-14 17:56:51 +0000

    shinichiro.h <s31552@mail.ecc.u-tokyo.ac.jp> news://20041112233233.3584a28f.s31552@mail.ecc.u-tokyo.ac.jp nttp://digitalmars.com/D.gnu:844

---
+++ run/enum_09.d
@@ -0,0 +1,18 @@
+// $HeadURL$ $Date$ ($Author$)
+
+// @author@	shinichiro.h <s31552@mail.ecc.u-tokyo.ac.jp>
+// @date@	2004-11-12
+// @uri@	news://20041112233233.3584a28f.s31552@mail.ecc.u-tokyo.ac.jp
+// @url@	nttp://digitalmars.com/D.gnu:844
+
+module dstress.run.enum_09;
+
+enum Enum{
+	A = -1
+}
+
+int main() {
+	Enum e = Enum.A;
+	assert(e < 0);
+	return 0;
+}
