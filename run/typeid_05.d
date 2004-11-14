50a82edac4618a8714e20fdae6a811b134a45e1c
thomask <devnull@localhost>
2004-11-14 18:06:21 +0000

    Ilya Zaitseff <sark7@mail333.com> news://opshbm24dqaaezs2@ilya.tec.amursk.ru nttp://digitalmars.com/digitalmars.D.bugs:2254

---
+++ run/typeid_05.d
@@ -0,0 +1,14 @@
+// $HeadURL$ $Date$ ($Author$)
+
+// @author@	Ilya Zaitseff <sark7@mail333.com>
+// @date@	2004-11-12
+// @uri@	news://opshbm24dqaaezs2@ilya.tec.amursk.ru
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2254
+
+module dstress.run.typeid_05;
+
+int main(){
+	TypeInfo ti = typeid(cfloat[]);
+	assert(!(ti is null));
+	return 0;
+}
