a773641352806b7c6911ce2e2e754ce58822b37f
thomask <devnull@localhost>
2004-11-14 18:30:46 +0000

    Buchan <kbuchan@xtra.co.nz> news://opshfzxz0b44buww@simon.homenet nttp://digitalmars.com/digitalmars.D.bugs:2262 (extended to "class" and "int")

---
+++ run/in_out_body_04.d
@@ -0,0 +1,27 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Buchan <kbuchan@xtra.co.nz>
+// @date@	2004-11-14
+// @uri@	news://opshfzxz0b44buww@simon.homenet
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2262
+
+module dstress.run.in_out_body_04;
+
+void outer(){
+	int inner()
+	in{
+		assert(1);
+	}out (result){
+		assert(result==1);
+	}body{
+		return 1;
+	}
+	assert(inner==1);
+}
+
+int main(){
+	outer();
+	return 0;
+}
