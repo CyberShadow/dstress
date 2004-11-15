e32690a15631c5f2a6605ae629ec76b9150bd968
thomask <devnull@localhost>
2004-11-15 13:18:18 +0000

    1) anonymous unions Stewart Gordon <smjg_1998@yahoo.com> news://cn9vah$1shn$1@digitaldaemon.com nttp://digitalmars.com/digitalmars.D.bugs:2270

---
+++ run/union_11.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2004-11-15
+// @uri@	news://cn9vah$1shn$1@digitaldaemon.com
+// @url@	nttp://digitalmars.com/digitalmars.D.bugs:2270
+
+module dstress.run.union_11;
+
+union {
+	short s=1234;
+	byte b;
+}
+
+int main(){
+	assert(s==1234);
+	return 0;
+}
