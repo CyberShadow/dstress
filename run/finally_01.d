11bdbd760f567c8ec630850de30fd8400c92cdd2
thomask <devnull@localhost>
2004-11-17 09:51:22 +0000

    error handling within "finally" Sean Kelly <sean@f4.ca> news:cndnk4$17ld$1@digitaldaemon.com nntp://digitalmars.com/digitalmars.D.bugs/2283

---
+++ run/finally_01.d
@@ -0,0 +1,26 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Sean Kelly <sean@f4.ca>
+// @date	2004-11-16
+// @uri@	news:cndnk4$17ld$1@digitaldaemon.com
+// @url@	nntp://digitalmars.com/digitalmars.D.bugs/2283
+int main(){
+	int status=0;
+	try{
+		try{
+			status++;
+			assert(status==1);
+			throw new Exception("first");
+		}finally{
+			status++;
+			assert(status==2);
+			status++;
+			throw new Exception("second");
+		}
+	}catch(Exception e){
+		assert(status==3);
+	}
+	return 0;
+}
