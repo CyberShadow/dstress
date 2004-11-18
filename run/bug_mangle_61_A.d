18bc2ec94984fe73d8b4ca1c84b082d590b9476b
thomask <devnull@localhost>
2004-11-18 08:13:26 +0000

    internal mangle_61 error Regan Heath <regan@netwin.co.nz> news:opshmqmfrn5a2sq9@digitalmars.com nntp://digitalmars.com/digitalmars.D.bugs/2306

---
+++ run/bug_mangle_61_A.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Regan Heath <regan@netwin.co.nz>
+// @date@	2004-11-18
+// @uri@	news:opshmqmfrn5a2sq9@digitalmars.com
+// @url@	nntp://digitalmars.com/digitalmars.D.bugs/2306
+
+module dstress.run.bug_mangle_61_A;
+
+template test(T:T[],alias S) {
+	char[] foo(T[] a, T[] b) {
+		return a ~ S ~ b;
+	}
+}
+
+alias test!(char[],comma).foo catComma;
+char[] comma = ", ";
+
+int main(){
+	char[] a = "Heath";
+	char[] b = "Regan";
+	
+	assert("Heath, Regan"==catComma(a,b));
+
+	return 0;
+}
