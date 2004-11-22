98b88c08bf9df84394c2a748c99be0d696416dbc
thomask <devnull@localhost>
2004-11-22 11:11:03 +0000

    parsing floating literals with multiple dots
    Ilya Zaitseff <sark7@mail333.com>
    news:opshte92rnaaezs2@localhost
    nntp://news.digitalmars.com/digitalmars.D.bugs/2316

---
+++ nocompile/float_litaeral_dec_15.d
@@ -0,0 +1,12 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ila Zaitseff <sark7@mail333.com>
+// @date@	2004-11-21
+// @uri@	news:opshte92rnaaezs2@localhost
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2316
+
+module dstress.nocompile.float_literal_dec_15;
+
+float a = 0.234.;
