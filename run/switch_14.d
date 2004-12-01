f7176022a6b1610e1876f7a51f74a877345e520d
thomask <devnull@localhost>
2004-12-01 07:33:15 +0000

    adapted feature "switch(dchar[])"

--- run/switch_14.d
+++ run/switch_14.d
@@ -7,24 +7,24 @@
 // @uri@	news:l3hr62-qov.ln1@kuehne.cn
 // @url@	nntp://digitalmars.com/digitalmars.D.bugs/2289
 
-// according to dmd-0.106's statement.html#switch
-// only integers, char[] and wchar[] are allowed
-
 // Note:
 // 	this crashs dmd-0.106 but results in the pseudo return code 1,
 // 	thus DStress can't detect this bug directly. Instead the compiler
 //	output is parsed for "Internal error"(dmd) and "gcc.gnu.org/bugs"(gdc/gcc)
 
-module dstress.nocompile.switch_14;
+module dstress.run.switch_14;
 
 int main(){
 	dchar[] array = "123";
 	switch(array){
-		case "123":{
+		case "12":{
+			assert(0);
+		}case "123":{
 			return 0;
+		}case "1234":{
+			assert(0);
 		}default:{
 			assert(0);
 		}
 	}
-		
 }
