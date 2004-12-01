dc48eaf18a2ef87585ce77bff9a67c668e24c228
thomask <devnull@localhost>
2004-12-01 14:44:06 +0000

    class_initialization
    k2 <k2_member@pathlink.com>
    2004-11-30
    news:cohgrc$n3v$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/2379

---
+++ run/class_initialization_01.d
@@ -0,0 +1,36 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	k2 <k2_member@pathlink.com>
+// @date@	2004-11-30
+// @uri@	news:cohgrc$n3v$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2379
+
+module dstress.run.class_initialization_01;
+
+class Parent{
+	int s;
+	this(){
+	}
+}
+
+
+class Child : Parent {
+	int a;
+	int b;
+	int c;
+	int d;
+}
+
+int main(){
+	Child a = new Child();
+
+	assert(a.s == 0);
+	assert(a.a == 0);
+	assert(a.b == 0);
+	assert(a.c == 0);
+	assert(a.d == 0);
+
+	return 0;
+}
