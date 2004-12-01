f97ee6230cb018405a4b0bc8cbf8b96623057b8b
thomask <devnull@localhost>
2004-12-01 07:34:52 +0000

    extended typeid tests

---
+++ run/typeid_83.d
@@ -0,0 +1,19 @@
+// @uri@	news:opsh702et0jccy7t@simon.homenet
+// @author@	Simon Buchan <currently@no.where>
+// @date@	2004-11-29
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2369
+
+module dstress.run.typeid_83;
+
+TypeInfo ti_a1 = typeid(int),
+          ti_a2 = typeid(int),
+          ti_a3 = typeid(uint);
+
+int main() {
+	assert(!(ti_a1 is null));
+	assert(!(ti_a2 is null));
+	assert(!(ti_a3 is null));
+	return 0;
+}
+
+
