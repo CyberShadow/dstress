c8200c0fffe0282b1950bacce6ffd14b8e2333f1
thomask <devnull@localhost>
2004-12-01 08:06:22 +0000

    Lionello Lunesu <lionello.lunesu@crystalinter.remove.com> 2004-11-30 news:cohvnb$1eil$1@digitaldaemon.com nntp://news.digitalmars.com/digitalmars.D.bugs/2383

---
+++ nocompile/array_initialization_04.d
@@ -0,0 +1,21 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Lionello Lunesu <lionello.lunesu@crystalinter.remove.com>
+// @date@	2004-11-30
+// @uri@	news:cohvnb$1eil$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2383
+
+module dstress.nocompile.array_initialization_04;
+
+void test(size_t size){
+}
+
+int main(){
+	for (int t=0; t<33; t++){
+		test((bit[t]).sizeof);
+	}
+	return 0;
+}
+
