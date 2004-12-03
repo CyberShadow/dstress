d506790b66769652f46ed23c7f23a43a77719703
thomask <devnull@localhost>
2004-12-03 13:09:37 +0000

    undefined symbol in uint switch crashes the compiler
    Geoff Hickey <Geoff_member@pathlink.com>
    2004-12-03
    news:coodc7$27qp$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/2431

---
+++ nocompile/bug_20041203_A.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Geoff Hickey <Geoff_member@pathlink.com>
+// @date@	2004-12-03
+// @uri@	news:coodc7$27qp$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2431
+
+module dstress.nocompile.bug_20041203_A;
+
+struct MyStruct{
+	uint i;
+}
+
+int main(){
+	MyStruct s;
+	s.i = 1;
+
+	switch( s.i ){
+		case undefined_identifier:
+			break;
+	}
+
+	return 0;
+}
+
+
+
+
+
