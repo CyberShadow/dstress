4d910461f54c3aa6599a7376ccdd383796c040cb
thomask <devnull@localhost>
2004-12-06 06:44:21 +0000

    class declaration order (cgywin 1.5.12-1)
    Russell Wilkins <rwilkins@grovestarsoftware.com>
    200-12-04
    news:cosoa0$2c50$1@digitaldaemon.com
    nntp://news.digitalmars.com/gnu.D/881

---
+++ run/class_08.d
@@ -0,0 +1,24 @@
+// $HeadURL
+// $Date$
+// $Author$
+
+// @author@	Russell Wilkins <rwilkins@grovestarsoftware.com>
+// @date@	200-12-04
+// @uri@	news:cosoa0$2c50$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/gnu.D/881
+
+module dstress.run.class_08;
+
+class C : B{
+}
+
+class B : A{
+}
+
+class A{
+}
+
+int main (){
+	C c = new C();
+	return 0;
+}
