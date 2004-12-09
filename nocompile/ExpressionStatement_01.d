823d79bcf82a1d9cf06bba760e68353b9d1daf2a
thomask <devnull@localhost>
2004-12-09 07:01:54 +0000

    added tests for ExpressionStatement without affect

---
+++ nocompile/ExpressionStatement_01.d
@@ -0,0 +1,11 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+//  Expressions that have no affect are illegal.
+module dstress.nocompile.ExpressionStatement_01;
+
+int main(){
+	0+1;
+	return 0;
+}
---
+++ nocompile/ExpressionStatement_02.d
@@ -0,0 +1,11 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+//  Expressions that have no affect are illegal.
+module dstress.nocompile.ExpressionStatement_02;
+
+int main(){
+	0;
+	return 0;
+}
