c828f452e49833a9910eaff76ce803af192c8be2
thomask <devnull@localhost>
2004-12-09 03:26:52 +0000

    bug_elfobj_1251
    Ben Hinkle <bhinkle4@juno.com>
    2004-12-09
    news:cp86l0$106o$2@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/2491

---
+++ run/bug_elfobj_1251_B.d
@@ -0,0 +1,25 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ben Hinkle <bhinkle4@juno.com>
+// @date@	2004-12-09
+// @uri@	news:cp86l0$106o$2@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2491
+
+module dstress.run.bug_elfobj_1251_B;
+
+bool foo() {
+	int x;
+	for (;;) {
+		if (x == 0)
+			return true;
+		x = 1;
+	}
+	return false;
+}
+
+int main(){
+	foo();
+	return 0;
+}
