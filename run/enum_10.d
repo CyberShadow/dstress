f0c4b9d0d205809273208a53edb364e156670225
thomask <devnull@localhost>
2004-12-12 23:08:32 +0000

    forward reference of enums
    John Reimer <brk_6502@yahoo.com>
    2004-12-11
    news:cpfvm8$krm$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/2534

---
+++ run/enum_10.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	John Reimer <brk_6502@yahoo.com>
+// @date@	2004-12-11
+// @uri@	news:cpfvm8$krm$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2534
+
+module dstress.run.enum_10;
+
+struct Struct{
+	Enum e;
+}
+
+enum Enum{
+	A,
+	B,
+	C
+}
+
+int main(){
+	Struct s;
+	assert(s.e == Enum.A);
+	assert(s.e == 0);
+	return 0;
+}
+
