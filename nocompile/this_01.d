5e115a153bd8ca5929b051e5ec08f37969b7edb2
thomask <devnull@localhost>
2004-12-23 20:49:06 +0000

    this in module level h3r3tic <foo@bar.baz> 2004-12-23 news:cqd25q$17mh$1@digitaldaemon.com nntp://digitalmars.com/digitalmars.D.bugs/2592

---
+++ nocompile/this_01.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	h3r3tic <foo@bar.baz>
+// @date@	2004-12-23
+// @uri@	news:cqd25q$17mh$1@digitaldaemon.com
+// @url@	nntp://digitalmars.com/digitalmars.D.bugs/2592
+
+module dstress.nocompile.this_01;
+
+void main(){
+     this.foobar;
+}
---
+++ nocompile/this_02.d
@@ -0,0 +1,12 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	h3r3tic <foo@bar.baz>
+// @date@	2004-12-23
+// @uri@	news:cqd25q$17mh$1@digitaldaemon.com
+// @url@	nntp://digitalmars.com/digitalmars.D.bugs/2592
+
+module dstress.nocompile.this_02;
+
+this.foobar;
