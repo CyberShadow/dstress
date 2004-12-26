fa5aa8e8d758f847394059adb374aa46e52bdf22
thomask <devnull@localhost>
2004-12-26 16:38:16 +0000

    e2ir.c 229
    h3r3tic <foo@bar.baz>
    2004-12-24
    news:cqi3v3$r2o$1@digitaldaemon.com
    nntp://digitalmars.com/digitalmars.D.bugs/2601

---
+++ run/bug_e2ir_299_B.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	h3r3tic <foo@bar.baz>
+// @date@	2004-12-24
+// @uri@	news:cqi3v3$r2o$1@digitaldaemon.com
+// @url@	nntp://digitalmars.com/digitalmars.D.bugs/2601
+
+module dstress.run.bug_e2ir_299_B;
+
+int main(){
+	void foo() {}
+	function void() { foo(); };
+	return 0;
+}
