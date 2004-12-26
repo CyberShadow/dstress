8a0013ffcbfb8e28a4d8bda869dd2b19f9a102db
thomask <devnull@localhost>
2004-12-26 21:33:40 +0000

    "!=" versus "!=="
    h3r3tic <foo@bar.baz>
    2004-12-26
    news:cqml2m$1ujj$1@digitaldaemon.com
    nntp://digitalmars.com/digitalmars.D.bugs/2604

---
+++ compile/bug_dsymbol_611_C.d
@@ -0,0 +1,30 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	h3r3tic <foo@bar.baz>
+// @date@	2004-12-26
+// @uri@	news:cqml2m$1ujj$1@digitaldaemon.com
+// @url@	nntp://digitalmars.com/digitalmars.D.bugs/2604
+
+module dstress.compile.bug_dsymbol_611_C;
+
+interface INode{
+	INode owner();
+}
+
+class BasicNode : INode{
+	INode findNode(){
+		foreach(INode c ; m_children){
+			if(c.owner !== this)
+				continue;
+			return null;
+		}
+	}
+	
+	INode owner(){
+		return this;
+	}
+
+	INode[] m_children;
+}
---
+++ nocompile/bug_dsymbol_611_B.d
@@ -0,0 +1,26 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	h3r3tic <foo@bar.baz>
+// @date@	2004-12-26
+// @uri@	news:cqml2m$1ujj$1@digitaldaemon.com
+// @url@	nntp://digitalmars.com/digitalmars.D.bugs/2604
+
+module dstress.nocompile.bug_dsymbol_611_B;
+
+interface INode{
+	INode owner();
+}
+
+class BasicNode : INode{
+	INode findNode(){
+		foreach(INode c ; m_children){
+			if(c.owner !== this)
+				continue;
+			return null;
+		}
+	}
+
+	INode[] m_children;
+}
