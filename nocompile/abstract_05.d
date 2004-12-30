9ed5e7b33ff33bc87576ad84ade575c8654b12a7
thomask <devnull@localhost>
2004-12-30 11:04:44 +0000

    abstract constructor
    Jarrett Billingsley <kb3ctd2@yahoo.com>
    2004-12-29
    news:cqvnro$26ul$1@digitaldaemon.com
    nntp://digitalmars.com/digitalmars.D.bugs/2624

---
+++ nocompile/abstract_05.d
@@ -0,0 +1,19 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Jarrett Billingsley <kb3ctd2@yahoo.com>
+// @date@	2004-12-29
+// @uri@	news:cqvnro$26ul$1@digitaldaemon.com
+// @url@	nntp://digitalmars.com/digitalmars.D.bugs/2624
+
+module dstress.nocompile.abstract_05;
+
+class Base{
+    abstract this();
+}
+
+int main(){
+	Base b=new Base();
+	return 0;
+}
