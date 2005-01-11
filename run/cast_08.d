bd68715c7ba31a9154cdf0ee9c58060bdfd54299
thomask <devnull@localhost>
2005-01-11 03:58:28 +0000

    downcasting classes not an lvalue
    Jarrett Billingsley <kb3ctd2@yahoo.com>
    2005-01-09
    news:crshla$2io3$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/2659

---
+++ run/cast_08.d
@@ -0,0 +1,23 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Jarrett Billingsley <kb3ctd2@yahoo.com>
+// @date@	2005-01-09
+// @uri@	news:crshla$2io3$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2659
+
+module dstress.run.cast_08;
+
+class Parent{
+}
+
+class Child{
+}
+
+int main(){
+	Child c = new Child();
+	Parent p = cast(Parent) c;
+	p = null;
+	return 0;
+}
---
+++ run/cast_10.d
@@ -0,0 +1,25 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Jarrett Billingsley <kb3ctd2@yahoo.com>
+// @date@	2005-01-09
+// @uri@	news:crshla$2io3$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2659
+
+module dstress.run.cast_10;
+
+class Parent{
+}
+
+class Child{
+}
+
+void test(inout Parent p){
+}
+
+int main(){
+	Child c = new Child();
+	test(c);
+	return 0;
+}
