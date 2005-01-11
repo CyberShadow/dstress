d94a2b3899a216a21f76075909c45da37985f163
thomask <devnull@localhost>
2005-01-11 16:31:24 +0000

    constant evaluation for enums
    Brian Hjøllund <Brian_member@pathlink.com>
    2005-01-11
    news:cs0tm5$1m35$1@digitaldaemon.com
    nttp://news.digitalmars.com/digitalmars.D.bugs/2670

---
+++ compile/const_13.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Brian Hjøllund <Brian_member@pathlink.com>
+// @date@	2005-01-11
+// @uri@	news:cs0tm5$1m35$1@digitaldaemon.com
+// @url@	nttp://news.digitalmars.com/digitalmars.D.bugs/2670
+
+module dstress.compile.const_13;
+
+const int A = -831;
+
+enum{
+	B = A
+}
