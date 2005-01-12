8a43babf780e51edaa7fc42023b9606801d06239
thomask <devnull@localhost>
2005-01-12 13:42:43 +0000

    struct recursion
    Stewart Gordon <smjg_1998@yahoo.com>
    2005-01-12
    news:cs2u76$18jn$1@digitaldaemon.com
    nttp://news.digitalmars.com/digitalmars.D.bugs/2674

---
+++ compile/struct_20.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2005-01-12
+// @uri@	news:cs2u76$18jn$1@digitaldaemon.com
+// @url@	nttp://news.digitalmars.com/digitalmars.D.bugs/2674
+
+module dstress.compile.struct_20;
+
+struct StructA {
+	StructB b;
+}
+
+struct StructB {
+	StructA a;
+}
