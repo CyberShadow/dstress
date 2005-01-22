3a5c9b351744efd313a32da60bea966847f85b11
thomask <devnull@localhost>
2005-01-22 17:14:08 +0000

    double alias and "need 'this'"
    Thomas Kuehne <thomas@kuehne.thisisspam.cn>
    2005-01-22
    news:csu1gv$see$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/2737

---
+++ run/alias_13.d
@@ -0,0 +1,40 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas@kuehne.thisisspam.cn>
+// @date@	2005-01-22
+// @uri@	news:csu1gv$see$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2737
+
+module dstress.run.alias_13;
+
+int status;
+
+class Check{
+	void sum(byte[] b){
+		status++;
+	}
+
+	void add(byte b){
+		assert(0);
+	}	
+
+	alias sum write;
+	alias add write;
+	
+	void test(){
+		byte[] buffer;
+		write(buffer);
+	}
+}
+
+
+int main(){
+	Check c = new Check();
+	status=0;
+	assert(status==0);
+	c.test();
+	assert(status==1);
+	return 0;
+}
