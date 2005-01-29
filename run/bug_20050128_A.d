2dbf36268f50fe815f5f507ec1352f1796e84061
thomask <devnull@localhost>
2005-01-29 09:28:45 +0000

    inner function in struct member functions
    Nick <Nick_member@pathlink.com>
    2005-01-28
    news:ctdtgj$30r$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/2784

---
+++ run/bug_20050128_A.d
@@ -0,0 +1,38 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @date@	Nick <Nick_member@pathlink.com>
+// @date@	2005-01-28
+// @uri@	news:ctdtgj$30r$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2784
+
+module dstress.run.bug_20050128_A;
+
+struct MyStruct{
+	int i;
+     
+	void display(){
+		assert(i==10);
+	}
+    
+	void someFunc(){
+		// We never call this function
+		void bug(MyStruct[] array){
+			array[0].i = i+1; // Comment out this line and the bug goes away
+		}
+    
+		assert(i==10);
+		display();
+		assert(i==10);
+	}
+}
+    
+int main(){
+	MyStruct m;
+	m.i = 10;
+	assert(m.i==10);
+	m.someFunc();
+	assert(m.i==10);
+	return 0;
+}   
