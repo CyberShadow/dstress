7b5afb48e29fbf045fdab3bf65829e5fe4b6f293
thomask <devnull@localhost>
2005-02-03 15:29:32 +0000

    extended bug_elfobj_1252
    Kevin Bealer <Kevin_member@pathlink.com>
    2005-02-03
    news:cts483$1klp$1@digitaldaemon.com

---
+++ run/bug_elfobj_1251_C.d
@@ -0,0 +1,26 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kevin Bealer <Kevin_member@pathlink.com>
+// @date@	2005-02-03
+// @uri@	news:cts483$1klp$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ -g
+
+module dstress.run.bug_elfobj_1251_C;
+
+bool foo(char[][] arg) {
+	int x;
+	for (;;) {
+		if (arg.length == 1)
+			return true;
+		x = 1;
+	}
+	return false;
+}
+
+int main(char[][] arg){
+	foo(arg);
+	return 0;
+}
