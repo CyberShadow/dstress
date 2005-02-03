d10ccb7b07e8ef0aaa3b33f9a58827a4e8f92b67
thomask <devnull@localhost>
2005-02-03 23:56:50 +0000

    new'ing a templated struct
    zwang <nehzgnaw@gmail.com>
    2005-02-03
    news:cttjcg$44f$1@digitaldaemon.com

---
+++ nocompile/bug_todt_194_A.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-02-03
+// @uri@	news:cttjcg$44f$1@digitaldaemon.com
+
+module dstress.nocompile.bug_todt_194_A;
+
+void main(){
+	assert((new MyStruct!()).i==4);
+}
+
+struct MyStruct(){
+	int i=func(0).sizeof;
+}
+
+int func(...);
+
