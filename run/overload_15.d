2498ed345f08162ae3bc0d7443533ef10119a66a
thomask <devnull@localhost>
2005-02-03 03:55:43 +0000

    function overloading / resolving
    Nick Sabalausky <z@a.a>
    2005-02-01
    news:ctpknf$21se$1@digitaldaemon.com
    nntp://news.digitmars.com/digitalmars.D.bugs

---
+++ run/overload_15.d
@@ -0,0 +1,41 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Nick Sabalausky <z@a.a>
+// @date@	2005-02-01
+// @uri@	news:ctpknf$21se$1@digitaldaemon.com
+// @url@	nntp://news.digitmars.com/digitalmars.D.bugs
+
+module dstress.run.overload_15;
+
+int status;
+
+void check(int x){
+	status++;
+}
+
+class MyClass{
+	void test(){
+		assert(status==0);
+		.check(0);
+		assert(status==1);
+		check();
+		assert(status==3);
+	}
+
+	void check(){
+		assert(status==1);
+		status+=2;
+	}
+}
+
+int main(){
+	MyClass c = new MyClass();
+	assert(status==0);
+	c.test();
+	assert(status==3);
+	check(0);
+	assert(status==4);
+	return 0;
+}
