967c149dc11e3a15079d065ce5c22711d9fdb63b
thomask <devnull@localhost>
2005-02-04 00:14:37 +0000

    extended bug_mtype_507 tests

---
+++ nocompile/bug_mtype_507_C.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-02-03
+// @uri@	news:ctthp6$25b$1@digitaldaemon.com
+
+module dstress.nocompile.bug_mtype_507_C;
+
+void test(){
+	0==Exception;
+}
---
+++ nocompile/bug_mtype_507_F.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-02-03
+// @uri@	news:cttjjg$4i0$2@digitaldaemon.com
+
+module dstress.nocompile.bug_mtype_507_F;
+
+void test(){
+	0 && Exception;
+}
