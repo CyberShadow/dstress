485d69a977e2e11b31a69c0f0aa78c90e2c8fc1e
thomask <devnull@localhost>
2005-02-05 22:35:25 +0000

    cast to void return
    Russell Wilkins <Russell.Wilkins@grovestarsoftware.com>
    2005-02-05
    news:cu1tku$18h6$1@digitaldaemon.com

---
+++ nocompile/cast_19.d
@@ -0,0 +1,19 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Russell Wilkins <Russell.Wilkins@grovestarsoftware.com>
+// @date@	2005-02-05
+// @uri@	news:cu1tku$18h6$1@digitaldaemon.com
+
+module dstress.nocompile.cast_19;
+
+class MyClass{
+}
+
+MyClass[char[]] myarray;
+
+void fn(){
+        foreach (MyClass mc; myarray) return mc;
+}
+
