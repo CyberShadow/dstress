2f83ab1091923bfbad27b6cd5f8597499f5a2fd4
thomask <devnull@localhost>
2005-02-06 11:29:02 +0000

    invariant and ctor
    Thomas Kuehne <thomas-dloop@kuehne.THISISSPAM.cn>
    2005-02-06
    news:vlbhd2-1n2.ln1@lnews.kuehne.cn

---
+++ run/invariant_24.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.THISISSPAM.cn>
+// @date@	2005-02-06
+// @uri@	news:vlbhd2-1n2.ln1@lnews.kuehne.cn
+
+module dstress.run.invariant_24;
+
+class MyClass{
+
+	this(){
+	}
+
+	invariant{
+		assert(0);
+	}
+}
+
+int main(){
+	try{
+		MyClass c = new MyClass();
+	}catch{
+		return 0;
+	}
+	assert(0);
+}
