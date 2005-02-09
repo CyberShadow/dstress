8d92ce1bacfde41037374c62f5447985d27c0110
thomask <devnull@localhost>
2005-02-09 08:08:29 +0000

    added cent and ucent tests

---
+++ nocompile/cent_01.d
@@ -0,0 +1,9 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// cent is reserved for 128bit integers
+
+module dstress.nocompile.cent_01;
+
+int cent;
---
+++ nocompile/ucent_01.d
@@ -0,0 +1,9 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// ucent is reserved for 128bit unsigned integers
+
+module dstress.nocompile.ucent_01;
+
+int ucent;
