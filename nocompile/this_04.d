179217c377f1d6e6ec521076b7de78856082f5d8
thomask <devnull@localhost>
2005-02-12 10:25:41 +0000

    this -> var in classes and structs
    Stewart Gordon <smjg_1998@yahoo.com>
    2005-02-08
    news:cua2nm$11pg$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/2888

---
+++ nocompile/this_04.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2005-02-08
+// @uri@	news:cua2nm$11pg$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2888
+
+module dstress.nocompile.this_04;
+
+struct MyStruct{
+	MyStruct* s = this;
+}
+ 
---
+++ nocompile/this_05.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2005-02-08
+// @uri@	news:cua2nm$11pg$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2888
+
+module dstress.nocompile.this_05;
+
+struct MyStruct{
+	int i;
+	int* p = &i; 
+}
+ 
