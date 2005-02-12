e4995ba19dd94810e5406607162b1cf098f20cd3
thomask <devnull@localhost>
2005-02-12 00:37:55 +0000

    new struct*
    marko <tm030127d@galeb.etf.bg.ac.yu>
    2005-02-10
    news:opslzmx6m0hjnhpc@pentium2
    nntp://news.digitalmars.com/digitalmars.D.bugs/2934

---
+++ run/new_01.d
@@ -0,0 +1,22 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	marko <tm030127d@galeb.etf.bg.ac.yu>
+// @date@	2005-02-10
+// @uri@	news:opslzmx6m0hjnhpc@pentium2
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2934
+
+module dstress.run.new_01;
+
+struct VNode{
+    int a;
+    Node next;
+}
+
+alias VNode* Node;
+
+int main(){
+    Node n = new VNode;
+	return 0;
+} 
