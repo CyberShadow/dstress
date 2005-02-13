6f2f05b0da8f56096810a7fad7ca9d42e4b953a9
thomask <devnull@localhost>
2005-02-13 21:15:50 +0000

    function as case condition
    Rev <Rev_member@pathlink.com>
    2005-02-13
    news:cunl9i$15r2$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/2966

---
+++ nocompile/case_02.d
@@ -0,0 +1,21 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Rev <Rev_member@pathlink.com>
+// @date@	2005-02-13
+// @uri@	news:cunl9i$15r2$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2966
+
+module dstress.nocompile.case_02;
+
+char[] getString(){
+	return "i";
+}
+
+void test(){
+	char[] hold;
+	switch(hold) {
+		case getString():
+	}
+}
