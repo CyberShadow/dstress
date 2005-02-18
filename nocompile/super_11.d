4117a3184f305d63b918a7bb422bf2f2295a21ef
thomask <devnull@localhost>
2005-02-18 23:06:03 +0000

    super outside of classes
    k2 <k2_member@pathlink.com>
    2005-02-18
    news:cv4dsm$2bmr$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/3002

---
+++ nocompile/super_11.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	k2 <k2_member@pathlink.com>
+// @date@	2005-02-18
+// @uri@	news:cv4dsm$2bmr$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/3002
+
+module dstress.nocompile.super_11;
+
+void test(){
+	super();
+}
