b78945088e530d704d620ad26c154ff54a98a400
thomask <devnull@localhost>
2005-02-19 11:23:31 +0000

    inline / static
    Thomas Kuehne <thomas-dloop@kuehne.THISISSPAM.cn>
    2005-02-19
    news:cv77ei$27mv$3@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/3017

---
+++ nocompile/inline_07.d
@@ -0,0 +1,23 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.THISISSPAM.cn>
+// @date@	2005-02-19
+// @uri@	news:cv77ei$27mv$3@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/3017
+
+// __DSTRESS_DFLAGS__ -inline
+
+module dstress.nocompile.inline_07;
+
+struct MyStruct{
+	int bug() {
+		return 3;
+	}
+}
+
+int main(){
+	assert(MyStruct.bug()==3);
+	return 0;
+}
