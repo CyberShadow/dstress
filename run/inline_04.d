b2edcc805ec44ba37dc6753776a6c8815271c9cb
thomask <devnull@localhost>
2005-02-19 11:20:29 +0000

    inline / foreach / struct
    Ilya Zaitseff <sark7@mail333.com>
    2005-02-14
    news:opsl6xtajtaaezs2@robingood
    nntp://news.digitalmars.com/digitalmars.D.bugs/2984

---
+++ run/inline_04.d
@@ -0,0 +1,39 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ilya Zaitseff <sark7@mail333.com>
+// @date@	2005-02-14
+// @uri@	news:opsl6xtajtaaezs2@robingood
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2984
+
+// __DSTRESS_DFLAGS__ -inline
+
+module dstress.run.inline_04;
+
+struct MyStruct{
+
+	int[] x;
+
+	int foo() {
+		return 3;
+	}
+
+	MyStruct bar(){
+		MyStruct v;
+		foreach (inout int f; v.x){
+			f=0;
+		}
+		return v;
+	}
+	
+	float bug(MyStruct p1) {
+		return bar.foo;
+	}
+}
+
+int main(){
+	MyStruct s;
+	assert(s.bug(s)==3);
+	return 0;
+}
---
+++ run/inline_06.d
@@ -0,0 +1,36 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ilya Zaitseff <sark7@mail333.com>
+// @date@	2005-02-14
+// @uri@	news:opsl6xtajtaaezs2@robingood
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/2984
+
+// __DSTRESS_DFLAGS__ -inline
+
+module dstress.run.inline_06;
+
+struct MyStruct{
+
+	int[] x;
+
+	int foo() {
+		return 3;
+	}
+
+	MyStruct bar(){
+		MyStruct v;
+		return v;
+	}
+	
+	float bug(MyStruct p1) {
+		return bar.foo;
+	}
+}
+
+int main(){
+	MyStruct s;
+	assert(s.bug(s)==3);
+	return 0;
+}
