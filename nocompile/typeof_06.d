201017ed622581d1194230b6eb0ff725554d4a96
thomask <devnull@localhost>
2005-02-20 09:57:03 +0000

    typeof / function
    zwang <nehzgnaw@gmail.com>
    2005-02-19
    news:cv7l03$2qfm$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/3023

---
+++ nocompile/typeof_06.d
@@ -0,0 +1,14 @@
+// $HeadULR$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-02-19
+// @uri@	news:cv7l03$2qfm$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/3023
+
+module dstress.nocompile.typeof_06;
+
+void test(){
+	typeof(test) x=0;
+} 
