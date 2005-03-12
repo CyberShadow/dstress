c6e5332419d28f359299a79236c2f4b8372af519
thomask <devnull@localhost>
2005-03-12 10:54:19 +0000

    offsetof
    <derick_eddington@nospam.yahoo.nospam.com>
    2005-03-06
    news:d0dfb6$1mrv$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.bugs/3091

---
+++ run/offsetof_65.d
@@ -0,0 +1,19 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<derick_eddington@nospam.yahoo.nospam.com>
+// @date@	2005-03-06
+// @uri@	news:d0dfb6$1mrv$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/3091
+
+module dstress.run.offsetof_65;
+
+struct MyStruct{
+	wchar x;
+}
+
+int main(){
+	size_t t = MyStruct.x.offsetof;
+	return 0;
+}
