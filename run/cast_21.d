1e9390a6cddc1e8a34f6c6f1cc68b5a908568f93
thomask <devnull@localhost>
2005-03-14 06:11:35 +0000

    ? / bool, implicit cast
    Tintor Marko <tm030127d@galeb.etf.bg.ac.yu>
    2005-03-13
    news:opsnk5ihes1w2jp2@pentium2
    nntp://news.digitalmars.com/digitalmars.D.bugs/3159

---
+++ run/cast_21.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Tintor Marko <tm030127d@galeb.etf.bg.ac.yu>
+// @date@	2005-03-13
+// @uri@	news:opsnk5ihes1w2jp2@pentium2
+// @url@	nntp://news.digitalmars.com/digitalmars.D.bugs/3159
+
+module dstress.run.cast_21;
+
+bool equal(byte* a, byte* b){
+	return (!a || !b) ? (!a && !b) : false;
+} 
+
+int main(){
+	return 0;
+}
