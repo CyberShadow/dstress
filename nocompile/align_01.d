639210dd392bf390ef61a1b318160518f697e67b
thomask <devnull@localhost>
2005-03-16 10:43:45 +0000

    updated align tests to dmd-118

--- nocompile/align_01.d
+++
@@ -1,6 +0,0 @@
-// according to the documentation align can only be applied to struct members
-module dstress.nocompile.align_01;
-
-align (1) int check(){
-	return 5;
-}
--- nocompile/align_03.d
+++
@@ -1,8 +0,0 @@
-// according to the documentation align can only be applied to struct members
-module dstress.nocompile.align_03;
-
-align (1) struct MyStruct{
-	int a;
-	byte* b;
-	int c();
-}
