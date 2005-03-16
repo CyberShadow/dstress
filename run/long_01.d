3e632bfe7b3c6c788221e4f493b6dd67819ca616
thomask <devnull@localhost>
2005-03-16 12:03:21 +0000

    long.min / long.sizeof
    Walter <walter@digitalmars.com>
    2005-03-16
    mail:002601c529e4$fc800da0$0200a8c0@colossus

--- run/long_01.d
+++ run/long_01.d
@@ -3,8 +3,8 @@ module dstress.run.long_01;
 int main(){
 	long a;
 	assert(a.max == 0x7FFF_FFFF_FFFF_FFFFl);
-	assert(a.min == 0xFFFF_FFFF_FFFF_FFFFl);
+	assert(a.min == 0x8000_0000_0000_0000l);
 	assert(a.init == 0);
-	assert(a.sizeof == 4);
+	assert(a.sizeof == 8);
 	return 0; 
 }
