655be0b7da6dbe623e7aee2eba1c8435428f8630
thomask <devnull@localhost>
2005-03-17 15:52:24 +0000

    float as slice index
    Manfred Nowak <svv1999@hotmail.com>
    2005-03-06
    news:d0fg6a$dfa$1@digitaldaemon.com

---
+++ nocompile/slice_10.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Manfred Nowak <svv1999@hotmail.com>
+// @date@	2005-03-06
+// @uri@	news:d0fg6a$dfa$1@digitaldaemon.com
+
+module dstress.nocompile.slice_10;
+
+void test(){
+	int[] array;
+	array.length=4;
+	float f=.2;
+	array[1..2]= array[f..1.3];
+}
+
