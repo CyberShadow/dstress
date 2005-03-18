6be01994c36135f86567d03cb9833cf99bd2a1c0
thomask <devnull@localhost>
2005-03-18 05:29:08 +0000

    array - int
    h3r3tic
    2005-03-18
    news:d1d4lc$25e2$1@digitaldaemon.com

---
+++ nocompile/cast_23.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	h3r3tic
+// @date@	2005-03-18
+// @uri@	news:d1d4lc$25e2$1@digitaldaemon.com
+
+// __DSTRESS_ERROR__ 16
+
+module dstress.nocompile.cast_23;
+
+int main(){
+	int[10] arr;
+	int *p = &arr[5];
+	cast(uint)(p - arr);
+	return 0;
+}
