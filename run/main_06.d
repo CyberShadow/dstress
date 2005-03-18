c2cf9560bd85d33ae8514b4a1b444e700292e53c
thomask <devnull@localhost>
2005-03-18 13:28:49 +0000

    return contains an Expression and no ExpressionStatement, thus effectless code is legal

---
+++ run/main_06.d
@@ -0,0 +1,12 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// return contains an Expression and no ExpressionStatement,
+// thus effectless code is legal
+
+module dstress.run.main_06;
+
+void main(){
+	return 0;
+}
