a45109fa238342210e71179f6d4ff63c5412311e
thomask <devnull@localhost>
2005-03-20 07:26:58 +0000

    forward referenced mixin
    Ben Hinkle
    2005-03-18
    news:d1faqu$1pir$1@digitaldaemon.com
    nntp://news.digitalmars.com/digitalmars.D.announce/10

---
+++ run/mixin_08.d
@@ -0,0 +1,27 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ben Hinkle
+// @date@	2005-03-18
+// @uri@	news:d1faqu$1pir$1@digitaldaemon.com
+// @url@	nntp://news.digitalmars.com/digitalmars.D.announce/10
+
+module dstress.run.mixin_08;
+
+template Foo(alias tail) {
+	int test(){
+		return 1;
+	}
+}
+
+struct List(V) {
+	V tail;
+	mixin Foo!(tail);
+}
+
+int main(){
+	List!(int) x;
+	assert(x.test()==1);
+	return 0;
+}
