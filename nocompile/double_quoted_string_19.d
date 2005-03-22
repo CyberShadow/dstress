6770f33837d1c10ab5bd0cc16839653b0b44156d
thomask <devnull@localhost>
2005-03-22 08:04:31 +0000

    Regan Heath <regan@netwin.co.nz> 2005-03-22 news:opsn0izzsh23k2f5@nrage.netwin.co.nz

---
+++ nocompile/double_quoted_string_19.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Regan Heath <regan@netwin.co.nz>
+// @date@	2005-03-22
+// @uri@	news:opsn0izzsh23k2f5@nrage.netwin.co.nz
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.double_quoted_string_19;
+
+void test(){
+	"stringliteral";
+}
