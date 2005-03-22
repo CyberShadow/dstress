8ed69f0f65718efe6784811ef29b652ed1fb45f6
thomask <devnull@localhost>
2005-03-22 08:06:01 +0000

    Regan Heath <regan@netwin.co.nz> 2005-03-19 news:opsnvajfcb23k2f5@nrage.netwin.co.nz

---
+++ run/init_03.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Regan Heath <regan@netwin.co.nz>
+// @date@	2005-03-19
+// @uri@	news:opsnvajfcb23k2f5@nrage.netwin.co.nz
+
+module dstress.run.init_03;
+
+int main(){
+        static int[4] array2 = [5,6,7,8];
+        TypeInfo i = typeid(typeof(array2.init));
+	assert(i !== null);
+	return 0;
+}
+
