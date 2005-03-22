221799cd42b907a62f5b7ce5f11243b61236e8b6
thomask <devnull@localhost>
2005-03-22 19:59:20 +0000

    mixin ctor
    Regan Heath <regan@netwin.co.nz>
    2005-03-17
    news:opsnp8x0v923k2f5@nrage.netwin.co.nz

---
+++ run/mixin_09.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Regan Heath <regan@netwin.co.nz>
+// @date@	2005-03-17
+// @uri@	news:opsnp8x0v923k2f5@nrage.netwin.co.nz
+
+module dstress.run.mixin_09;
+
+class A {
+        template ctor(Type)
+        {
+                this(Type[] arr)
+                {
+                        foreach(Type v; arr) writefln(typeid(typeof(v)));
+                }
+        }
+
+        mixin ctor!(int);
+}
+
+int main(){
+	static int[] ints = [0,1,2,3];
+	A a = new A(ints);
+	return 0;
+}
+
---
+++ run/mixin_10.d
@@ -0,0 +1,29 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Regan Heath <regan@netwin.co.nz>
+// @date@	2005-03-17
+// @uri@	news:opsnp8x0v923k2f5@nrage.netwin.co.nz
+
+module dstress.run.mixin_10;
+
+class A {
+        template ctor(Type)
+        {
+                this(Type[] arr)
+                {
+                        foreach(Type v; arr) writefln(typeid(typeof(v)));
+                }
+        }
+
+        mixin ctor!(int);
+	mixin ctor!(Object);
+}
+
+int main(){
+	static int[] ints = [0,1,2,3];
+	A a = new A(ints);
+	return 0;
+}
+
