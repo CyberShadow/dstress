6f19958205726e4d6bf1871126b01deee57eaeb5
thomask <devnull@localhost>
2005-03-25 07:33:35 +0000

    struct alignment / gc
    Ben Hinkle <bhinkle@mathworks.com>
    2005-03-23
    news:d1s5gs$2odc$1@digitaldaemon.com

---
+++ run/align_09.d
@@ -0,0 +1,35 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ben Hinkle <bhinkle@mathworks.com>
+// @date@	2005-03-23
+// @uri@	news:d1s5gs$2odc$1@digitaldaemon.com
+
+// @WARNING@	imports Phobos' gc
+
+module dstress.run.align_09;
+
+import std.gc;
+
+struct S{
+	char[] font_face;
+	byte charset;
+}
+
+int main(){
+	S[] x;
+
+	for (int i=0;i<3;i++) {
+		S s;
+		s.font_face="font face".dup;
+		x ~= s;
+	}
+
+	fullCollect();
+
+	assert(x[0].font_face=="font face");
+	assert(x[1].font_face=="font face");
+
+	return 0;
+}
---
+++ run/align_10.d
@@ -0,0 +1,39 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ben Hinkle <bhinkle@mathworks.com>
+// @date@	2005-03-23
+// @uri@	news:d1s5gs$2odc$1@digitaldaemon.com
+
+// @WARNING@	imports Phobos' gc
+
+module dstress.run.align_10;
+
+import std.gc;
+
+struct S{
+	char[] font_face;
+	byte charset;
+}
+
+int main(){
+	S[] x;
+
+	S s;
+	s.font_face="font face".dup;
+	x ~= s;
+	s.font_face="font face".dup;
+	x ~= s;
+	s.font_face="font face".dup;
+	x ~= s;
+	s.font_face="font face".dup;
+	x ~= s;
+
+	fullCollect();
+
+	assert(x[0].font_face=="font face");
+	assert(x[1].font_face=="font face");
+
+	return 0;
+}
