1554f0335184b7b8301f6917ac5ec44d2a6e099d
thomask <devnull@localhost>
2005-03-25 08:33:16 +0000

    bit[] ~= bit[]
    Regan Heath <regan@netwin.co.nz>
    2005-03-17
    news:opsnra6xw523k2f5@nrage.netwin.co.nz

---
+++ run/opCatAssign_10.d
@@ -0,0 +1,84 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Regan Heath <regan@netwin.co.nz>	
+// @date@	2005-03-17
+// @uri@	news:opsnra6xw523k2f5@nrage.netwin.co.nz
+
+module dstress.run.opCatAssign_10;
+
+int main(){
+
+	byte[] a;
+	byte[] b;
+    
+	a.length = 8;
+	a[0] = 0;
+	a[1] = 1;
+	a[2] = 1;
+	a[3] = 0;
+	a[4] = 0;
+	a[5] = 1;
+	a[6] = 0;
+	a[7] = 0;
+    
+	assert(a.length==8);
+	assert(b.length==0);
+	b ~= a;
+
+	assert(a.length==8);
+	assert(b.length==8);
+	
+	assert(a[0]==0);
+	assert(a[1]==1);
+	assert(a[2]==1);
+	assert(a[3]==0);
+	assert(a[4]==0);
+	assert(a[5]==1);
+	assert(a[6]==0);
+	assert(a[7]==0);
+	
+	assert(b[0]==0);
+	assert(b[1]==1);
+	assert(b[2]==1);
+	assert(b[3]==0);
+	assert(b[4]==0);
+	assert(b[5]==1);
+	assert(b[6]==0);
+	assert(b[7]==0);
+	
+	
+	b ~= a;
+
+	assert(a.length==8);
+	assert(b.length==16);
+
+	assert(a[0]==0);
+	assert(a[1]==1);
+	assert(a[2]==1);
+	assert(a[3]==0);
+	assert(a[4]==0);
+	assert(a[5]==1);
+	assert(a[6]==0);
+	assert(a[7]==0);
+	
+	assert(b[0]==0);
+	assert(b[1]==1);
+	assert(b[2]==1);
+	assert(b[3]==0);
+	assert(b[4]==0);
+	assert(b[5]==1);
+	assert(b[6]==0);
+	assert(b[7]==0);
+	assert(b[8]==0);
+	assert(b[9]==1);
+	assert(b[10]==1);
+	assert(b[11]==0);
+	assert(b[12]==0);
+	assert(b[13]==1);
+	assert(b[14]==0);
+	assert(b[15]==0);
+
+	return 0;
+}
