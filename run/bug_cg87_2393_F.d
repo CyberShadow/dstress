a5b8e238f361a84cfe707c3af8d989f0298b1a1d
thomask <devnull@localhost>
2005-03-27 18:44:42 +0000

    foat/ifloat/cfloat: -O -inline
    Tom S <h3r3tic@remove.mat.uni.torun.pl>
    2005-03-27
    news:d26pug$2m4m$1@digitaldaemon.com

---
+++ run/bug_cg87_2393_F.d
@@ -0,0 +1,26 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Tom S <h3r3tic@remove.mat.uni.torun.pl>
+// @date@	2005-03-27
+// @uri@	news:d26pug$2m4m$1@digitaldaemon.com
+
+module dstress.run.bug_cg87_2393_F;
+
+cfloat x;
+    
+void foo(){
+	x = -x;
+}
+
+void bar(){
+	return foo();
+}
+
+int main(){
+	x=2;
+	bar();
+	assert(x==-2);
+	return 0;
+} 
---
+++ run/bug_cg87_279_G.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Tom S <h3r3tic@remove.mat.uni.torun.pl>
+// @date@	2005-03-27
+// @uri@	news:d26pug$2m4m$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ -O -inline
+
+module dstress.run.bug_cg87_279_G;
+
+int x;
+    
+void foo(){
+	x = -x;
+}
+
+void bar(){
+	return foo();
+}
+
+int main(){
+	x=2;
+	bar();
+	assert(x==-2);
+	return 0;
+} 
