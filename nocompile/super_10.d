bfa62c7d83b27cac95d6dd05851b39c607a5669f
thomask <devnull@localhost>
2005-04-08 05:33:27 +0000

    super call from classmember function
    Carlos Santander B. <csantander619@gmail.com>
    2005-04-07
    news:d34se7$18vb$1@digitaldaemon.com

---
+++ nocompile/super_10.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Carlos Santander B. <csantander619@gmail.com>
+// @date@	2005-04-07
+// @uri@	news:d34se7$18vb$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.super_10;
+
+class A{
+	void foo(){
+		super();
+	}
+}
