aace7ea11ecf88728407c6658d47f7148ca44e42
thomask <devnull@localhost>
2005-04-10 23:34:13 +0000

    &unittest0
    Thomas Kuehne <thomas-dloop.kuehne.thisisspam.cn>
    2005-04-10
    news:5v4mi2-l36.ln1@lnews.kuehne.cn

---
+++ nocompile/unittest_07.d
@@ -0,0 +1,19 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop.kuehne.thisisspam.cn>
+// @date@	2005-04-10
+// @uri@	news:5v4mi2-l36.ln1@lnews.kuehne.cn
+
+// __DSTRESS_ELINE__ 
+
+module dstress.nocompile.unittest_07;
+
+unittest{
+}
+
+void test(){
+	void function() u = &unittest0;
+}
+
