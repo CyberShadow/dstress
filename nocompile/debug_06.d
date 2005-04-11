bc2757a3cfd40b003bdba767f0dd1e81298497f6
thomask <devnull@localhost>
2005-04-11 19:48:41 +0000

    debug/version with multiple causes
    Manfred Nowak <svv1999@hotmail.com>
    2005-04-11
    news:d3dl5r$24k7$1@digitaldaemon.com

---
+++ nocompile/debug_06.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Manfred Nowak <svv1999@hotmail.com>
+// @date@	2005-04-11
+// @uri@	news:d3dl5r$24k7$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.debug_06;
+
+void test(){
+	debug (1)(2)(3);
+}
+
---
+++ nocompile/version_32.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Manfred Nowak <svv1999@hotmail.com>
+// @date@	2005-04-11
+// @uri@	news:d3dl5r$24k7$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.version_32;
+
+void test(){
+	version (1)(2)(3);
+}
+
