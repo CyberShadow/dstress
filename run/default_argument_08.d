75cffc36d36333f3760a7cf2c808a09bb130d9a9
thomask <devnull@localhost>
2005-04-11 04:05:37 +0000

    default argument, forward reference
    Lars Ivar Igesund <larsivar@igesund.net>
    2005-04-09
    news:d39ij9$2bf9$1@digitaldaemon.com

---
+++ run/default_argument_08.d
@@ -0,0 +1,24 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Lars Ivar Igesund <larsivar@igesund.net>	
+// @date@	2005-04-09
+// @uri@	news:d39ij9$2bf9$1@digitaldaemon.com
+
+module dstress.run.default_argument_08;
+
+int foo(char [] string = BAR){
+	return string.length;
+}
+
+char [] BAR = "Bar";
+
+int main(){
+	assert(foo()==3);
+	assert(foo("a")==1);
+	assert(foo==3);
+	BAR="ab";
+	assert(foo()==2);
+	return 0;
+}
