f06e86685a5ccb49673a8711fe780f96845fc9b0
thomask <devnull@localhost>
2005-04-12 14:28:49 +0000

    setting length of constant array
    MicroWizard <MicroWizard_member@pathlink.com>
    2005-04-11
    news:d3em9h$bdg$1@digitaldaemon.com

---
+++ nocompile/length_07.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	MicroWizard <MicroWizard_member@pathlink.com>
+// @date@	2005-04-11
+// @uri@	news:d3em9h$bdg$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.length_07;
+
+void test(){
+	char[2] kuka;
+	kuka.length=2;
+}
+
