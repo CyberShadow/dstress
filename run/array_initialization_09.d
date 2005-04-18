9fe876a9f49af62fa7110dcb4957b743be80991c
thomask <devnull@localhost>
2005-04-18 07:21:15 +0000

    big array crash
    zwang <nehzgnaw@gmail.com>
    2005-04-17
    news:d3sq2j$1086$1@digitaldaemon.com

---
+++ run/array_initialization_09.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-04-17
+// @uri@	news:d3sq2j$1086$1@digitaldaemon.com
+
+
+module dstress.run.array_initialization_09;
+
+int main(){
+	char a[int.max/32];
+	return 0;
+}
+
