f3c4c67d9bb391eec591c87003e03534cb40bd5c
thomask <devnull@localhost>
2005-04-18 08:29:45 +0000

    non-existand module versus type name clash

---
+++ nocompile/import_06_A.d
@@ -0,0 +1,8 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// __DSTRESS_DFLAGS__ -Inocompile
+
+import import_06_B;
+import someName;
---
+++ nocompile/import_06_B.d
@@ -0,0 +1,9 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// __DSTRESS_DFLAGS__ -Inocompile
+
+import import_06_A;
+
+someName noop;
