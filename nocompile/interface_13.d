ced1dcf5926ac78cee35c22975a4538bafd698e4
thomask <devnull@localhost>
2005-04-20 07:33:17 +0000

    interface segfault
    zwang <nehzgnaw@gmail.com>
    2005-04-20

---
+++ nocompile/interface_13.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date	2005-04-20
+// @uri@	news:d44vt0$2hf2$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.interface_13;
+
+interface : Name
---
+++ nocompile/interface_14.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date	2005-04-20
+// @uri@	news:d45015$2hf2$2@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.interface_14;
+
+interface
