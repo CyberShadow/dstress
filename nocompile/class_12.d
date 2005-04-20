db9c5dec2cdbbd1209b8e465ecdcfa018ae02c82
thomask <devnull@localhost>
2005-04-20 05:24:22 +0000

    extended segmentfault for anonymous classes

---
+++ nocompile/class_12.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.thisisspam.cn>
+// @date@	2005-04-19
+// @uri@	news:d6bgj2-k2c.ln1@lnews.kuehne.cn
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.class_11;
+
+class 
