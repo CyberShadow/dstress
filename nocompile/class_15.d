9114716a7c5b985f725df481fad1e4c72922b53d
thomask <devnull@localhost>
2005-04-20 12:42:30 +0000

    cyclic classes/interfaces (2nd)
    zwang <nehzgnaw@gmail.com>
    2005-04-20
    news:d452oo$2km6$1@digitaldaemon.com

---
+++ nocompile/class_15.d
@@ -0,0 +1,21 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-04-20
+// @uri@	news:d452oo$2km6$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 17
+
+module dstress.nocompile.class_15;
+
+class A:B{
+	void f();
+}
+
+class B:A{
+	void f();
+}
+
+
---
+++ nocompile/interface_17.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-04-20
+// @uri@	news:d452oo$2km6$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 17
+
+module dstress.nocompile.interface_17;
+
+interface A:B{
+	void f();
+}
+
+interface B:A{
+	void f();
+}
+
