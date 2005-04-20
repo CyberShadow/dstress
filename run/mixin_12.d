c0f5c551f2823336ee805dd80f3f93fc93a341c0
thomask <devnull@localhost>
2005-04-20 19:47:10 +0000

    mixin opCall
    Jarrett Billingsley <kb3ctd2@yahoo.com>
    2005-03-23
    news:d1sosj$cmo$1@digitaldaemon.com

---
+++ run/mixin_12.d
@@ -0,0 +1,45 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Jarrett Billingsley <kb3ctd2@yahoo.com>
+// @date@	2005-03-23
+// @uri@	news:d1sosj$cmo$1@digitaldaemon.com
+
+module dstress.run.mixin_12;
+
+class A{
+	template M(){
+		int C(){
+			return 10;
+		}
+
+		int opCall(){
+			return 15;
+		}
+	}
+
+	mixin M m;
+
+	template N(){
+		int C(){
+			return 20;
+		}
+
+		int opCall(){
+			return 25;
+		}
+	}
+
+	mixin N n;
+}
+
+int main(){
+	A a=new A;
+	assert(a.m.C()==10);
+	assert(a.n.C()==20);
+
+	assert(a.m()==15);
+	assert(a.n()==25);
+	return 0;
+}
