9f610506481440c326f9b641b8bf6e6fd120a8b5
thomask <devnull@localhost>
2005-04-20 04:15:53 +0000

    static assert inside templated class/struct
    Thomas Kuehne <thomas-dloop@kuehne.thisisspam.cn>
    2005-04-20
    news:3m1hj2-ni3.ln1@lnews.kuehne.cn

---
+++ nocompile/assert_08.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.thisisspam.cn>
+// @date@	2005-04-20
+// @uri@	news:3m1hj2-ni3.ln1@lnews.kuehne.cn
+
+// __DTSRESS_ELINE__ 14 
+
+module dstress.nocompile.assert_08;
+
+class A (T){
+	static assert(0);
+}	
---
+++ nocompile/assert_09.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.thisisspam.cn>
+// @date@	2005-04-20
+// @uri@	news:3m1hj2-ni3.ln1@lnews.kuehne.cn
+
+// __DTSRESS_ELINE__ 14 
+
+module dstress.nocompile.assert_09;
+
+struct A (T){
+	static assert(0);
+}	
