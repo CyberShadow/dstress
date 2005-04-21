c47ed48cd08b6a282ff2242ee9244ae96f581dd2
thomask <devnull@localhost>
2005-04-21 05:34:31 +0000

    type safety of variadic arguments
    Manfred Nowak <svv1999@hotmail.com>
    2005-04-21
    news:d46udp$1mqe$1@digitaldaemon.com

---
+++ run/variadic_argument_04.d
@@ -0,0 +1,30 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Manfred Nowak <svv1999@hotmail.com>
+// @date	2005-04-21
+// @uri@	news:d46udp$1mqe$1@digitaldaemon.com
+
+// @WARNING@    direct access to Phobos
+
+module dstress.run.variadic_argument_04;
+
+import std.stdarg;
+
+int status;
+
+template t(T){
+	T t(...){
+		assert(_arguments[ 0] != typeid( T));
+		status++;
+		return va_arg!(T)(_argptr);
+	}
+}
+
+int main(){
+	t!(int delegate()) (delegate real(){return 1.1L;});
+	assert(status==1);	
+	return 0;
+}
+
