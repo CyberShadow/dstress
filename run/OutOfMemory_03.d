f8f3c9ccbf3d8d8550d404e2f55df621ee34c1ad
thomask <devnull@localhost>
2005-04-23 06:55:16 +0000

    OutOfMemory not called
    Manfred Nowak <svv1999@hotmail.com>
    2005-04-023
    news:d4a8b9$1rr3$1@digitaldaemon.com

---
+++ run/OutOfMemory_03.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Manfred Nowak <svv1999@hotmail.com>
+// @date@	2005-04-023
+// @uri@	news:d4a8b9$1rr3$1@digitaldaemon.com
+
+module dstress.run.OutOfMemory_03;
+
+debug import std.c.stdio;
+
+Object[] a;
+
+int main(){
+	int delta= 100000;
+	while(1){
+		try{
+			a.length = a.length + delta;
+debug			fprintf(stderr,"%d ", a.length);
+		}catch{
+debug			fprintf(stderr, "Allocated array of length %d.\n", a.length);
+			return 0;
+		}
+	}
+	assert(0)
+}
+
