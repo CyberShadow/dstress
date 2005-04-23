c08c0beb7ce677a1440e9e2801863c1ba9a96d2d
thomask <devnull@localhost>
2005-04-23 06:12:33 +0000

    override alignof
    Jarrett Billingsley <kb3ctd2@yahoo.com>
    2005-04-22
    news:d4c3qc$eu8$1@digitaldaemon.com

---
+++ nocompile/alignof_02.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Jarrett Billingsley <kb3ctd2@yahoo.com>
+// @date@	2005-04-22
+// @uri@	news:d4c3qc$eu8$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.alignof_02;
+
+class A{
+	size_t alignof;
+}
