ec7bb2d1f14c1641bf182fe3776b21871cc2e408
thomask <devnull@localhost>
2005-04-23 05:56:24 +0000

    implicit conversion between ifloat and float
    David L. Davis <SpottedTiger@yahoo.com>
    2005-04-23
    news:d4cemj$mma$1@digitaldaemon.com

---
+++ nocompile/bug_cg87_1240_A.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	David L. Davis <SpottedTiger@yahoo.com>
+// @date@	2005-04-23
+// @uri@	news:d4cemj$mma$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 16
+
+module dstress.nocompile.bug_cg87_1240_A;
+
+int main(){
+	real r;
+ 
+	if (r > ireal.max){
+		assert(0);
+	}		
+	return 0;
+}
---
+++ nocompile/bug_cg87_1240_D.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	David L. Davis <SpottedTiger@yahoo.com>
+// @date@	2005-04-23
+// @uri@	news:d4cemj$mma$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 16
+
+module dstress.nocompile.bug_cg87_1240_D;
+
+int main(){
+	ifloat r;
+ 
+	if (r < float.max){
+		assert(0);
+	}		
+	return 0;
+}
