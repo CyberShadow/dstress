795a12d9532ea829cfe0b846fde1819560e413b8
thomask <devnull@localhost>
2005-04-23 06:12:08 +0000

    override alignof / offsetof
    Jarrett Billingsley <kb3ctd2@yahoo.com>
    2005-04-22
    news:d4c3qc$eu8$1@digitaldaemon.com

---
+++ nocompile/offsetof_76.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Jarrett Billingsley <kb3ctd2@yahoo.com>
+// @date@	2005-04-22
+// @uri@	news:d4c3qc$eu8$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.offsetof_76;
+
+struct A{
+	static size_t alignof(){
+		return 0;
+	}
+}
