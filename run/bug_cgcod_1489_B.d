cec5386729de679ec4c3aed4a9f720c17b21260d
thomask <devnull@localhost>
2005-04-25 05:25:13 +0000

    cgcod.c 1489
    Kevin Bealer <Kevin_member@pathlink.com>
    2005-04-24
    news:d4fgcc$6ob$1@digitaldaemon.com

---
+++ run/bug_cgcod_1489_B.d
@@ -0,0 +1,30 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kevin Bealer <Kevin_member@pathlink.com>
+// @date@	2005-04-24
+// @uri@	news:d4fgcc$6ob$1@digitaldaemon.com
+
+module dstress.run.bug_cgcod_1489_B;
+
+struct S {
+	int a, b;
+}
+    
+S[] x;
+        
+S bug(S s){
+	x.length = 9;
+	return x[0] = s;
+}
+
+int main(){
+	S var;
+	var.a=2;
+
+	assert(var==bug(var));
+
+	return 0;
+}
+
