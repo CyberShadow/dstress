216341942763b3ccfc75bc24a960d6905f2ddb35
thomask <devnull@localhost>
2005-04-27 12:21:18 +0000

    template loop
    Regan Heath <regan@netwin.co.nz>
    2005-03-30
    news:opsof4hwgy23k2f5@nrage.netwin.co.nz

---
+++ run/bug_20050330_B.d
@@ -0,0 +1,27 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Regan Heath <regan@netwin.co.nz>
+// @date@	2005-03-30
+// @uri@	news:opsof4hwgy23k2f5@nrage.netwin.co.nz
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.run.bug_20050330_B;
+
+template Blah(int a, alias B){
+	mixin Blah!(a-1, alias B);
+}
+
+template Blah(int a:0, alias B){
+	int x(){
+		return a;
+	}
+}
+
+int main(){
+	int a;
+	mixin Blah!(5,a);
+	return 0;
+}
