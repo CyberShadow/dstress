ae64ce1166f4577fa8a72ba993361fde0cb1cbb5
thomask <devnull@localhost>
2005-04-27 05:49:25 +0000

    template / private member
    Mike Parker <aldacron71@yahoo.com>
    2005-04-25
    news:d4i19u$12td$1@digitaldaemon.com

---
+++ run/package_03.d
@@ -0,0 +1,24 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Mike Parker <aldacron71@yahoo.com>
+// @date@	2005-04-25
+// @uri@	news:d4i19u$12td$1@digitaldaemon.com
+
+module dstress.run.package_03;
+
+template T(){
+	package int x;
+}
+
+class MyClass{
+	mixin T;
+}
+
+int main(){
+	MyClass c=new MyClass;
+	c.x = 1;
+	return 0;
+}
+
