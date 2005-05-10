2ad07f9073490a5ee7809fd9d2f5868ff10ed249
thomask <devnull@localhost>
2005-05-10 23:48:17 +0000

    interface (no circular)
    zwang <nehzgnaw@gmail.com>
    2005-05-05
    news:d5c7i4$c4i$1@digitaldaemon.com

---
+++ compile/interface_20.d
@@ -0,0 +1,21 @@
+// $HeadURL: http://dstress.kuehne.cn/compile/interface_20.d $
+// $Date: 2005-05-06 11:14:07 +0200 (Fr, 06 Mai 2005) $
+// $Author: thomask $
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-05-05
+// @uri@	news:d5c7i4$c4i$1@digitaldaemon.com
+
+module dstress.compile.interface_20;
+
+interface A:B{
+}
+
+interface B:C{
+}
+
+interface C:D{
+}
+
+interface D{
+}
---
+++ compile/interface_21.d
@@ -0,0 +1,22 @@
+// $HeadURL: http://dstress.kuehne.cn/compile/interface_21.d $
+// $Date: 2005-05-06 11:14:07 +0200 (Fr, 06 Mai 2005) $
+// $Author: thomask $
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-05-05
+// @uri@	news:d5c7i4$c4i$1@digitaldaemon.com
+
+module dstress.compile.interface_21;
+
+class A:B{
+}
+
+interface B:C{
+}
+
+interface C:D{
+}
+
+interface D{
+}
+
