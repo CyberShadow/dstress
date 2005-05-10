9423ebec7e10da5f3e5c7c88e6a4bf8cc9539200
thomask <devnull@localhost>
2005-05-10 23:45:02 +0000

    moved big static array test to nocompile

--- nocompile/array_initialization_08.d
+++ nocompile/array_initialization_08.d
@@ -6,8 +6,7 @@
 // @date@	2005-04-17
 // @uri@	news:d3sq2j$1086$1@digitaldaemon.com
 
-
-module dstress.run.array_initialization_08;
+module dstress.nocompile.array_initialization_08;
 
 int main(){
 	static char a[int.max/32];
