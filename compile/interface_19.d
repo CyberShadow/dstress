babf6cadbb8c09ad4c8a711b991ff609815e8a12
thomask <devnull@localhost>
2005-05-10 23:47:41 +0000

    empty interface
    zwang <nehzgnaw@gmail.com>
    2005-05-05
    news:d5c8la$d1d$1@digitaldaemon.com

---
+++ compile/interface_19.d
@@ -0,0 +1,13 @@
+// $HeadURL: http://dstress.kuehne.cn/compile/interface_19.d $
+// $Date: 2005-05-06 11:07:26 +0200 (Fr, 06 Mai 2005) $
+// $Author: thomask $
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-05-05
+// @uri@	news:d5c8la$d1d$1@digitaldaemon.com
+
+module dstress.compile.interface_19;
+
+interface I{
+}
+
---
+++ nocompile/interface_18.d
@@ -0,0 +1,14 @@
+// $HeadURL: http://dstress.kuehne.cn/nocompile/interface_18.d $
+// $Date: 2005-05-06 11:07:26 +0200 (Fr, 06 Mai 2005) $
+// $Author: thomask $
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-05-05
+// @uri@	news:d5c8la$d1d$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.interface_18;
+
+interface I;
+
