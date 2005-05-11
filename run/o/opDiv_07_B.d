9ddabae6590c49c3e0441a3c035d807f7532302a
thomask <devnull@localhost>
2005-05-11 17:06:05 +0000

    compiletime versus runtime opDiv
    David Friedman <d3rdclsmail_a_@_t_earthlink_d_._t_net>
    2005-05-03
    news:d595il$lqr$2@digitaldaemon.com

---
+++ run/o/opDiv_07_B.d
@@ -0,0 +1,25 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	David Friedman <d3rdclsmail_a_@_t_earthlink_d_._t_net>
+// @date@	2005-05-03
+// @uri@	news:d595il$lqr$2@digitaldaemon.com
+
+module dstress.run.o.opDiv_07_B;
+
+int main(){
+	ifloat a = -1.0fi / 0.0fi;
+
+	ifloat b = -1.0fi;
+	ifloat c = 0.0fi;
+	ifloat d;
+
+	void* X = &a;
+
+	d = b/c;
+	void* Y = &d;
+
+	assert((cast(int*)X)[0]==(cast(int*)Y)[0]);
+	return 0;
+}
---
+++ run/o/opDiv_08_B.d
@@ -0,0 +1,27 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	David Friedman <d3rdclsmail_a_@_t_earthlink_d_._t_net>
+// @date@	2005-05-03
+// @uri@	news:d595il$lqr$2@digitaldaemon.com
+
+module dstress.run.o.opDiv_08_B;
+
+int main(){
+	idouble a = -1.0i / 0.0i;
+
+	idouble b = -1.0i;
+	idouble c = 0.0i;
+	idouble d;
+
+	void* X = &a;
+
+	d = b/c;
+	void* Y = &d;
+
+	assert((cast(int*)X)[0]==(cast(int*)Y)[0]);
+	assert((cast(int*)X)[1]==(cast(int*)Y)[1]);
+
+	return 0;
+}
