b48d036c98f6ad0778d8b30fc6a99b9a35c89d9b
thomask <devnull@localhost>
2005-05-11 16:32:17 +0000

    attributes & alias

---
+++ nocompile/a/alias_21.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-05-01
+// @uri@	news:d5247u$28ch$2@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.a.alias_18;
+
+alias override int mInt;
