a77ac0b18489d6107b626d852f6868abbf033a75
thomask <devnull@localhost>
2005-05-12 00:16:05 +0000

    empty declaration makes illegal delegate declaration valid
    Manfred Nowak <svv1999@hotmail.com>
    2005-05-09
    news:d5nifh$k61$2@digitaldaemon.com

---
+++ nocompile/d/delegate_14_A.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Manfred Nowak <svv1999@hotmail.com>
+// @date@	2005-05-09
+// @uri@	news:d5nifh$k61$2@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.d.delegate_14_A;
+
+int main(){
+	delegate void ();
+}
+
