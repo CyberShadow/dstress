c625fe5ec04d8ef4d17056caa740a078e82a621f
thomask <devnull@localhost>
2005-05-14 05:12:26 +0000

    nested foreach and opApply
    pragma <pragma_member@pathlink.com>
    2005-05-12
    news:d6010n$i01$1@digitaldaemon.com

---
+++ compile/f/foreach_30_A.d
@@ -0,0 +1,27 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	pragma <pragma_member@pathlink.com>
+// @date@	2005-05-12
+// @uri@	news:d6010n$i01$1@digitaldaemon.com
+
+module dstress.compile.f.foreach_30_A;
+
+class Collection{
+	int opApply(int delegate(inout int) dg){
+		return 0;
+	}
+}
+
+bool test(){
+	Collection a,b;
+	
+	foreach(int x; a){
+		foreach(int y; b){
+			return false;
+		}
+	}
+	
+	return true;
+}
