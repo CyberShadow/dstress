3e71cf53e4aca5dd38f22b16bfd33d013915cf21
thomask <devnull@localhost>
2005-05-14 07:05:51 +0000

    mixin / class member / this
    David Friedman <d3rdclsmail_a_@_t_earthlink_d_._t_net>
    2005-05-04
    news:d595ii$lqr$1@digitaldaemon.com

---
+++ run/m/mixin_13.d
@@ -0,0 +1,38 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	David Friedman <d3rdclsmail_a_@_t_earthlink_d_._t_net>
+// @date@	2005-05-04
+// @uri@	news:d595ii$lqr$1@digitaldaemon.com
+
+module dstress.run.m.mixin_13;
+
+int status;
+
+class C {
+	template T(alias f) {
+		void check(){
+			f();		
+			assert(status++==2);
+		}
+	}
+
+	void test(){
+		assert(status++==1);
+	}
+
+	void run() {
+		assert(status++==0);
+		mixin T!(test) x;
+		x.check();
+		assert(status++==3);
+	}
+}
+
+int main(){
+	C c = new C;
+	c.run();
+	assert(status==4);
+	return 0;
+}
