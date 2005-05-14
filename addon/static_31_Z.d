fde9864bfad194ef92119f7a9af9905fd69de65f
thomask <devnull@localhost>
2005-05-14 07:29:49 +0000

    import / static class members
    David Friedman <d3rdclsmail_a_@_t_earthlink_d_._t_net>
    2005-05-13
    news:d61bou$1ghv$1@digitaldaemon.com

---
+++ addon/static_31_Z.d
@@ -0,0 +1,11 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+module addon.static_31_Z;
+
+class MyClass{
+	static int test(int i){
+		return i+1;
+	}
+}
---
+++ run/s/static_31.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	David Friedman <d3rdclsmail_a_@_t_earthlink_d_._t_net>
+// @date@	2005-05-13
+// @uri@	news:d61bou$1ghv$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ addon/static_31_Z.d
+
+module dstress.run.s.static_31;
+import addon.static_31_Z;
+
+int main(){
+	assert(MyClass.test(1)==2);
+	return 0;
+}
