a86b7e29f3805629b55064621b442a954c5b61f5
thomask <devnull@localhost>
2005-05-17 14:44:52 +0000

    botched wysiwyg-stringliterals
    Hiroshi Sakurai <Hiroshi_member@pathlink.com>
    2005-05-17
    news:d6bm67$cfr$1@digitaldaemon.com
    http://f17.aaa.livedoor.jp/~labamba/?BugTrack%2F13

---
+++ run/u/unicode_08_D.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Hiroshi Sakurai <Hiroshi_member@pathlink.com>
+// @date@	2005-05-17
+// @uri@	news:d6bm67$cfr$1@digitaldaemon.com	
+// @url@	http://f17.aaa.livedoor.jp/~labamba/?BugTrack%2F13
+
+module dstress.run.u.unicode_08_D;
+
+int main(){
+	static ubyte[] master = [
+		0xE3u, 0x83u, 0xAFu, 0xE3u, 0x83u, 0xADu, 0xE3u, 0x82u,
+		0xB9u, 0xEFu, 0xBDu, 0x97u
+	];
+
+	char[] string = x"E3 83 AF E3 83 AD E3 82 B9 EF BD 97";
+
+	assert(string.length==master.length);
+
+	for(int i=0; i<master.length; i++){
+		assert(string[i]==master[i]);
+	}
+
+	return 0;
+}
+	
