6e6dfe05b29517c1c9c7b038b4a123794a0fb8e4
thomask <devnull@localhost>
2005-05-20 10:08:23 +0000

    Ben Hinkle <ben.hinkle@gmail.com> 2005-05-20 news:d6j9fq$2qdd$1@digitaldaemon.com

--- run/s/struct_initialization_07_B.d
+++ run/s/struct_initialization_07_B.d
@@ -6,13 +6,12 @@
 // @date@	2005-05-20
 // @uri@	news:d6j9fq$2qdd$1@digitaldaemon.com
 
-module dstress.run.s.struct_initialization_07;
+module dstress.run.s.struct_initialization_07_B;
 
 struct Node {
 	Node* left,right,parent;
-	int color;
-	char[] key;
-	char[] value;
+	int key;
+	int value;
 }
 
 int main(){
@@ -20,7 +19,7 @@ int main(){
 	assert(x.left is null);
 	assert(x.right is null);
 	assert(x.parent is null);
-	assert(x.key.length == 0);
-	assert(x.value.length == 0);
+	assert(x.key == 0);
+	assert(x.value == 0);
 	return 0;
 }
