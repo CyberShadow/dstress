b069c5c503857585c2e807b4f2583bddfff2384c
thomask <devnull@localhost>
2005-06-01 11:58:48 +0000

    -inline calling wrong virtual function
    Vathix <vathix@dprogramming.com>
    2005-05-22
    news:op.sq4gt8ddkcck4r@esi

---
+++ run/i/inline_10_A.d
@@ -0,0 +1,36 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Vathix <vathix@dprogramming.com>
+// @date@	2005-05-22
+// @uri@	news:op.sq4gt8ddkcck4r@esi
+// @desc@	-inline calling wrong virtual function
+
+module dstress.run.i.inline_10_A;
+
+class Base{
+	int test(){
+		return 1;
+	}
+}
+
+class Derived : Base{
+	int test(){
+		return 2;
+	}
+
+	package int baseTest(){
+		return super.test();
+	}
+}
+
+int main(){
+	Base b = new Base();
+	assert(b.test()==1);
+
+	Derived f = new Derived();
+	assert(f.test()==2);
+	assert(f.baseTest()==1);
+	return 0;
+}
