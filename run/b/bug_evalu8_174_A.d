269d906b158737e4f101d77540cdc2ab915385f6
thomask <devnull@localhost>
2005-06-01 12:34:19 +0000

    Internal error: ../ztc/evalu8.c 174
    Nick <Nick_member@pathlink.com>
    2005-05-25
    news:d72dg6$40l$1@digitaldaemon.com

---
+++ run/b/bug_evalu8_174_A.d
@@ -0,0 +1,24 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Nick <Nick_member@pathlink.com>
+// @date@	2005-05-25
+// @uri@	news:d72dg6$40l$1@digitaldaemon.com
+// @desc@	Internal error: ../ztc/evalu8.c 174
+
+module dstress.run.b.bug_evalu8_174_A;
+
+void foo(){
+	cdouble[] a;
+	cdouble[] b;
+	foreach(cdouble d; b){
+		d = -a[0];
+	}
+}
+
+int main(){
+	foo();
+	return 0;
+}
+
---
+++ run/b/bug_evalu8_174_D.d
@@ -0,0 +1,26 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Nick <Nick_member@pathlink.com>
+// @date@	2005-05-25
+// @uri@	news:d72dg6$40l$1@digitaldaemon.com
+// @desc@	Internal error: ../ztc/evalu8.c 174
+
+// __DSTRESS_DFLAGS__ -release -O
+
+module dstress.run.b.bug_evalu8_174_D;
+
+void foo(){
+	cdouble[] a;
+	cdouble[] b;
+	foreach(cdouble d; b){
+		d = -a[0];
+	}
+}
+
+int main(){
+	foo();
+	return 0;
+}
+
