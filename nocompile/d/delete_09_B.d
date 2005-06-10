c18d4d737a6108d18c0e53438821e9a17025265e
thomask <devnull@localhost>
2005-06-10 19:10:21 +0000

    undetected deprecated "delete"
    David L. Davis <SpottedTiger@yahoo.com>
    2005-06-08
    news:d87vbn$2tnm$1@digitaldaemon.com

---
+++ nocompile/d/delete_09_B.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	David L. Davis <SpottedTiger@yahoo.com>
+// @date@	2005-06-08
+// @uri@	news:d87vbn$2tnm$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 16
+
+module dstress.nocompile.d.delete_09_B;
+
+alias char string;
+ 
+void test(string[int] arr, int key ){
+	delete arr[ key ];
+} 
