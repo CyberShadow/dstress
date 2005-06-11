3e79e3ecc1a90cb62fd5b34f913f448ac0d60397
thomask <devnull@localhost>
2005-06-11 16:25:27 +0000

    catchup 1/3

---
+++ run/o/odd_bug_03_C.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @desc@	vararg / char[] / foreach code generation bug
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-05-29
+// @uri@	news:d7d2kq$1mc$1@digitaldaemon.com
+
+// @WARNING@ direct use of Phobos
+
+module dstress.run.o.odd_bug_03_C;
+
+import std.format;
+
+void f(...){
+	char[] s;
+
+	void putc(dchar c){
+		s ~= c;       
+	}
+    
+	std.format.doFormat(&putc, _arguments, _argptr);
+    
+	assert(s=="mystery");
+}
+
+int main(){
+	f("mystery");
+	return 0;
+} 
