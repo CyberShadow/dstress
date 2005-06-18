9e18fe167d019cbdb4d079e34fe5a00410e5644e
thomask <devnull@localhost>
2005-06-18 08:27:57 +0000

    misplaced initialisers in static rectangular array
    Stewart Gordon <smjg_1998@yahoo.com>
    2005-06-14
    news:d8m7rs$mtv$2@digitaldaemon.com

---
+++ run/a/array_initialization_17_A.d
@@ -0,0 +1,33 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	20005-06-14
+// @uri@	news:d8m7rs$mtv$2@digitaldaemon.com
+// @desc@	misplaced initialisers in static rectangular array
+
+module dstress.run.a.array_initialization_17_A;
+
+const char[3][13] month = [
+	1: "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+	8: "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+
+int main(){	
+	assert(month[1]=="Jan");
+	assert(month[6]=="Jun");
+	assert(month[8]=="Aug");
+	assert(month[12]=="Dec");
+
+	foreach(char c; month[0]){
+		assert(c==char.init);
+	}
+
+	foreach(char c; month[7]){
+		assert(c==char.init);
+	}
+
+	return 0;
+}
---
+++ run/a/array_initialization_17_C.d
@@ -0,0 +1,40 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	20005-06-14
+// @uri@	news:d8m7rs$mtv$2@digitaldaemon.com
+// @desc@	misplaced initialisers in static rectangular array
+
+module dstress.run.a.array_initialization_17_C;
+
+const cfloat[6] arr = [
+	1: 1.0f, 2.0f,
+	4: 4.0f
+];
+
+bit equal(cfloat a, cfloat b){
+	byte* aa = cast(byte*) &a;
+	byte* bb = cast(byte*) &b;
+
+	for(int index=0; index<a.sizeof; index++){
+		if(*aa != *bb)
+			return false;
+		aa++;
+		bb++;
+	}
+
+	return true;
+}
+
+int main(){	
+	assert(equal(arr[0], cfloat.nan));
+	assert(arr[1]==1.0f);
+	assert(arr[2]==2.0f);
+	assert(equal(arr[3], cfloat.nan));
+	assert(arr[4]==4.0f);
+	assert(equal(arr[5], cfloat.nan));
+
+	return 0;
+}
