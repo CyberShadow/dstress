65b062fcfffa6ea6e863ce7876b1d0e7df01c7a0
thomask <devnull@localhost>
2005-06-22 11:47:16 +0000

    arse.c:4119: Expression* Parser::parseUnaryExp(): Assertion `e' failed.
    Stewart Gordon <smjg_1998@yahoo.com>
    2005-06-22
    news:d9bap2$1rrp$1@digitaldaemon.com

---
+++ nocompile/i/is_01_B.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com> 
+// @date@	2005-06-22
+// @uri@	news:d9bap2$1rrp$1@digitaldaemon.com
+// @desc@	parse.c:4119: Expression* Parser::parseUnaryExp(): Assertion `e' failed.
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.i.is_01_B;
+
+void main() {
+     is
+}
+
