153bad88390fa914051728ab58eea4c511c4f420
thomask <devnull@localhost>
2005-06-22 11:23:07 +0000

    reclasification

--- run/o/opCat_07_A.d
+++ run/o/opCat_07_A.d
@@ -7,11 +7,10 @@
 // @uri@	news:opsrb0xvou23k2f5@nrage.netwin.co.nz
 // @desc@	Internal error: e2ir.c 488 (char 00914B24)  (0x811ad08)
 
-// __DSTRESS_ELINE__ 19
-
-module dstress.nocompile.o.opCat_07_A;
+module dstress.run.o.opCat_07_A;
 
 void foo(char[] s){
+	assert(s=="is this it? yep!")
 }
 
 int main(){
--- run/o/opCat_07_B.d
+++ run/o/opCat_07_B.d
@@ -7,13 +7,14 @@
 // @uri@	news:opsrb0xvou23k2f5@nrage.netwin.co.nz
 // @desc@	Internal error: e2ir.c 488 (char 00914B24)  (0x811ad08)
 
-// __DSTRESS_ELINE__ 17
-
-module dstress.nocompile.o.opCat_07_B;
+module dstress.run.o.opCat_07_B;
 
 int main(){
 	char c = '?';
-	char[] s;
+	char[] s="a";
 	s = s ~ c ~ s;
+
+	assert(s=="a?a");
+
 	return 0;
 }
