ca882258db816c7476f816447aab75f376e16d3c
thomask <devnull@localhost>
2005-06-23 03:53:25 +0000

    John Demme <me@teqdruid.com> 2005-06-07 news:1118118941.10321.21.camel@localhost.localdomain

---
+++ nocompile/a/abstract_14.d
@@ -0,0 +1,24 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	John Demme <me@teqdruid.com>
+// @date@	2005-06-07
+// @uri@	news:1118118941.10321.21.camel@localhost.localdomain
+
+// :23 is tested in abstract_08
+
+// __DSTRESS_ELINE__ 16
+
+module dstress.nocompile.a.abstract_14;
+
+class A{
+	abstract void test();
+}
+
+class B : A{
+}
+
+void main(){
+	B b = new B;
+}
