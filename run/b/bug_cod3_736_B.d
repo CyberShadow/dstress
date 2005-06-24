c5a29e7e0467ca1839ae362a99261b1318d973d3
thomask <devnull@localhost>
2005-06-24 08:28:49 +0000

    Victor Nakoryakov <nail-mail@mail.ru> 2005-06-23 news:d9erom$2roj$1@digitaldaemon.com

---
+++ run/b/bug_cod3_736_B.d
@@ -0,0 +1,40 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Victor Nakoryakov <nail-mail@mail.ru>
+// @date@	2005-06-23
+// @uri@	news:d9erom$2roj$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ -inline
+
+module dstress.run.b.bug_cod3_736_B;
+
+struct Struct{
+
+	int id;
+
+	real foo(){
+		return 0;
+	}
+	
+	void bar(out Struct Q){
+		if (foo < 0){
+			Q = *this;
+		}			
+	}
+}
+
+int main(){
+	Struct s;
+	s.id=1;
+
+	Struct ss;
+	ss.id=2;
+
+	s.bar(ss);
+	assert(ss.id==0);
+	
+	return 0;	
+}
+
