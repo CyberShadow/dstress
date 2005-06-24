c75071a937e99536f3bcfdb1d8e25fcd4dd1183d
thomask <devnull@localhost>
2005-06-24 08:18:37 +0000

    -inline / opMul
    Victor Nakoryakov <nail-mail@mail.ru>
    2005-06-23
    news:d9eouv$2okr$1@digitaldaemon.com

---
+++ run/i/inline_12_A.d
@@ -0,0 +1,39 @@
+// $HeadURI$
+// $Date$
+// $Author$
+
+// @author@	Victor Nakoryakov <nail-mail@mail.ru>
+// @date@	2005-06-23
+// @uri@	news:d9eouv$2okr$1@digitaldaemon.com
+
+module dstress.run.i.inline_12_A;
+
+struct Struct{
+	int opMul(int x){
+		return x+1;
+	}
+}
+
+class Class{
+	int opMul(int f){
+		return (foo * f) + 3;
+	}
+	
+	Struct foo(){
+		Struct s;
+		return s;
+	}
+}
+
+import std.stdio;
+int main(){
+	Struct s;
+	assert(s * 1 == 2);
+
+	Class c = new Class;
+	assert(c * 1 == 5);	
+	
+	return 0;	
+}
+
+
---
+++ run/i/inline_12_C.d
@@ -0,0 +1,37 @@
+// $HeadURI$
+// $Date$
+// $Author$
+
+// @author@	Victor Nakoryakov <nail-mail@mail.ru>
+// @date@	2005-06-23
+// @uri@	news:d9eouv$2okr$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ -inline
+
+module dstress.run.i.inline_12_C;
+
+struct Struct{
+	int opMul(int x){
+		return x+1;
+	}
+}
+
+class Class{
+	int opMul(int f){
+		Struct foo;
+		return (foo * f) + 3;
+	}
+}
+
+import std.stdio;
+int main(){
+	Struct s;
+	assert(s * 1 == 2);
+
+	Class c = new Class;
+	assert(c * 1 == 5);	
+	
+	return 0;	
+}
+
+
