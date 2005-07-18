e7c86ea67e7c84cbb7304913117de219ba0d3439
thomask <devnull@localhost>
2005-07-18 22:57:06 +0000

    fixed file name typo

---
+++ norun/r/return_07_A.d
@@ -0,0 +1,19 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Vathix <chris@dprogramming.com>
+// @date@	2005-07-14
+// @uri@	news:op.stwu8jtdl2lsvj@esi
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.norun.r.return_07_A;
+
+int foo(){
+}
+
+int main(){
+	foo();
+	return 0;
+}
---
+++ norun/r/return_07_B.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Vathix <chris@dprogramming.com>
+// @date@	2005-07-14
+// @uri@	news:op.stwu8jtdl2lsvj@esi
+
+// __DSTRESS_DFLAGS__ -inline
+// __DSTRESS_ELINE__ 15
+
+module dstress.norun.r.return_07_B;
+
+int foo(){
+}
+
+int main(){
+	foo();
+	return 0;
+}
