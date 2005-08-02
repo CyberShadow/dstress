0a7cb64e145c1be2726a203a628dd9e8d85f0d34
thomask <devnull@localhost>
2005-08-02 22:44:10 +0000

    zwang <nehzgnaw@gmail.com> 2005-08-01 news:dckqqq$24nd$2@digitaldaemon.com

---
+++ nocompile/d/deprecated_20.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Autor$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-08-01
+// @uri@	news:dckqqq$24nd$2@digitaldaemon.com
+
+module dstress.nocompile.d.deprecated_20;
+
+deprecated int main(char[][] args){
+	return 0;
+} 
