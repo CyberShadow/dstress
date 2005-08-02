7158865a915c47cb02f67857294d01d80b4e995e
thomask <devnull@localhost>
2005-08-02 22:46:24 +0000

    Sean Kelly <sean@f4.ca> 2005-07-31 news:dciqn7$73f$1@digitaldaemon.com

---
+++ run/v/volatile_01_B.d
@@ -0,0 +1,26 @@
+// $HeadURL$
+// $Date$
+// $Autor$
+
+// @author@	Sean Kelly <sean@f4.ca>
+// @date@	2005-07-31
+// @uri@	news:dciqn7$73f$1@digitaldaemon.com
+	
+module dstress.run.v.volatile_01_B;
+
+template load( T, int i, size_t sizeofT : 4 = T.sizeof ){
+	T load( inout T val ){
+		asm{
+			naked;
+			mov EAX, [EAX];
+			ret;
+		}
+	}
+}
+
+int main(){
+	int i;
+	load!(int,1)(i);
+	load!(int,2)(i);
+	return 0;
+}
