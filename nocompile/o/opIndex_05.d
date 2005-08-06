c4cc080d7d44da7ab1da55643a739fee0b89f2ea
thomask <devnull@localhost>
2005-08-06 14:35:50 +0000

    zwang <nehzgnaw@gmail.com> 2005-08-04 news:dctjfi$kkv$1@digitaldaemon.com

---
+++ nocompile/o/opIndex_05.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-08-04
+// @uri@	news:dctjfi$kkv$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__  15
+
+module dtsress.nocompile.o.opIndex_05;
+
+void main(){
+	int[
+		0.128
+	] a;
+} 
