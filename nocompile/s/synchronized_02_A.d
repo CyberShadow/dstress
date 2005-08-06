8f40f82eb338c110fed712699ba5d233a3983f5e
thomask <devnull@localhost>
2005-08-06 14:34:54 +0000

    Sean Kelly <sean@f4.ca> 2005-08-05 news:dd0p34$a82$1@digitaldaemon.com

---
+++ nocompile/s/synchronized_02_A.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Sean Kelly <sean@f4.ca>
+// @date@	2005-08-05
+// @uri@	news:dd0p34$a82$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__  15
+
+module dtsress.nocompile.s.synchronized_02_A;
+
+void main(){
+	synchronized(
+			foo
+		)
+	{
+		// blah
+	}
+}
