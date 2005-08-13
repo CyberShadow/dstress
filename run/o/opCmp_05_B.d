3f2d7471713cddd187e3276cf1899474cbd5256a
thomask <devnull@localhost>
2005-08-13 20:54:43 +0000

    Negative numbers are treated as Positive in compares.
    David L. Davis <SpottedTiger@yahoo.com>
    2005-08-10
    news:dddj2k$lrr$1@digitaldaemon.com

---
+++ run/o/opCmp_05_B.d
@@ -0,0 +1,35 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	David L. Davis <SpottedTiger@yahoo.com>
+// @date@	2005-08-10
+// @uri@	news:dddj2k$lrr$1@digitaldaemon.com
+// @desc@	Negative numbers are treated as Positive in compares.
+
+
+module dstress.run.o.opCmp_05_B;
+
+int main(){
+	if(-128<0){
+		assert(1);
+	}else{
+		assert(0);
+	}
+
+	if(-128<=0){
+		assert(1);
+	}else{
+		assert(0);
+	}
+	
+	if(-128>0){
+		assert(0);
+	}
+	
+	if(-128>=0){
+		assert(0);
+	}
+	
+	return 0;
+}
---
+++ run/o/opCmp_05_C.d
@@ -0,0 +1,34 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	David L. Davis <SpottedTiger@yahoo.com>
+// @date@	2005-08-10
+// @uri@	news:dddj2k$lrr$1@digitaldaemon.com
+// @desc@	Negative numbers are treated as Positive in compares.
+
+module dstress.run.o.opCmp_05_C;
+
+int main(){
+	static if(-128<0){
+		assert(1);
+	}else{
+		assert(0);
+	}
+
+	static if(-128<=0){
+		assert(1);
+	}else{
+		assert(0);
+	}
+	
+	static if(-128>0){
+		assert(0);
+	}
+	
+	static if(-128>=0){
+		assert(0);
+	}
+	
+	return 0;
+}
