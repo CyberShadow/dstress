1b5dfc80b38eeb4d85c2c58b71cdaaa7308ba714
thomask <devnull@localhost>
2005-08-13 21:35:40 +0000

    foreach doesn't work when accessing elements as supertypes
    xs0 <xs0@xs0.com>
    2005-08-10
    news:dddlpj$q05$1@digitaldaemon.com

---
+++ run/c/cast_29_A.d
@@ -0,0 +1,27 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	xs0 <xs0@xs0.com>
+// @date@	2005-08-10
+// @uri@	news:dddlpj$q05$1@digitaldaemon.com
+// @desc@	foreach doesn't work when accessing elements as supertypes
+
+module dstress.run.c.cast_29_A;
+
+class Foo{
+}
+
+class Bar : Foo{
+}
+
+int main(){
+	Bar[] bars=new Bar[100];
+	Foo[] foos=bars;
+	
+	foreach(Foo f; foos){
+		return 0;
+	}
+	
+	assert(0);
+} 
---
+++ run/c/cast_29_B.d
@@ -0,0 +1,26 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	xs0 <xs0@xs0.com>
+// @date@	2005-08-10
+// @uri@	news:dddlpj$q05$1@digitaldaemon.com
+// @desc@	foreach doesn't work when accessing elements as supertypes
+
+module dstress.run.c.cast_29_B;
+
+class Foo{
+}
+
+class Bar : Foo{
+}
+
+int main(){
+	Bar[] bars=new Bar[100];
+	
+	foreach(Foo f; bars){
+		return 0;
+	}
+	
+	assert(0);
+} 
