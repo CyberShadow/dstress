4ba136fb8db4db26fff5f13a1c466a30018f563c
thomask <devnull@localhost>
2005-08-17 21:28:25 +0000

    type x = void;
    Thomas Kühne <thomas-dloop@kuehne.THISISSPAM.cn>
    2005-08-17

---
+++ nocompile/v/void_03_J.d
@@ -0,0 +1,12 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kühne <thomas-dloop@kuehne.THISISSPAM.cn>
+// @date@	2005-08-17
+
+// __DSTRESS_ELINE__ 12
+
+module dstress.nocompile.v.void_03_J;
+
+float x = void;
---
+++ nocompile/v/void_03_W.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kühne <thomas-dloop@kuehne.THISISSPAM.cn>
+// @date@	2005-08-17
+
+// __DSTRESS_ELINE__ 16
+
+module dstress.nocompile.v.void_03_W;
+
+enum Enum{
+	A, B
+}
+
+Enum x = void;
