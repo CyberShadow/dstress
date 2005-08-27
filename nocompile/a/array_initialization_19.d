b44ae89ef11576d0a233d19c90f9c1fa41686127
thomask <devnull@localhost>
2005-08-27 18:48:51 +0000

    Stewart Gordon <smjg_1998@yahoo.com> 2005-08-20 news:de6npa$l97$1@digitaldaemon.com

---
+++ nocompile/a/array_initialization_19.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2005-08-20
+// @uri@	news:de6npa$l97$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.a.array_initialization_19;
+
+void main(){
+	void[] data;
+	data.length=1;
+} 
