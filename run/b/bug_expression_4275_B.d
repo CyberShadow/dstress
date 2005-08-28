b7aa22bfd69d20ce1a1b727f0e1fffd6316bdb5a
thomask <devnull@localhost>
2005-08-28 09:50:54 +0000

    Victor Nakoryakov <nail-mail@mail.ru> 2005-07-07 news:daj84k$15p4$1@digitaldaemon.com

---
+++ run/b/bug_expression_4275_B.d
@@ -0,0 +1,37 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Victor Nakoryakov <nail-mail@mail.ru>
+// @date@	2005-07-07
+// @uri@	news:daj84k$15p4$1@digitaldaemon.com
+
+module dstress.run.b.bug_expression_4275_B;
+
+class MyClass{
+	template foo(T){
+		T[] foo(){
+			T[] x;
+			return x;
+		}
+	}
+	
+	int dummy;
+}
+
+class Container{
+	MyClass element(){
+		MyClass x = new MyClass;
+		return x;
+	}
+}
+
+int main(){
+	Container container = new Container();
+	MyClass myclass = new MyClass();
+    
+	assert( container.element().foo!(MyClass).sizeof == (MyClass[]).sizeof );
+	assert( myclass.foo!(MyClass).length == 0 || myclass.foo!(MyClass)[0] is myclass );
+
+	return 0;
+}
