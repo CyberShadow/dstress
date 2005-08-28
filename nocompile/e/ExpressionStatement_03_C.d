3e561d960f883434c38e2cb87cd3ec714e3b57d9
thomask <devnull@localhost>
2005-08-28 09:49:56 +0000

    Stewart Gordon <smjg_1998@yahoo.com> 2005-08-23 news:deeu1o$5tm$2@digitaldaemon.com

---
+++ nocompile/e/ExpressionStatement_03_C.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2005-08-23
+// @uri@	news:deeu1o$5tm$2@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.e.ExpressionStatement_03_C;
+
+void main(){
+	int x;
+	x * 5;
+} 
---
+++ nocompile/e/ExpressionStatement_05.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2005-08-23
+// @uri@	news:deeu1o$5tm$2@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.e.ExpressionStatement_05;
+
+void main(){
+	"Hallo";
+} 
