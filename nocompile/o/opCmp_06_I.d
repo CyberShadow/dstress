ad694480a42a39b2aff3729bb095212ebb4f0a53
thomask <devnull@localhost>
2005-09-07 22:34:23 +0000

    <jmjmak@utu.invalid.fi> 2005-08-31 news:df4t72$27kf$1@digitaldaemon.com

---
+++ nocompile/o/opCmp_06_I.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<jmjmak@utu.invalid.fi>
+// @date@	2005-08-31
+// @uri@	news:df4t72$27kf$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 27
+
+module dstress.nocompile.o.opCmp_06_I;
+
+interface Interface{
+	int test(int);
+}
+
+class Class : Interface{
+	int test(int i){
+		return ++i;
+	}
+}
+
+void main(){
+	Interface a = new Class();
+	Interface b = new Class();
+
+	assert(a <> b);
+}
