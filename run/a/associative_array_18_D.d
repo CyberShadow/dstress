4fe7d2c45b70db96dc7467aa9578ffb0a32cfd59
thomask <devnull@localhost>
2005-09-07 10:34:35 +0000

    Bastiaan Veelo <Bastiaan.N.Veelo@ntnu.no> 2005-09-04 news:dffjik$2p30$1@digitaldaemon.com

---
+++ run/a/associative_array_18_D.d
@@ -0,0 +1,29 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Bastiaan Veelo <Bastiaan.N.Veelo@ntnu.no>
+// @date@	2005-09-04
+// @uri@	news:dffjik$2p30$1@digitaldaemon.com
+
+module dstress.run.a.associative_array_18_D;
+
+interface Interface{
+	int test(int);
+}
+
+class Class : Interface{
+	int test(int i){
+		return i+1;
+	}
+}
+
+int main(){
+	Class[Class] aa;
+
+	Class o = new Class();
+
+	aa[o] = o;
+
+	return 0; 
+}
