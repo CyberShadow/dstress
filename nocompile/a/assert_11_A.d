6168bffbe6b4472c3062f3dde837c14a9b5bb09c
thomask <devnull@localhost>
2005-09-07 22:06:40 +0000

    tetsuya <tetsuya_member@pathlink.com> 2005-09-02 news:df86a5$248l$1@digitaldaemon.com

---
+++ nocompile/a/assert_11_A.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	tetsuya <tetsuya_member@pathlink.com>
+// @date@	2005-09-02
+// @uri@	news:df86a5$248l$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ 14
+
+module dstress.nocompile.a.assert_11_A;
+
+void main(){
+	static assert(-1 > 0);
+}
