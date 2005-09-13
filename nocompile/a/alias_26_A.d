e20b9efdebd2e1dfc0b8f106cfca14b2fb9d3b8b
thomask <devnull@localhost>
2005-09-13 22:48:23 +0000

    zwang <nehzgnaw@gmail.com> 2005-09-13 news:dg6s40$1ihf$1@digitaldaemon.com

---
+++ nocompile/a/alias_26_A.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-09-13
+// @uri@	news:dg6s40$1ihf$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.a.alias_26_A;
+
+alias main a;
+alias void a;
+
+void main(){
+    a;
+} 
