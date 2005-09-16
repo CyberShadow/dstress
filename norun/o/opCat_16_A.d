d09b576bdbedce4ae4bfc2a6eb81fc9658e7b38a
thomask <devnull@localhost>
2005-09-16 17:20:58 +0000

    zwang <nehzgnaw@gmail.com> 2005-09-15 news:dgap19$2lhi$1@digitaldaemon.com

---
+++ norun/o/opCat_16_A.d
@@ -0,0 +1,30 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-09-15
+// @uri@	news:dgap19$2lhi$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 29
+
+module dstress.norun.o.opCat_16_A;
+
+bool called;
+
+void test(wchar[] w){
+	assert(w.length==4);
+	assert(w[0]=='b');
+	assert(w[1]=='u');
+	assert(w[2]=='g');
+	assert(w[3]=='s');
+	called = true;
+}
+
+void main(){
+	const wchar[] S = "bug"w;
+	assert(!called);
+	test(S ~ "s"w);
+	assert(called);
+	assert(0);
+}
