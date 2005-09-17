94d0a13265e1a6e98037731842ae891768555e75
thomask <devnull@localhost>
2005-09-17 13:02:29 +0000

    zwang <nehzgnaw@gmail.com> 2005-09-17 news:dggsko$pi$1@digitaldaemon.com

---
+++ addon/import_08_Z.d
@@ -0,0 +1,9 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+module addon.import_08_Z;
+
+int test(int i){
+	return i+1;
+}
---
+++ run/i/import_08_C.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-09-17
+// @uri@	news:dggsko$pi$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGSE__ addon/import_08_Z.d
+
+module dstress.run.i.import_08_C;
+
+import addon.import_08_Z;
+
+class C{
+}
+
+int main(){
+	int x;
+	
+	with(new C){
+		x = addon.import_08_Z.test(1);
+	}
+	
+	assert(x==2);
+	
+	return 0;
+}
