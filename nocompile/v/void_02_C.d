54e40f1ef74f4014ec64e001bef2cea3f199dcd3
thomask <devnull@localhost>
2005-09-20 08:58:40 +0000

    zwang <nehzgnaw@gmail.com> 2005-09-20 news:dgoa9j$bsv$1@digitaldaemon.com

---
+++ nocompile/v/void_02_C.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	zwang <nehzgnaw@gmail.com>
+// @date@	2005-09-20
+// @uri@	news:dgoa9j$bsv$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.v.void_02_C;
+
+void main(){
+	void[1] v;
+} 
