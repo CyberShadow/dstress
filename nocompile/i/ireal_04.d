ef2d84cc9e82e3dae2e92ff5346b9b91a8a10965
thomask <devnull@localhost>
2005-09-28 20:14:48 +0000

    Thomas Kuehne <thomas-dloop@kuehne.cn> 2005-09-28 news:dhetn8$jgi$1@digitaldaemon.com

---
+++ nocompile/i/ireal_04.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2005-09-28
+// @uri@	news:dhetn8$jgi$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.i.ireal_04;
+
+void main(){
+	ireal i = 3;
+}
+
