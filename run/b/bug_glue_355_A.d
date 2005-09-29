0bcb0ba0b888d0082846953a1426051e19aded41
thomask <devnull@localhost>
2005-09-29 06:02:12 +0000

    John C <johnch_atms@hotmail.com> 2005-09-29 news:dhf4c3$pua$1@digitaldaemon.com

---
+++ run/b/bug_glue_355_A.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	John C <johnch_atms@hotmail.com>
+// @date@	2005-09-29
+// @uri@	news:dhf4c3$pua$1@digitaldaemon.com
+
+module dstress.run.b.bug_glue_355_A;
+
+class Foo {
+    this(int[] params ...) {
+    }
+}
+
+class Bar {
+
+    private this() {
+        init();
+    }
+
+    private void init() {
+        Foo f = new Foo(1, 2, 3);
+    }
+
+}
+
+int main(){
+	Bar b = new Bar();
+	return 0;
+}
---
+++ run/b/bug_glue_355_C.d
@@ -0,0 +1,33 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	John C <johnch_atms@hotmail.com>
+// @date@	2005-09-29
+// @uri@	news:dhf4c3$pua$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ -inline
+
+module dstress.run.b.bug_glue_355_C;
+
+class Foo {
+    this(int[] params ...) {
+    }
+}
+
+class Bar {
+
+    private this() {
+        init();
+    }
+
+    void init() {
+        Foo f = new Foo(1, 2, 3);
+    }
+
+}
+
+int main(){
+	Bar b = new Bar();
+	return 0;
+}
