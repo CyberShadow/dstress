6a148351f99194c79a8b5d2b6cedf877f2545498
thomask <devnull@localhost>
2005-10-02 20:51:18 +0000

    John C <johnch_atms@hotmail.com> 2005-09-30 news:dhj6pt$1crc$1@digitaldaemon.com

---
+++ run/i/inline_15_A.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	John C <johnch_atms@hotmail.com>
+// @date@	2005-09-30
+// @uri@	news:dhj6pt$1crc$1@digitaldaemon.com
+
+module dstress.run.i.inline_15_A;
+
+int[] toArray(int[] params ...) {
+        return params.dup;
+}
+
+int[] values;
+
+void ensure() {
+	values = toArray(1, 2, 3);
+}
+
+int main(){
+	ensure();
+	assert(values.length==3);
+	assert(values[0]==1);
+	assert(values[1]==2);
+	assert(values[2]==3);
+	return 0;
+}
