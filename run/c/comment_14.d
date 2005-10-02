77401df3fd8409c01ed37797af2dc366e462711d
thomask <devnull@localhost>
2005-10-02 20:50:44 +0000

    clayasaurus <clayasaurus@gmail.com> 2005-10-02 news:dhpbkg$h0f$1@digitaldaemon.com

---
+++ run/c/comment_14.d
@@ -0,0 +1,21 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	clayasaurus <clayasaurus@gmail.com>
+// @date@	2005-10-02
+// @uri@	news:dhpbkg$h0f$1@digitaldaemon.com
+
+module dstress.run.c.comment_14;
+
+/**
+	a
+*/
+enum{
+	X
+}
+
+int main(){
+	assert(X==0);
+	return 0;
+}
