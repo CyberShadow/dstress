ce901de12b7d0116d14bffeab62f77011a2096bd
thomask <devnull@localhost>
2005-10-16 12:44:05 +0000

    extended invalid "static if" tests

---
+++ nocompile/s/static_if_01_E.d
@@ -0,0 +1,9 @@
+// $HeadURL: svn://dstress.kuehne.cn/nocompile/s/static_if_01_C.d $
+// $Date: 2005-05-21 21:32:38 +0200 (Sat, 21 May 2005) $
+// $Author: thomask $
+
+// __DSTRESS_ELINE__ 9
+
+module dstress.nocompile.s.static_if_01_E;
+
+static if ()
