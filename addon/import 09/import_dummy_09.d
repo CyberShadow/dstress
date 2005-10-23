23e188e31150ef5f29e9db935e1ef40d36ff1f44
thomask <devnull@localhost>
2005-10-23 11:45:46 +0000

    Eugene Pelekhay <pelekhay@gmail.com> news:dj5nic$24r7$1@digitaldaemon.com 2005-10-19

---
+++ addon/import 09/import_dummy_09.d
@@ -0,0 +1,9 @@
+// $HeadURL: svn://dstress.kuehne.cn/addon/import_01_dummy.d $
+// $Date: 2005-04-06 10:12:29 +0200 (Wed, 06 Apr 2005) $
+// $Author: thomask $
+
+module import_dummy_09;
+
+byte test(byte b){
+	return b+1;
+}
---
+++ run/i/import_09.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Eugene Pelekhay <pelekhay@gmail.com>
+// @uri@	news:dj5nic$24r7$1@digitaldaemon.com
+// @date@	2005-10-19
+
+// __DSTRESS_ELINE__ "-Iaddon/import 09" "addon/import 09/import_dummy_09.d"
+
+module dstress.run.i.import_09;
+
+import import_dummy_09;
+
+int main(){
+	assert(2==test(2));
+	return 0;
+}
