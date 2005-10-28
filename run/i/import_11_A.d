5860ebbff58cd2a457dc22e9aed45f1a7b5eee72
thomask <devnull@localhost>
2005-10-28 21:27:15 +0000

    Stewart Gordon <smjg_1998@yahoo.com> 2005-10-26 news:djnicb$1a5t$1@digitaldaemon.com

---
+++ run/i/import_11_A.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2005-10-26
+// @uri@	news:djnicb$1a5t$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ run/i/import_11_B.d
+
+module /*dstress.*/run.i.import_11_A;
+import /*dstress.*/run.i.import_11_B;
+
+int array[Enum.max + 1];
+
+int main(){
+	assert(array.length == 3);
+	return 0;
+}
+
---
+++ run/i/import_11_B.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2005-10-26
+// @uri@	djnicb$1a5t$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ run/i/import_11_A.d
+
+module /*dstress.*/run.i.import_11_B;
+import /*dstress.*/run.i.import_11_A;
+
+enum Enum {
+	A = -1,
+	B = 2
+}
