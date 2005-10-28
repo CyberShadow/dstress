ac4344c8b6f5b4d5588204e46afd5fa08985a093
thomask <devnull@localhost>
2005-10-28 21:27:43 +0000

    Stewart Gordon <smjg_1998@yahoo.com> 2005-10-26 news:djnie3$1a5t$2@digitaldaemon.com

---
+++ run/i/import_10_A.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2005-10-26
+// @uri@	news:djnie3$1a5t$2@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ run/i/import_10_B.d
+
+module /*dstress.*/run.i.import_10_A;
+import /*dstress.*/run.i.import_10_B;
+
+const uint len = 42;
+
+int main(){
+	assert(array.length == 42);
+	return 0;
+}
+
---
+++ run/i/import_10_B.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg_1998@yahoo.com>
+// @date@	2005-10-26
+// @uri@	news:djnie3$1a5t$2@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ run/i/import_10_A.d
+
+module /*dstress.*/run.i.import_10_B;
+import /*dstress.*/run.i.import_10_A;
+
+int[len] array;
