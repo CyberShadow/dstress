88924fa21e693494b925595c4f2e822ba95726f2
thomask <devnull@localhost>
2005-11-05 08:29:03 +0000

    Thomas Kuehne <thomas-dloop@kuehne.cn> 2005-11-04 news:th-92A9AC784C1CE3592F06EC49@birke.kuehne.cn

---
+++ run/e/enum_41_B.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2005-11-04
+// @uri@	news:th-92A9AC784C1CE3592F06EC49@birke.kuehne.cn
+
+module dstress.run.e.enum_41_B;
+
+enum E : ubyte{
+	A = 1,
+	B = 2,
+	C = 3
+}
+
+int main(){
+	ubyte[] array;
+	
+	E e = E.B;
+	
+	array = array ~ e;
+	
+	assert(array.length==1);
+	assert(array[0]==E.B);
+	
+	return 0;
+}
---
+++ run/e/enum_41_D.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2005-11-04
+// @uri@	news:th-92A9AC784C1CE3592F06EC49@birke.kuehne.cn
+
+module dstress.run.e.enum_41_D;
+
+enum E : ushort{
+	A = 1,
+	B = 2,
+	C = 3
+}
+
+int main(){
+	ushort[] array;
+	
+	E e = E.B;
+	
+	array = array ~ e;
+	
+	assert(array.length==1);
+	assert(array[0]==E.B);
+	
+	return 0;
+}
