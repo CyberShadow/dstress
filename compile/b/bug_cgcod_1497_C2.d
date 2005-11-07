ca910ed4374e719e4a1655a4ab8b8d143c6b77a7
thomask <devnull@localhost>
2005-11-07 01:05:06 +0000

    MicroWizard <MicroWizard_member@pathlink.com> 2005-11-04 news:dkgkic$2v7n$1@digitaldaemon.com

---
+++ compile/b/bug_cgcod_1497_C2.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	MicroWizard <MicroWizard_member@pathlink.com>
+// @date@	2005-11-04
+// @uri@	news:dkgkic$2v7n$1@digitaldaemon.com
+
+// __DSTRESS_DFLAGS__ -O -inline
+
+module dstress.compile.b.bug_cgcod_1497_C2;
+
+class C{
+	private void bug(char[] array){
+		bug(array[1 .. array.length]);
+	}
+}
