011de4f0be931e34e907d8670b3fdb72ae687c63
thomask <devnull@localhost>
2005-11-07 01:05:48 +0000

    Kris <du@bar.com> 2005-10-28 news:djtsj4$2aci$1@digitaldaemon.com

---
+++ run/s/static_34_B.d
@@ -0,0 +1,29 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kris <du@bar.com>
+// @date@	2005-10-28
+// @uri@	news:djtsj4$2aci$1@digitaldaemon.com
+
+module dstress.run.s.static_34_B;
+
+int main(){
+	static class C{
+		static C create(){
+			return new C;
+		}
+	}
+
+	C c1 = new C;
+	assert(c1);
+	
+	C c2 = c1.create();
+	assert(c2);
+
+	C c3 = C.create();
+	assert(c3);
+
+	return 0;
+}
+
