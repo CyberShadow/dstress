8ddc2102acc6be42ef4bdf4a2d5b142805e7d477
thomask <devnull@localhost>
2005-11-19 21:56:52 +0000

    Don Clugston <dac@nospam.com.au> 2005-11-14 news:dladdb$1vp4$1@digitaldaemon.com

---
+++ run/v/variadic_argument_10_B.d
@@ -0,0 +1,37 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kris <fu@bar.com>
+// @date@	2005-11-18
+// @uri@	news:dllsvr$2agc$1@digitaldaemon.com
+
+module dstress.run.v.variadic_argument_10_B;
+
+int state;
+
+class Bar{
+	this(...){
+	}
+}
+
+class Foo{
+	void opAdd(Bar b){
+		assert(b);
+		state++;
+	}
+}
+
+int main(){
+	Foo f = new Foo;
+	assert(state == 0);
+	assert(f);
+	
+	f + new Bar;
+
+	assert(state == 1);
+	assert(f);
+
+	return 0;
+}
+
---
+++ run/v/variadic_argument_10_C.d
@@ -0,0 +1,37 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kris <fu@bar.com>
+// @date@	2005-11-18
+// @uri@	news:dllsvr$2agc$1@digitaldaemon.com
+
+module dstress.run.v.variadic_argument_10_C;
+
+int state;
+
+class Bar{
+	this(int i, ...){
+	}
+}
+
+class Foo{
+	void opAdd(Bar b){
+		assert(b);
+		state++;
+	}
+}
+
+int main(){
+	Foo f = new Foo;
+	assert(state == 0);
+	assert(f);
+	
+	f + new Bar(1, 0);
+
+	assert(state == 1);
+	assert(f);
+
+	return 0;
+}
+
