4dc6bb3d899d4b05b6d3ab4af55a2ca1e736da8e
thomask <devnull@localhost>
2005-11-20 08:41:28 +0000

    Garett Bass <garettbass@studiotekne.com> 2005-11-07 news:dkp7gq$18ri$1@digitaldaemon.com

---
+++ run/t/template_19_D.d
@@ -0,0 +1,32 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Garett Bass <garettbass@studiotekne.com>
+// @date@	2005-11-07
+// @uri@	news:dkp7gq$18ri$1@digitaldaemon.com
+
+module dstress.run.t.template_19_D;
+
+template add(T) {
+	T add(T x, T y){
+		return x + y;
+	}
+}
+
+alias add!(int)   addX;
+alias add!(float) addX;
+
+int main(){
+	assert(addX(1, 2) == 3.0);
+	float f = addX(1.3, 2.4);
+	f -= 3.7;
+	if(f < 0.0){
+		f *= -1;
+	}
+
+	assert(f < f.epsilon * 4);
+
+	return 0;
+}
+
