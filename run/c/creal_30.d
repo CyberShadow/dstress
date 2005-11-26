2aa6cdd0e47a3ac8ae0b4e1c72176f8d1a714c27
thomask <devnull@localhost>
2005-11-26 21:44:48 +0000

    Tiago Gasiba <tiago.gasiba@gmail.com> 2005-11-24 news:dm3vu1$2vaf$1@digitaldaemon.com

---
+++ run/c/creal_30.d
@@ -0,0 +1,27 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Tiago Gasiba <tiago.gasiba@gmail.com>
+// @date@	2005-11-24
+// @uri@	news:dm3vu1$2vaf$1@digitaldaemon.com
+
+module dstress.run.c.creal_30;
+
+int main( ){
+	creal[] array;
+	array.length = 3;
+	array[0] = 1.0;
+	array[1] = 1.0i;
+	array[2] = 1.0 + 1.0i;
+
+	for(int i = 0; i < array.length; i++ ){
+		array[i] += -1.0i * array[i];
+	}
+
+	assert(array[0] == 1.0 - 1.0i);
+	assert(array[1] == 1.0 + 1.0i);
+	assert(array[2] == 2.0);
+
+	return 0;
+}
