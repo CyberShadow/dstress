603ef83d3d23ee882b2a6d5c136f3e4486fd40b1
thomask <devnull@localhost>
2005-12-04 00:04:21 +0000

    Sean Kelly <sean@f4.ca> 2005-12-02 news:dmquts$177g$1@digitaldaemon.com

---
+++ run/b/bug_glue_387_E.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Sean Kelly <sean@f4.ca>
+// @date@	2005-12-02
+// @uri@	news:dmquts$177g$1@digitaldaemon.com
+
+module dstress.run.b.bug_glue_387_E;
+
+template atomicStore( T )
+{
+     void atomicStore(T newval)
+     {
+         volatile asm
+         {
+             mov EAX, newval;
+         }
+     }
+}
+
+int main(){
+	atomicStore!(int)( 1 );
+	atomicStore!(uint)( 2 );
+
+	return 0;
+}
+
---
+++ run/b/bug_glue_387_H.d
@@ -0,0 +1,27 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Sean Kelly <sean@f4.ca>
+// @date@	2005-12-02
+// @uri@	news:dmquts$177g$1@digitaldaemon.com
+
+module dstress.run.b.bug_glue_387_H;
+
+template atomicStore( T )
+{
+     void atomicStore(T newval)
+     {
+         volatile asm
+         {
+             mov EAX, newval;
+         }
+     }
+}
+
+int main(){
+	atomicStore!(int)( 1 );
+
+	return 0;
+}
+
