63243afb97697203472a62c03de1b9ad9c9c0842
thomask <devnull@localhost>
2005-12-10 10:36:48 +0000

    Kris <fu@bar.com> 2005-12-05 news:dn27o6$24c4$1@digitaldaemon.com

---
+++ run/s/static_35_D.d
@@ -0,0 +1,23 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kris <fu@bar.com>
+// @date@	2005-12-05
+// @uri@	news:dn27o6$24c4$1@digitaldaemon.com
+
+module dstress.run.s.static_35_D;
+
+class Outer{
+	static struct Inner(T){
+		static int test(){
+			return 2 + T.sizeof;
+		}
+	}
+}
+
+int main(){
+	if(Outer.Inner!(char).test() == 3){
+		return 0;
+	}
+}
