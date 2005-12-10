e4cb0e651e6ab2f797cc4e055dde7ac028fd42c7
thomask <devnull@localhost>
2005-12-10 00:41:05 +0000

    Don Clugston <dac@nospam.com.au> 2005-12-06 news:dn488i$11kk$1@digitaldaemon.com

---
+++ run/m/mangleof_14_F.d
@@ -0,0 +1,25 @@
+// $HeadURL: svn://dstress.kuehne.cn/run/__DATE__01.d $
+// $Date: 2005-03-12 14:49:58 +0100 (Sat, 12 Mar 2005) $
+// $Author: thomask $
+
+// @author@	Don Clugston <dac@nospam.com.au>
+// @date@	2005-12-06
+// @uri@	news:dn488i$11kk$1@digitaldaemon.com
+
+module dstress.run.m.mangleof_14_F;
+
+template a(char n){
+	const char a = n;
+}
+
+template b(f) {
+	const int b =  a!(f.mangleof[$ - 1]);
+}
+
+int main(){
+	char c = b!(int[2]);
+
+	if(c == 'i'){
+		return 0;
+	}
+}
