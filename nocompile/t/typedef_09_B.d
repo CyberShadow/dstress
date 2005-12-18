ed745e178a7c374b21b1ddf60110767737b279b9
thomask <devnull@localhost>
2005-12-18 23:26:36 +0000

    Roberto Mariottini <Roberto_member@pathlink.com> 2005-12-05 dn1gtg$145u$1@digitaldaemon.com

---
+++ nocompile/t/typedef_09_B.d
@@ -0,0 +1,22 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Roberto Mariottini <Roberto_member@pathlink.com>
+// @date@	2005-12-05
+// @uri@	dn1gtg$145u$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 20
+
+module dstress.nocompile.t.typedef_09_B;
+
+typedef int A;
+typedef int B;
+
+void main(){
+	A a;
+	B b;
+
+	a += b;
+}
+
---
+++ run/t/typedef_09_E.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Roberto Mariottini <Roberto_member@pathlink.com>
+// @date@	2005-12-05
+// @uri@	dn1gtg$145u$1@digitaldaemon.com
+
+module dstress.run.t.typedef_09_E;
+
+typedef int A;
+
+int main(){
+	A a;
+
+	a = 1 + a;
+
+	return 0;
+}
+
