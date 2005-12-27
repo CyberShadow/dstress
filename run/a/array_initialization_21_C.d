c40d1ef641408c790a2222016e66f1e151cb2403
thomask <devnull@localhost>
2005-12-27 19:58:14 +0000

    clayasaurus <clayasaurus@gmail.com> 2005-12-25 news:donatt$1acv$1@digitaldaemon.com>

---
+++ run/a/array_initialization_21_C.d
@@ -0,0 +1,24 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	clayasaurus <clayasaurus@gmail.com>
+// @date@	2005-12-25
+// @uri@	news:donatt$1acv$1@digitaldaemon.com>
+
+module dstress.run.a.array_initialization_21_C;
+
+const int MAX_PARTICLES = 30000;
+
+struct Particle{
+	float a;
+	float b;
+	float c;
+}
+
+Particle particles[MAX_PARTICLES];
+
+int main(){
+	assert(particles.length == MAX_PARTICLES);
+	return 0;
+}
---
+++ run/a/array_initialization_21_E.d
@@ -0,0 +1,25 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	clayasaurus <clayasaurus@gmail.com>
+// @date@	2005-12-25
+// @uri@	news:donatt$1acv$1@digitaldaemon.com>
+
+module dstress.run.a.array_initialization_21_E;
+
+const int MAX_PARTICLES = 20000;
+
+struct Particle{
+	float a;
+	float b;
+	float c;
+	float d;
+}
+
+Particle particles[MAX_PARTICLES];
+
+int main(){
+	assert(particles.length == MAX_PARTICLES);
+	return 0;
+}
