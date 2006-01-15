83bf124043c57a2c65e1e1c06b4eb1596db26929
thomask <devnull@localhost>
2006-01-15 11:17:56 +0000

    Don Clugston <dac@nospam.com.au> 2006-01-13 news:dq7rhv$8ep$1@digitaldaemon.com

---
+++ run/a/assert_12_A.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don Clugston <dac@nospam.com.au>
+// @date@	2006-01-13
+// @uri@	news:dq7rhv$8ep$1@digitaldaemon.com
+
+module dstress.run.a.assert_12_A;
+
+int main(){
+	static assert(0.9 + 3.5L == 0.9L + 3.5L);
+	return 0;
+}
---
+++ run/a/assert_12_C.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don Clugston <dac@nospam.com.au>
+// @date@	2006-01-13
+// @uri@	news:dq7rhv$8ep$1@digitaldaemon.com
+
+module dstress.run.a.assert_12_C;
+
+int main(){
+	bool b  = (0.9 + 3.5L == 0.9L + 3.5L);
+	assert(b);
+	return 0;
+}
