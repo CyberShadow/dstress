ebc414657b605cf9a0aa3cdb078101c319ee7609
thomask <devnull@localhost>
2006-01-15 11:18:56 +0000

    Don Clugston <dac@nospam.com.au> 2006-01-12 news:dq57on$6ee$1@digitaldaemon.com

---
+++ run/t/template_23_B.d
@@ -0,0 +1,24 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don Clugston <dac@nospam.com.au>
+// @date@	2006-01-12
+// @uri@	news:dq57on$6ee$1@digitaldaemon.com
+
+module dstress.run.t.template_23_B;
+
+template whale(char walrus){
+	const char whale = walrus;
+}
+
+template dolphin(char[] fish){
+	const char dolphin = whale!(fish[4]);
+}
+
+const char urchin = dolphin!("anenome");
+
+int main(){
+	static assert(urchin == 'o');
+	return 0;
+}
