18a905c6903f23c56de25b1806631b5200bcf61b
thomask <devnull@localhost>
2006-01-31 09:46:07 +0000

    Kris <fu@bar.com> 2006-01-12 news:dq6ca9$16i7$1@digitaldaemon.com

---
+++ norun/d/destructor_06.d
@@ -0,0 +1,83 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kris <fu@bar.com>
+// @date@	2006-01-12
+// @uri@	news:dq6ca9$16i7$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 82
+
+module dstress.norun.d.destructor_06;
+
+int step;
+int b_step;
+int a_step;
+int module_step;
+
+class A{
+	static this(){
+		step++;
+		a_step = step;
+	}
+
+	static ~this(){
+		assert(step == a_step);
+		a_step = 10;
+		step--;
+		if(step == 0){
+			check();
+		}
+	}
+}
+
+static this(){
+	step++;
+	module_step = step;
+}
+
+static ~this(){
+	assert(step == module_step);
+	module_step = 10;
+	step--;
+	if(step == 0){
+		check();
+	}
+}
+
+class B{
+	static this(){
+		step++;
+		b_step = step;
+	}
+
+	static ~this(){
+		assert(step == b_step);
+		b_step = 10;
+		step--;
+		if(step == 0){
+			check();
+		}
+	}
+}
+
+int main(){
+	return 0;
+}
+
+void check(){
+	if(a_step != 10){
+		assert(0);
+	}
+
+	if(b_step != 10){
+		assert(0);
+	}
+
+	if(module_step != 10){
+		assert(0);
+	}
+
+	// all destructors have been run in the reverse sequence of the constructors
+	assert(0);
+}
