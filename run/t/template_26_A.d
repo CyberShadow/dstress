7969e69b96c8995745c03dc95cbde09b04b8cc85
thomask <devnull@localhost>
2006-02-05 09:59:22 +0000

    Don Clugston <dac@nospam.com.au> 2006-01-25 news:dr7mmm$1f0t$1@digitaldaemon.com

---
+++ run/t/template_26_A.d
@@ -0,0 +1,30 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don Clugston <dac@nospam.com.au>
+// @date@	2006-01-25
+// @uri@	news:dr7mmm$1f0t$1@digitaldaemon.com
+
+module dstress.run.t.template_26_A;
+
+template factorial(uint n){
+	static if (n<2){
+		const int factorial = 1;
+	}else{
+		const int factorial = n * factorial!(n-1);
+	}
+}
+
+template rhino(alias hippo){
+	const int rhino = hippo!(3);
+}
+
+const int lion = rhino!(factorial);
+
+int main(){
+	static if(lion == 1 * 2 *3){
+		return 0;
+	}
+}
+
