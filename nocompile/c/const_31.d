8f81d9ede4027b5dc701c7b7928fc5c2bcf3fdc0
thomask <devnull@localhost>
2006-02-11 09:31:20 +0000

    Artem Rebrov <ar_other@mail.ru> 2006-02-06 news:op.s4kei3z6ncj208@comp

---
+++ nocompile/c/const_31.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Artem Rebrov <ar_other@mail.ru>
+// @date@	2006-02-06
+// @uri@	news:op.s4kei3z6ncj208@comp
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.c.const_31;
+
+const c = 1uL-1;
+
