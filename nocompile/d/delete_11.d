160ccdf7cc14a8ed694436843c72d990f95db2ce
thomask <devnull@localhost>
2006-02-11 09:37:19 +0000

    Dave <Dave_member@pathlink.com> 2006-02-11 news:dsjs7l$ai9$1@digitaldaemon.com

---
+++ nocompile/d/delete_11.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Dave <Dave_member@pathlink.com>
+// @date@	2006-02-11
+// @uri@	news:dsjs7l$ai9$1@digitaldaemon.com
+
+module dstress.nocompile.d.delete_11;
+
+void main(){
+	int[] a = new int[10];
+	int[] b = new int[10];
+	delete a, b;
+}
+
