e098db3acb2d6dd6ab39c563672d68d19b54692d
thomask <devnull@localhost>
2006-02-16 11:50:21 +0000

    Thomas Kuehne <thomas-dloop@kuehne.cn> 2006-02-16 news:uepdc3-1p2.ln1@birke.kuehne.cn

---
+++ nocompile/b/bang_02_C.d
@@ -0,0 +1,16 @@
+#! blah foo
+
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2006-02-16
+// @uri@	news:uepdc3-1p2.ln1@birke.kuehne.cn
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.b.bang_02_C;
+
+x;
+
