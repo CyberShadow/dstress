d0b1402f278936aa27b10bbf9261094a954d1485
thomask <devnull@localhost>
2006-02-16 11:48:44 +0000

    Thomas Kuehne <thomas-dloop@kuehne.cn> 2006-02-16 news:uepdc3-1p2.ln1@birke.kuehne.cn

---
+++ run/b/bang_01_A.d
@@ -0,0 +1,11 @@
+#! blah foo bar
+
+// $HeadURL$
+// $Date$
+// $Author$
+
+module dstess.run.b.bang_01_A;
+
+int main(){
+	return 0;
+ }
---
+++ run/b/bang_02_A.d
@@ -0,0 +1,18 @@
+#! blah foo
+
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2006-02-16
+// @uri@	news:uepdc3-1p2.ln1@birke.kuehne.cn
+
+module dstress.run.b.bang_02_A;
+
+int main(){
+	if(__LINE__ == 14){
+		return 0;
+	}
+}
+
