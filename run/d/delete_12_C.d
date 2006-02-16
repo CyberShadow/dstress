7d7a83e26d7f5b3f1c90869ad89e8addf7605891
thomask <devnull@localhost>
2006-02-16 11:41:35 +0000

    Nick <Nick_member@pathlink.com> 2006-02-15 news:dsv036$qlt$1@digitaldaemon.com

---
+++ run/d/delete_12_C.d
@@ -0,0 +1,62 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Nick <Nick_member@pathlink.com>
+// @date@	2006-02-15
+// @uri@	news:dsv036$qlt$1@digitaldaemon.com
+
+// @WARNING@	direct use of Phobos
+
+module dstress.run.d.delete_12_C;
+
+import std.c.stdlib;
+import std.outofmemory;
+import std.gc;
+
+int status;
+
+class Foo{
+	int i;
+
+	new(size_t sz){
+		void* p;
+
+		p = malloc(sz);
+    
+		if (!p){
+			throw new OutOfMemoryException();
+		}
+		
+		if(status++ != 1){
+			assert(0);
+		}
+
+		return p;
+	}
+
+	delete(void* p){
+		if(status++ != 2){
+			assert(0);
+		}
+	}
+}
+
+void test(){
+	Foo f = new Foo();
+	delete f;
+}
+
+int main(){
+	if(status++ != 0){
+		assert(0);
+	}
+
+	test();
+
+	if(status++ != 3){
+		assert(0);
+	}
+
+	return 0;
+}
