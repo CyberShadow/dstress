7fe3d9428d116ba415430ae91762e72ae4271a08
thomask <devnull@localhost>
2006-02-17 13:02:08 +0000

    Wang Zhen <nehzgnaw@gmail.com> 2006-02-17 news:dt4ahd$2ikp$2@digitaldaemon.com

---
+++ nocompile/c/const_32_B.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Wang Zhen <nehzgnaw@gmail.com>
+// @date@	2006-02-17
+// @uri@	news:dt4ahd$2ikp$2@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.c.const_32_B;
+
+const int a = .b;
+const int b = .a;
+
