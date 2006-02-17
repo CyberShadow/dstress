dd7cb905d3649784f8cde694653c44da0edbb49f
thomask <devnull@localhost>
2006-02-17 10:11:26 +0000

    Derek Parnell <derek@psych.ward> 2006-02-16 news:9zjj1lakhsqb$.vh2lqmuohfvm$.dlg@40tude.net

---
+++ run/a/assert_13_A.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Derek Parnell <derek@psych.ward>
+// @date@	2006-02-16
+// @uri@	news:9zjj1lakhsqb$.vh2lqmuohfvm$.dlg@40tude.net
+
+module dstress.run.a.assert_13_A;
+
+int main(){
+	assert("\u00aa", "\u00aa\uff16");
+
+	return 0;
+}
