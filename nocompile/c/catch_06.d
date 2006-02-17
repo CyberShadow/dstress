82a80bb84b1ee49dbe9604b94f3268a3bfd318b6
thomask <devnull@localhost>
2006-02-17 10:12:00 +0000

    Thomas Kuehne <thomas-dloop@kuehne.cn> 2006-02-17 news:86jgc3-gc7.ln1@birke.kuehne.cn

---
+++ nocompile/c/catch_06.d
@@ -0,0 +1,19 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2006-02-17
+// @uri@	news:86jgc3-gc7.ln1@birke.kuehne.cn
+
+// Porting: C#
+
+// __DSTRESS_ELINE__ 17
+
+module dstress.nocompile.c.catch_06;
+
+void main(){
+	try{
+	}catch(Object){
+	}
+}
