6bfbb7d083fd36b3ea48e9565d16220bb2b47fba
thomask <devnull@localhost>
2006-02-18 08:07:42 +0000

    Wang Zhen <nehzgnaw@gmail.com> 2006-02-17 news:dt4b75$2j45$4@digitaldaemon.com

---
+++ nocompile/t/typeinfo_01_C.d
@@ -0,0 +1,19 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Wang Zhen <nehzgnaw@gmail.com>
+// @date@	2006-02-17
+// @uri@	news:dt4b75$2j45$4@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 18
+
+module dstress.nocompile.t.typeinfo_01_C;
+
+struct S{
+}
+
+void main(){
+	S s;
+	typeinfo(s);
+}
