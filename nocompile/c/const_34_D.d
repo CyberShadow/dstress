47b9f93badbdbe3dca971e537a6976edd3929af1
thomask <devnull@localhost>
2006-02-18 08:05:22 +0000

    Wang Zhen <nehzgnaw@gmail.com> 2006-02-17 news:dt4b4a$2j45$3@digitaldaemon.com

---
+++ nocompile/c/const_34_D.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Wang Zhen <nehzgnaw@gmail.com>
+// @date@	2006-02-17
+// @uri@	news:dt4b4a$2j45$3@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.c.const_34_D;
+
+auto a = a;
+
---
+++ nocompile/c/const_34_F.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Wang Zhen <nehzgnaw@gmail.com>
+// @date@	2006-02-17
+// @uri@	news:dt4b4a$2j45$3@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.c.const_34_F;
+
+const int a = a;
+
+void main(){
+}
+
