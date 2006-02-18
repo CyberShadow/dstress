70898003854dbc6b88d9e88c8ec48081ee5fc1af
thomask <devnull@localhost>
2006-02-18 08:05:57 +0000

    Chris Miller <chris@dprogramming.com> 2006-02-16 news:op.s42ueypxpo9bzi@moe

---
+++ nocompile/d/delegate_16.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Chris Miller <chris@dprogramming.com>
+// @date@	2006-02-16
+// @uri@	news:op.s42ueypxpo9bzi@moe
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.d.delegate_16;
+
+void delegate() func();
+
---
+++ nocompile/f/function_05.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Chris Miller <chris@dprogramming.com>
+// @date@	2006-02-16
+// @uri@	news:op.s42ueypxpo9bzi@moe
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.f.function_05;
+
+void function() func();
+
