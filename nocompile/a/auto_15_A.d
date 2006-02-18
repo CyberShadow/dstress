6e8f03cd49e9b20cfb92b3b6c0459abfb3879808
thomask <devnull@localhost>
2006-02-18 08:04:40 +0000

    Wang Zhen <nehzgnaw@gmail.com> 2006-02-17 news:dt4b13$2j45$2@digitaldaemon.com

---
+++ nocompile/a/auto_15_A.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Wang Zhen <nehzgnaw@gmail.com>
+// @date@	2006-02-17
+// @uri@	news:dt4b13$2j45$2@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.a.auto_15_A;
+
+auto a = .a;
+
