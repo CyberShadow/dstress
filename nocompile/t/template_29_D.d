94bb9c09318629b5e73222ecdd79108e5ade530c
thomask <devnull@localhost>
2006-02-24 07:26:09 +0000

    Chris Miller <chris@dprogramming.com> 2006-02-20 news:op.s5anrdhxpo9bzi@moe

--- nocompile/t/template_29_D.d
+++ nocompile/t/template_29_D.d
@@ -7,8 +7,6 @@
 // @uri@	news:dt2eo3$tea$1@digitaldaemon.com
 // @desc@	Internal error: e2ir.c 736
 
-// __DSTRESS_ELINE__ 16
-
 module dstress.nocompile.t.template_29_D;
 
 template foo(size_t i){
---
+++ run/t/typeid_88_D.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Chris Miller <chris@dprogramming.com>
+// @date@	2006-02-20
+// @uri@	news:op.s5anrdhxpo9bzi@moe
+
+module dstress.run.t.typeid_88_D;
+
+int main(){
+	if(typeid(int*[]) != typeid(short*[])){
+		return 0;
+	}
+}
