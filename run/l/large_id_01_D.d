fbe287e0112bf000eeec85458375a31b27358e3a
thomask <devnull@localhost>
2006-02-25 08:30:52 +0000

    Thomas Kuehne <thomas-dloop@kuehne.cn> 2006-02-25 news:igi5d3-ug8.ln1@birke.kuehne.cn

---
+++ run/l/large_id_01_D.d
@@ -0,0 +1,147 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2006-02-25
+// @uri@	news:news:igi5d3-ug8.ln1@birke.kuehne.cn
+
+module dstress.run.l.large_id_01_D;
+
+class C_128 {
+static class C_127 {
+static class C_126 {
+static class C_125 {
+static class C_124 {
+static class C_123 {
+static class C_122 {
+static class C_121 {
+static class C_120 {
+static class C_119 {
+static class C_118 {
+static class C_117 {
+static class C_116 {
+static class C_115 {
+static class C_114 {
+static class C_113 {
+static class C_112 {
+static class C_111 {
+static class C_110 {
+static class C_109 {
+static class C_108 {
+static class C_107 {
+static class C_106 {
+static class C_105 {
+static class C_104 {
+static class C_103 {
+static class C_102 {
+static class C_101 {
+static class C_100 {
+static class C_99 {
+static class C_98 {
+static class C_97 {
+static class C_96 {
+static class C_95 {
+static class C_94 {
+static class C_93 {
+static class C_92 {
+static class C_91 {
+static class C_90 {
+static class C_89 {
+static class C_88 {
+static class C_87 {
+static class C_86 {
+static class C_85 {
+static class C_84 {
+static class C_83 {
+static class C_82 {
+static class C_81 {
+static class C_80 {
+static class C_79 {
+static class C_78 {
+static class C_77 {
+static class C_76 {
+static class C_75 {
+static class C_74 {
+static class C_73 {
+static class C_72 {
+static class C_71 {
+static class C_70 {
+static class C_69 {
+static class C_68 {
+static class C_67 {
+static class C_66 {
+static class C_65 {
+static class C_64 {
+static class C_63 {
+static class C_62 {
+static class C_61 {
+static class C_60 {
+static class C_59 {
+static class C_58 {
+static class C_57 {
+static class C_56 {
+static class C_55 {
+static class C_54 {
+static class C_53 {
+static class C_52 {
+static class C_51 {
+static class C_50 {
+static class C_49 {
+static class C_48 {
+static class C_47 {
+static class C_46 {
+static class C_45 {
+static class C_44 {
+static class C_43 {
+static class C_42 {
+static class C_41 {
+static class C_40 {
+static class C_39 {
+static class C_38 {
+static class C_37 {
+static class C_36 {
+static class C_35 {
+static class C_34 {
+static class C_33 {
+static class C_32 {
+static class C_31 {
+static class C_30 {
+static class C_29 {
+static class C_28 {
+static class C_27 {
+static class C_26 {
+static class C_25 {
+static class C_24 {
+static class C_23 {
+static class C_22 {
+static class C_21 {
+static class C_20 {
+static class C_19 {
+static class C_18 {
+static class C_17 {
+static class C_16 {
+static class C_15 {
+static class C_14 {
+static class C_13 {
+static class C_12 {
+static class C_11 {
+static class C_10 {
+static class C_9 {
+static class C_8 {
+static class C_7 {
+static class C_6 {
+static class C_5 {
+static class C_4 {
+static class C_3 {
+static class C_2 {
+static class C_1 {
+	int i = 3;
+}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+int main(){
+	auto o = new C_128.C_127.C_126.C_125.C_124.C_123.C_122.C_121.C_120.C_119.C_118.C_117.C_116.C_115.C_114.C_113.C_112.C_111.C_110.C_109.C_108.C_107.C_106.C_105.C_104.C_103.C_102.C_101.C_100.C_99.C_98.C_97.C_96.C_95.C_94.C_93.C_92.C_91.C_90.C_89.C_88.C_87.C_86.C_85.C_84.C_83.C_82.C_81.C_80.C_79.C_78.C_77.C_76.C_75.C_74.C_73.C_72.C_71.C_70.C_69.C_68.C_67.C_66.C_65.C_64.C_63.C_62.C_61.C_60.C_59.C_58.C_57.C_56.C_55.C_54.C_53.C_52.C_51.C_50.C_49.C_48.C_47.C_46.C_45.C_44.C_43.C_42.C_41.C_40.C_39.C_38.C_37.C_36.C_35.C_34.C_33.C_32.C_31.C_30.C_29.C_28.C_27.C_26.C_25.C_24.C_23.C_22.C_21.C_20.C_19.C_18.C_17.C_16.C_15.C_14.C_13.C_12.C_11.C_10.C_9.C_8.C_7.C_6.C_5.C_4.C_3.C_2.C_1();
+	if(o.i == 3){
+		return 0;
+	}
+}
