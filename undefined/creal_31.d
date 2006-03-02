88ea67795ccbcb60e15dac861bc5046192fe0556
thomask <devnull@localhost>
2006-03-02 08:14:35 +0000

    bobef <bobef@lessequal.com> 2006-03-01 news:du4ojh$1b0n$1@digitaldaemon.com

---
+++ undefined/creal_31.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2006-03-02
+// @uri@	news:du1rjs$1si3$1@digitaldaemon.com
+
+module dstress.undefined.creal_31;
+
+int main(){
+	if(1.0 > 1.0i){
+		// unordered or "greater than" ?
+	}
+
+	return 0;
+}
