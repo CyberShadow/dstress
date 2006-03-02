6a0eb9deb034a5c5075167bfc6c489234d78c53b
thomask <devnull@localhost>
2006-03-02 08:14:25 +0000

    Don Clugston <dac@nospam.com.au> 2006-03-01 news:du4en6$seu$1@digitaldaemon.com

---
+++ nocompile/o/opAddAssign_19_E.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don Clugston <dac@nospam.com.au>
+// @date@	2006-03-01
+// @uri@	news:du4en6$seu$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.o.opAddAssign_19_E;
+
+void main(){
+	bool b = true;
+	b += 0.0i;
+}
---
+++ nocompile/o/opDiv_14_D.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don Clugston <dac@nospam.com.au>
+// @date@	2006-03-01
+// @uri@	news:du4en6$seu$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.o.opDiv_14_D;
+
+void main(){
+	bool b = true;
+	b = b / ifloat.nan;
+}
