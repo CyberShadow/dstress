24da289f2a357a1b6ce03b65215795f71b104bd8
thomask <devnull@localhost>
2006-03-07 10:22:28 +0000

    Ant <duitoolkit@yahoo.ca> 2006-03-06 news:duiilb$2heo$1@digitaldaemon.com

---
+++ run/s/struct_24_C.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ant <duitoolkit@yahoo.ca>
+// @date@	2006-03-06
+// @uri@	news:duiilb$2heo$1@digitaldaemon.com
+
+module dstess.run.s.struct_24_C;
+
+struct S;
+
+int main(char[][] args){
+	S* s = new S;
+
+	// don't trip the "no effect" filter:
+	size_t* x = cast(size_t*)s;
+
+	return *x * (args.length - 1);
+}
