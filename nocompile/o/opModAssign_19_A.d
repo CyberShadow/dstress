ca77f5d7b17fbdb46c04f7c66a14ef7885868ba0
thomask <devnull@localhost>
2006-03-08 14:06:14 +0000

    <ddparnell@bigpond.com> 2006-03-08 news:bug-24-3@http.d.puremagic.com/bugzilla/

---
+++ nocompile/o/opModAssign_19_A.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<ddparnell@bigpond.com>
+// @date@	2006-03-08
+// @uri@	news:bug-24-3@http.d.puremagic.com/bugzilla/
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.o.opModAssign_19_A;
+
+void main(){
+	bool b;
+	b %= b;
+}
