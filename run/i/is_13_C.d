371452bd17fc0aeffb9cc206ac8ff071a529ce96
thomask <devnull@localhost>
2006-03-15 20:21:44 +0000

    Sean Kelly <sean@f4.ca> 2006-03-15 news:dv9okf$mfp$1@digitaldaemon.com

---
+++ run/i/is_13_C.d
@@ -0,0 +1,23 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Sean Kelly <sean@f4.ca>
+// @date@	2006-03-15
+// @uri@	news:dv9okf$mfp$1@digitaldaemon.com
+
+module dstress.run.i.is_13_C;
+
+class C{
+	static int test(int i){
+		return i * 2;
+	}
+}
+
+int main(){
+	if(!is(C.test == function)){
+		assert(0);	
+	}
+
+	return 0;
+}
