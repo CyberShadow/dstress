9ff1fbc2c7cb6e6ee7016e1763af7c00aff0eecc
thomask <devnull@localhost>
2006-03-19 07:38:32 +0000

    <oskar.linde@gmail.com> 2006-03-16 news:bug-54-3@http.d.puremagic.com/bugzilla/

---
+++ run/t/typeof_09_A.d
@@ -0,0 +1,29 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<oskar.linde@gmail.com>
+// @date@	2006-03-16
+// @uri@	news:bug-54-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.t.typeof_09_A;
+
+typedef int MyInt = 4;
+
+template declare(X){
+	X declare;
+}
+
+typeof(declare!(MyInt[1]).ptr[0]) y;
+
+int main(){
+	static if(!is(typeof(y) == MyInt)){
+		static assert(0);
+	}
+
+	if(y != cast(MyInt)4){
+		assert(0);
+	}
+
+	return 0;
+}
---
+++ run/t/typeof_09_F.d
@@ -0,0 +1,25 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<oskar.linde@gmail.com>
+// @date@	2006-03-16
+// @uri@	news:bug-54-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.t.typeof_09_F;
+
+typedef int MyInt = 4;
+
+typeof((new MyInt[0]).ptr[0]) y;
+
+int main(){
+	static if(!is(typeof(y) == MyInt)){
+		static assert(0);
+	}
+
+	if(y != cast(MyInt)4){
+		assert(0);
+	}
+
+	return 0;
+}
