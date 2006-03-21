126d2f55cb671985636132dd01fc989e1bce4694
thomask <devnull@localhost>
2006-03-21 10:42:11 +0000

    <clugdbug@yahoo.com.au> 2006-03-13 news:dv3rrs$2m8i$1@digitaldaemon.com

---
+++ run/m/mangleof_15_C.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<clugdbug@yahoo.com.au>
+// @date@	2006-03-13
+// @uri@	news:dv3rrs$2m8i$1@digitaldaemon.com
+
+module dstress.run.m.mangleof_15_C;
+
+int main(){
+	char [] s = (int*).mangleof;
+	
+	if( s.length > 2 && s[2] == 'x' ){
+		assert(0);
+	}
+
+	return 0;
+}
+
