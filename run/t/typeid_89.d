a5c686da17f49edd536d3dca1f1978bac23edd88
thomask <devnull@localhost>
2006-03-24 14:25:03 +0000

    <ericanderton@yahoo.com> 2006-03-22 news:bug-60-3@http.d.puremagic.com/bugzilla/

---
+++ run/t/typeid_89.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<ericanderton@yahoo.com>
+// @date@	2006-03-22
+// @uri@	news:bug-60-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.t.typeid_89;
+
+int main(){
+	Typeid t = typeid(bit);
+
+	assert(t !== null);
+
+	return 0;
+}
