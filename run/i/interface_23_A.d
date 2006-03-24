de7ba688a0ef9bb776ed0f18fb151d6bd91b923f
thomask <devnull@localhost>
2006-03-24 14:25:30 +0000

    <smjg@iname.com> 2006-03-22 news:bug-65-3@http.d.puremagic.com/bugzilla/

---
+++ run/i/interface_23_A.d
@@ -0,0 +1,62 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<smjg@iname.com>
+// @date@	2006-03-22
+// @uri@	news:bug-65-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.i.interface_23_A;
+
+int status;
+
+interface I {
+}
+
+class Parent {
+	I test() {
+		status += 7;
+		return new Child(42);
+	}
+}
+
+class Child : Parent, I {
+	int data;
+
+	this(int d) {
+		data = d;
+	}
+
+	override Child test() {
+		status += 1;
+		return new Child(69);
+	}
+}
+
+int main() {
+	Child a = new Child(105);
+	assert(a);
+	assert(status == 0);
+	assert(a.data == 105);
+
+	Parent p = a;
+	assert(p);
+	assert(status == 0);
+
+	Child c = a.test();
+	assert(c);
+	assert(status == 1);
+	assert(c.data == 69);
+
+	I i = p.test();
+	assert(i);
+	assert(status == 2);
+
+	Child d = cast(Child) i;
+	assert(d);
+	assert(status == 2);
+	assert(d.data == 69);
+
+	return 0;
+}
+
---
+++ run/i/interface_23_D.d
@@ -0,0 +1,64 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<smjg@iname.com>
+// @date@	2006-03-22
+// @uri@	news:bug-65-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.i.interface_23_D;
+
+int status;
+
+interface I {
+	int showData();
+}
+
+class Parent {
+	I test() {
+		status += 7;
+		return new Child(42);
+	}
+}
+
+class Child : Parent, I {
+	int data;
+
+	this(int d) {
+		data = d;
+	}
+
+	override Child test() {
+		status += 1;
+		return new Child(69);
+	}
+
+	override int showData(){
+		return data;
+	}
+}
+
+int main() {
+	Child a = new Child(105);
+	assert(a);
+	assert(status == 0);
+	assert(a.data == 105);
+
+	Parent p = a;
+	assert(a);
+	assert(status == 0);
+
+	a.test();
+	assert(status == 1);
+
+	I i = p.test();
+	assert(i);
+	assert(status == 2);
+
+	assert(a.data == 105);
+	assert(a.showData() == 105);
+	assert(i.showData() == 69);
+
+	return 0;
+}
+
