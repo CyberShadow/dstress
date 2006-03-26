b3dcc4c7329a46f66f68eb44d77e464e1c4f4c65
thomask <devnull@localhost>
2006-03-26 10:22:58 +0000

    <godaves@yahoo.com> 2006-03-26 news:bug-74-3@http.d.puremagic.com/bugzilla/

---
+++ run/i/if_14_D.d
@@ -0,0 +1,34 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<godaves@yahoo.com>
+// @date@	2006-03-26
+// @uri@	news:bug-74-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.i.if_14_D;
+
+int test(int i){
+	int j = i * i;
+	if(j)
+		return j;
+	else
+		return 10;
+}
+
+int main(){
+	if(test(1) != 1){
+		assert(0);
+	}
+
+	if(test(0) != 10){
+		assert(0);
+	}
+
+	if(test(-3) != 9){
+		assert(0);
+	}
+
+	return 0;
+}
+
---
+++ run/i/if_14_G.d
@@ -0,0 +1,34 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<godaves@yahoo.com>
+// @date@	2006-03-26
+// @uri@	news:bug-74-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.i.if_14_G;
+
+int test(int i){
+	if(auto j = i * i){
+		return j;
+	}else{
+		return 10;
+	}
+}
+
+int main(){
+	if(test(1) != 1){
+		assert(0);
+	}
+
+	if(test(0) != 10){
+		assert(0);
+	}
+
+	if(test(-3) != 9){
+		assert(0);
+	}
+
+	return 0;
+}
+
