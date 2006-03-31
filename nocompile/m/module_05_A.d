d7bebd030745a0ef759942ea989a39f31c556222
thomask <devnull@localhost>
2006-03-31 09:02:10 +0000

    <thomas-dloop@kuehn.cn> 2006-03-31 news:bug-78-3@http.d.puremagic.com/bugzilla/

---
+++ nocompile/m/module_05_A.d
@@ -0,0 +1,11 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<thomas-dloop@kuehn.cn>
+// @date@	2006-03-31
+// @uri@	news:bug-78-3@http.d.puremagic.com/bugzilla/
+
+// __DSTRESS_ELINE__ 11
+
+module dstress . nocompile . m . module_05_A;
---
+++ nocompile/m/module_05_D.d
@@ -0,0 +1,11 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<thomas-dloop@kuehn.cn>
+// @date@	2006-03-31
+// @uri@	news:bug-78-3@http.d.puremagic.com/bugzilla/
+
+// __DSTRESS_ELINE__ 11
+
+module dstress.no co mp ile.m.module_05_D;
