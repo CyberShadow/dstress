4ae70a7d791560e8cbd37c33c3b52251048b6814
thomask <devnull@localhost>
2006-04-02 11:53:42 +0000

    <daiphoenix@lycos.com> 2006-03-14 news:bug-50-3@http.d.puremagic.com/bugzilla/

---
+++ run/n/nested_class_05_A.d
@@ -0,0 +1,34 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<daiphoenix@lycos.com>
+// @date@	2006-03-14
+// @uri@	news:bug-50-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.n.nested_class_05_A;
+
+class Outer{
+	int i;
+	
+	class Inner{
+		int x;
+
+		this(){
+			x = i;
+		}
+	}
+}
+
+int main(){
+	Outer outer = new Outer();
+	outer.i = 1;
+	Outer.Inner inner = new outer.Inner();
+	outer.i = 2;
+
+	if(inner.x != 1){
+		assert(0);
+	}
+
+	return 0;
+}
