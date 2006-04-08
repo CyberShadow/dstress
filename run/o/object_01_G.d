63a0f1d0fa4da9db127f60ad234089cad2a5aab4
thomask <devnull@localhost>
2006-04-08 19:09:20 +0000

    <thomas-dloop@kuehne.cn> 2006-04-09 news:bug-94-3@http.d.puremagic.com/bugzilla/

---
+++ run/o/object_01_G.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<thomas-dloop@kuehne.cn>
+// @date@	2006-04-09
+// @uri@	news:bug-91-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.o.object_01_G;
+
+class Object{
+}
+
+int main(){
+	auto o = new Object();
+
+	return 0;
+}
