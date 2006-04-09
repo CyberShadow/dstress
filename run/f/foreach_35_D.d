4fe2a0b75a8dcce7543382ef810d6d92bbdfa8fd
thomask <devnull@localhost>
2006-04-09 19:51:17 +0000

    <ericanderton@yahoo.com> 2006-04-08 news:bug-95-3@http.d.puremagic.com/bugzilla/

---
+++ run/f/foreach_35_D.d
@@ -0,0 +1,26 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<ericanderton@yahoo.com>
+// @date@	2006-04-08
+// @uri@	news:bug-95-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.f.foreach_35_D;
+
+int main(){
+	int[int] array;
+	array[2] = 3;
+	array.remove(2);
+	
+	if(array.keys.length != 0){
+		assert(0);
+	}
+	
+	foreach(int elem; array){
+		assert(0);
+	}
+
+	return 0;
+}
+
