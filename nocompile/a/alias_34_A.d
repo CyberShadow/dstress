c09a9ac7ed501aa409ecd04eb58d46a5fd3bbf23
thomask <devnull@localhost>
2006-04-14 10:24:08 +0000

    <clayasaurus@gmail.com> 2006-04-13 news:bug-104-3@http.d.puremagic.com/bugzilla/

---
+++ nocompile/a/alias_34_A.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<clayasaurus@gmail.com>
+// @date@	2006-04-13
+// @uri@	news:bug-104-3@http.d.puremagic.com/bugzilla/
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.a.alias_34_A;
+
+alias int function(Foo) Foofunc;
+
+Foofunc f;
---
+++ nocompile/t/typedef_15_A.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<clayasaurus@gmail.com>
+// @date@	2006-04-13
+// @uri@	news:bug-104-3@http.d.puremagic.com/bugzilla/
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.t.typedef_15_A;
+
+Foofunc f;
+
+typedef int function(Foo) Foofunc;
