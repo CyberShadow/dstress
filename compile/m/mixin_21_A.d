45f02826ad311b203a0bc2c02d9b00d2695371b6
thomask <devnull@localhost>
2006-04-18 06:26:55 +0000

    <benoit@tionex.de> 2006-04-15 news:bug-107-3@http.d.puremagic.com/bugzilla/

---
+++ compile/m/mixin_21_A.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<benoit@tionex.de>
+// @date@	2006-04-15
+// @uri@	news:bug-107-3@http.d.puremagic.com/bugzilla/
+
+module /*dstress.*/compile.m.mixin_21_A;
+
+template T(){
+	UNDEFINED x;
+}
+
---
+++ nocompile/m/mixin_21_B.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<benoit@tionex.de>
+// @date@	2006-04-15
+// @uri@	news:bug-107-3@http.d.puremagic.com/bugzilla/
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.m.mixin_21_B;
+
+import /*dstress.*/compile.m.mixin_21_A;
+
+mixin T!();
+
