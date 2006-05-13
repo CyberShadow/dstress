aaa44244cb4d20709ce9633e1bd1d0ff03e90a59
thomask <devnull@localhost>
2006-05-13 08:26:00 +0000

    inline ASM review

--- run/a/asm_fistp_01_C.d
+++ run/a/asm_fistp_01_C.d
@@ -2,32 +2,41 @@
 // $Date$
 // $Author$
 
-// __DSTRESS_DFLAGS__ addon/cpuinfo.d
-
 module dstress.run.a.asm_fistp_01_C;
-import addon.cpuinfo;
 
-int main(){
-	version(D_InlineAsm){
+version(D_InlineAsm_X86){
+	version = runTest;
+}else version(D_InlineAsm_X86_64){
+	version = runTest;
+}
+
+version(runTest){
+	import addon.cpuinfo;
+
+	int main(){
 		haveFPU!()();
 		
-		real r = -800.0L;
+		float r = -800.0f;
 		long a, b;
 		
 		asm{
-			finit;
+			fldz;
 			fld1;
 			fld r;
 			fistp a;
 			fistp b;
 		}
 		
-		assert(a == -800);
-		assert(b == 1);
+		if(a != -800){
+			assert(0);
+		}
+		if(b != 1){
+			assert(0);
+		}
 		
 		return 0;
-	}else{
-		pragma(msg, "no inline asm support");
-		static assert(0);
 	}
+}else{
+	pragma(msg, "DSTRESS{XFAIL}: no inline ASM support");
+	static assert(0);
 }
