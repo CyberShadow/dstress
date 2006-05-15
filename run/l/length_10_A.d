69f115f135f655f7edfaf9be9f9983755cba6721
thomask <devnull@localhost>
2006-05-15 04:22:42 +0000

    <CppCoder@gmail.com> 2006-05-02 news:bug-123-3@http.d.puremagic.com/bugzilla/

---
+++ run/l/length_10_A.d
@@ -0,0 +1,25 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<CppCoder@gmail.com>
+// @date@	2006-05-02
+// @uri@	news:bug-123-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.l.length_10_A;
+
+size_t intRes(){
+	return 4;
+}
+
+static const char[] foo = "abc123";
+
+int main(){
+	char c = foo[intRes() % $];
+
+	if(c != '2'){
+		assert(0);
+	}
+
+	return 0;
+}
---
+++ run/l/length_10_G.d
@@ -0,0 +1,21 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<CppCoder@gmail.com>
+// @date@	2006-05-02
+// @uri@	news:bug-123-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.l.length_10_G;
+
+static char[] foo = "abc123";
+
+int main(){
+	char c = foo[4 % $];
+
+	if(c != '2'){
+		assert(0);
+	}
+
+	return 0;
+}
