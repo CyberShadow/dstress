cc231b330417517ba4c653b4186b81b2534f9d67
thomask <devnull@localhost>
2006-05-21 20:04:36 +0000

    Conflicting identifiers between imported modules are reported in the wrong file
    <smjg@iname.com>
    2006-05-13
    news:bug-140-3@http.d.puremagic.com/bugzilla/

---
+++ nocompile/i/import_14_A.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<smjg@iname.com>
+// @date@	2006-05-13
+// @uri@	news:bug-140-3@http.d.puremagic.com/bugzilla/
+
+// __DSTRESS_EFILE__ 17
+
+module dstress.nocompile.i.import_14_A;
+
+import addon.import_01_dummy;
+import addon.import_08_Z;
+
+void main(){
+	test(0);	
+}
