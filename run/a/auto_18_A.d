a53f97ade0fb9c67b7fbb2c0d07fdc5a4430bb06
thomask <devnull@localhost>
2006-05-22 20:14:48 +0000

    Ivan Senji <ivan.senji_REMOVE_@_THIS__gmail.com> 2005-11-28 news:dmeunm$256t$1@digitaldaemon.com

---
+++ run/a/auto_18_A.d
@@ -0,0 +1,54 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ivan Senji <ivan.senji_REMOVE_@_THIS__gmail.com>
+// @date@	2005-11-28
+// @uri@	news:dmeunm$256t$1@digitaldaemon.com
+
+module dstress.run.a.auto_18_A;
+
+int main(){
+	static int[][] a = [[0, 1], [-2, 3, 4], [5]];
+	auto b = -2;
+
+	static if(!is(typeof(a[0][0]) : typeof(b))){
+		static assert(0);
+	}
+
+	if(a.length != 3){
+		assert(0);
+	}
+
+	if(a[0].length != 2){
+		assert(0);
+	}
+	if(a[0][0] != 0){
+		assert(0);
+	}
+	if(a[0][1] != 1){
+		assert(0);
+	}
+
+	if(a[1].length != 3){
+		assert(0);
+	}
+	if(a[1][0] != -2){
+		assert(0);
+	}
+	if(a[1][1] != 3){
+		assert(0);
+	}
+	if(a[1][2] != 4){
+		assert(0);
+	}
+	
+	if(a[2].length != 1){
+		assert(0);
+	}
+	if(a[2][0] != 5){
+		assert(0);
+	}
+	return 0;
+}
+
---
+++ run/a/auto_18_C.d
@@ -0,0 +1,35 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ivan Senji <ivan.senji_REMOVE_@_THIS__gmail.com>
+// @date@	2005-11-28
+// @uri@	news:dmeunm$256t$1@digitaldaemon.com
+
+module dstress.run.a.auto_18_C;
+
+int main(){
+	static auto a = [1, -2, 3];
+	auto b = -2;
+
+	static if(!is(typeof(a[0]) : typeof(b))){
+		static assert(0);
+	}
+
+	if(a.length != 3){
+		assert(0);
+	}
+
+	if(a[0] != 3){
+		assert(0);
+	}
+	if(a[1] != -2){
+		assert(0);
+	}
+	if(a[2] != 1){
+		assert(0);
+	}
+
+	return 0;
+}
+
