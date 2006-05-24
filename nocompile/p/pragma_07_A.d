42608913d95acf626bbb683ebcfaeefe76aa783e
thomask <devnull@localhost>
2006-05-24 16:23:17 +0000

    pragma without ';' results in undefined symbols
    <lio@lunesu.com>
    2006-05-23
    news:bug-151-3@http.d.puremagic.com/bugzilla/

---
+++ nocompile/p/pragma_07_A.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<lio@lunesu.com>
+// @date@	2006-05-23
+// @uri@	news:bug-151-3@http.d.puremagic.com/bugzilla/
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.p.pragma_07_A;
+
+pragma(msg, "blah") class A{}
+
