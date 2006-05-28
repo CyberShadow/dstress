2d447d4ae7ce710e8cad5039b9a07e8a5ad1185c
thomask <devnull@localhost>
2006-05-28 15:49:26 +0000

    "Unexpected FunExp type" error on function literals
    <dvdfrdmn@users.sf.net>
    2006-05-27
    news:bug-157-31@http.d.puremagic.com/bugzilla/

---
+++ run/f/function_06_A.d
@@ -0,0 +1,24 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<dvdfrdmn@users.sf.net>
+// @date@	2006-05-27
+// @uri@	news:bug-157-31@http.d.puremagic.com/bugzilla/
+
+module dstress.run.f.function_06_A;
+
+int function(int)[] table;
+
+int main() {	
+	table ~= function int(int x) {
+			return x + 99;
+		};
+
+	if(table[0](1) != 100){
+		assert(0);
+	}
+
+	return 0;
+}
+
---
+++ run/f/function_06_B.d
@@ -0,0 +1,24 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<dvdfrdmn@users.sf.net>
+// @date@	2006-05-27
+// @uri@	news:bug-157-31@http.d.puremagic.com/bugzilla/
+
+module dstress.run.f.function_06_B;
+
+int function(int)[1] table;
+
+int main() {	
+	table[0] = function int(int x) {
+			return x + 99;
+		};
+
+	if(table[0](1) != 100){
+		assert(0);
+	}
+
+	return 0;
+}
+
