a1cef9ec9fd9906fbb0c7367853fe57246e7e7d1
thomask <devnull@localhost>
2006-06-01 17:50:13 +0000

    weird crash when nesting unions and structs; code order dependent
    <h3r3tic@mat.uni.torun.pl>
    2006-05-27
    news:bug-158-3@http.d.puremagic.com/bugzilla/

---
+++ run/u/union_17_A.d
@@ -0,0 +1,47 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<h3r3tic@mat.uni.torun.pl>
+// @date@	2006-05-27
+// @uri@	news:bug-158-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.u.union_17_A;
+
+struct Bar {
+	union {
+		struct {
+			union {
+				Foo f;
+			}
+		}
+	}
+}
+
+struct Foo {
+	int i;
+}
+
+static if(Foo.sizeof != Bar.sizeof){
+	static assert(0);
+}
+
+int main(){
+	Foo a;
+	a.i = 2;
+	
+	Bar b;
+	b.f = a;
+
+	a.i = 3;
+	
+	if(a.i != 3){
+		assert(0);
+	}
+
+	if(b.f.i != 2){
+		assert(0);
+	}
+
+	return 0;
+}
---
+++ run/u/union_17_H.d
@@ -0,0 +1,45 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<h3r3tic@mat.uni.torun.pl>
+// @date@	2006-05-27
+// @uri@	news:bug-158-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.u.union_17_H;
+
+struct Foo {
+	int i;
+}
+
+struct Bar {
+	union {
+		struct {
+			Foo f;
+		}
+	}
+}
+
+static if(Foo.sizeof != Bar.sizeof){
+	static assert(0);
+}
+
+int main(){
+	Foo a;
+	a.i = 2;
+	
+	Bar b;
+	b.f = a;
+
+	a.i = 3;
+	
+	if(a.i != 3){
+		assert(0);
+	}
+
+	if(b.f.i != 2){
+		assert(0);
+	}
+
+	return 0;
+}
