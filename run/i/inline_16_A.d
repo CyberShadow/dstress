57564f43c8c36e1ed18d9047347700aaaa2e4fdb
thomask <devnull@localhost>
2006-06-01 17:50:33 +0000

    Inheriting nested classes and "-inline" throws access violation
    <sky@quit-clan.de>
    2006-05-24
    news:bug-156-3@http.d.puremagic.com/bugzilla/

---
+++ run/i/inline_16_A.d
@@ -0,0 +1,49 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<sky@quit-clan.de>
+// @date@	2006-05-24
+// @uri@	news:bug-156-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.i.inline_16_A;
+
+int status;
+
+class Foo{
+	class Bar{
+		void doSayHello(){
+			sayHello();
+		}
+	}
+
+	Bar bar;
+
+	void sayHello(){
+		status++;
+	}
+
+	this(){
+		bar = new Bar();
+	}
+}
+
+class Foo2 : Foo{
+}
+
+int main(){
+	Foo f = new Foo();
+
+	if(status != 0){
+		assert(0);
+	}
+	
+	f.bar.doSayHello();
+
+	if(status != 1){
+		assert(0);
+	}
+
+	return 0;
+}
+
---
+++ run/i/inline_16_C.d
@@ -0,0 +1,49 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<sky@quit-clan.de>
+// @date@	2006-05-24
+// @uri@	news:bug-156-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.i.inline_16_C;
+
+int status;
+
+class Foo{
+	class Bar{
+		void doSayHello(){
+			sayHello();
+		}
+	}
+
+	Bar bar;
+
+	void sayHello(){
+		status++;
+	}
+
+	this(){
+		bar = new Bar();
+	}
+}
+
+class Foo2 : Foo{
+}
+
+int main(){
+	Fooi2 f = new Foo2();
+
+	if(status != 0){
+		assert(0);
+	}
+	
+	f.bar.doSayHello();
+
+	if(status != 1){
+		assert(0);
+	}
+
+	return 0;
+}
+
