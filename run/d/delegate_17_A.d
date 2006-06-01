796e90828c714d10b8f3ba2c7c166218ea464780
thomask <devnull@localhost>
2006-06-01 17:50:42 +0000

    Nested classes can't return delegates to their parents
    <sky@quit-clan.de>
    2006-05-24
    news:bug-155-3@http.d.puremagic.com/bugzilla/

---
+++ run/d/delegate_17_A.d
@@ -0,0 +1,51 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<sky@quit-clan.de>
+// @date@	2006-05-24
+// @uri@	news:bug-155-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.d.delegate_17_A;
+
+int status;
+
+class Foo{
+	class Bar{
+		int delegate() getDelegate(){
+			return &sayHello;
+		}
+	}
+	Bar bar;
+
+	int sayHello(){
+		return ++status;
+	}
+
+	this(){
+		bar = new Bar();
+	}
+}
+
+int main(){
+	Foo foo = new Foo();
+	int delegate() hallo;
+	
+	hallo = foo.bar.getDelegate();
+
+	if(!hallo){
+		assert(0);
+	}
+	if(status != 0){
+		assert(0);
+	}
+	if(hallo() != 1){
+		assert(0);
+	}
+	if(status != 1){
+		assert(0);
+	}
+		
+	return 0;
+}
+
