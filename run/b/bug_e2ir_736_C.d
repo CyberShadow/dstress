5c2f5616671342233d61dbf2c119775640b27f86
thomask <devnull@localhost>
2006-06-02 16:56:21 +0000

    "Internal error: e2ir.c 736" on valid code
    <h3r3tic@mat.uni.torun.pl>
    2006-06-01
    news:bug-172-3@http.d.puremagic.com/bugzilla/

---
+++ run/b/bug_e2ir_736_C.d
@@ -0,0 +1,32 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<h3r3tic@mat.uni.torun.pl>
+// @date@	2006-06-01
+// @uri@	news:bug-172-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.b.bug_e2ir_736_C;
+
+int status;
+
+template Foo(){
+	int func() {
+		return status;
+	}
+}
+
+class Baz{
+	mixin Foo bar;
+}
+
+int main() {
+	status = 3;
+	Baz b = new Baz();
+	if(b.bar.func() != 3){
+		assert(0);
+	}
+
+	return 0;
+}
+
---
+++ run/b/bug_e2ir_736_D.d
@@ -0,0 +1,33 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<h3r3tic@mat.uni.torun.pl>
+// @date@	2006-06-01
+// @uri@	news:bug-172-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.b.bug_e2ir_736_D;
+
+int status;
+
+template Foo(){
+	template T(){
+		int func() {
+			return status;
+		}
+	}
+}
+
+class Baz{
+	mixin Foo bar;
+}
+
+int main() {
+	status = 3;
+	if(Baz.bar.T!().func() != 3){
+		assert(0);
+	}
+
+	return 0;
+}
+
