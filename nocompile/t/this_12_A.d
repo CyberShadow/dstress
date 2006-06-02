895bd2f52d2ea342f6a2c794bda96f9cdafd159a
thomask <devnull@localhost>
2006-06-02 16:56:30 +0000

    link error for 2+ static this()
    BCS <BCS_member@pathlink.com>
    2006-06-01
    news:e5n6sp$1at7$1@digitaldaemon.com

---
+++ nocompile/t/this_12_A.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	BCS <BCS_member@pathlink.com>
+// @date@	2006-06-01
+// @uri@	news:e5n6sp$1at7$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 16
+
+module dstress.nocompile.t.this_12_A;
+
+static this(){
+}
+
+static this(){
+}
