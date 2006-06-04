460557923d542f5345cc724f15b0a9195a5dc201
thomask <devnull@localhost>
2006-06-04 06:28:03 +0000

    added GDC-0.18 results

---
+++ run/o/opAndAnd_02_B.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<clugdbug@yahoo.com.au>
+// @date@	2006-05-29
+// @uri@	news:bug-160-3@http.d.puremagic.com/bugzilla/
+
+module dstress.run.o.opAndAnd_02_B;
+
+template fish(){
+	static assert(0);
+	bool fish = true;
+}
+
+template dog(char [] bird){
+	static if ((bird.length > 99) && fish!()){
+		const int dog = 2;
+	}else{
+		const int dog = 3;
+	}
+}
+
+const int pig = dog!("a");
+
+int main(){
+	if(pig != 3){
+		assert(0);
+	}
+	return 0;
+}
