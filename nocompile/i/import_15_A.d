fbfd5fa2413623aef3203bcdcb871b74ed9df493
thomask <devnull@localhost>
2006-06-07 21:30:06 +0000

    [Bug 176] New: message "module and package have the same name"
    <benoit@tionex.de>
    2006-06-06
    news:bug-176-3@http.d.puremagic.com/bugzilla/

---
+++ nocompile/i/import_15_A.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<benoit@tionex.de>
+// @date@	2006-06-06
+// @uri@	news:bug-176-3@http.d.puremagic.com/bugzilla/
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.i.import_15_A;
+
+import nocompile.i.import_15_A.d;
