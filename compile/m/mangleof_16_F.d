e1d0c3c843f45667cacf89fa8ddf83144c86d095
thomask <devnull@localhost>
2006-06-29 13:25:40 +0000

    <clugdbug@yahoo.com.au> 2006-06-23 news:bug-220-3@http.d.puremagic.com/issues/

---
+++ compile/m/mangleof_16_F.d
@@ -0,0 +1,19 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<clugdbug@yahoo.com.au>
+// @date@	2006-06-23
+// @uri@	news:bug-220-3@http.d.puremagic.com/issues/
+
+module dstress.compile.m.mangleof_16_F;
+
+alias int function (int) x;
+
+template T(char[] str){
+	const int T = 1;
+}
+
+static assert(T!((x).mangleof));
+
+
