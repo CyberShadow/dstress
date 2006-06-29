4d210f007bc88b95a33eadda2cebf629a196fc8c
thomask <devnull@localhost>
2006-06-29 13:26:28 +0000

    updated struct_24 to DMD-0.161

---
+++ nocompile/s/struct_24_B.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Ant <duitoolkit@yahoo.ca>
+// @date@	2006-03-06
+// @uri@	news:duiilb$2heo$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 13
+
+module dstess.run.s.struct_24_B;
+
+struct S;
