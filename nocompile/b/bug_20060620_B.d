61b704a48bb46fdff062e6e134f54198efd6c189
thomask <devnull@localhost>
2006-06-29 13:25:01 +0000

    k2 <k2_member@pathlink.com> 2006-06-20 news:e797n4$2fn8$1@digitaldaemon.com

---
+++ nocompile/b/bug_20060620_B.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	k2 <k2_member@pathlink.com>
+// @date@	2006-06-20
+// @uri@	news:e797n4$2fn8$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 13
+
+module dstress.nocompile.b.bug_20060620_B;
+
+int bar = "bar";
