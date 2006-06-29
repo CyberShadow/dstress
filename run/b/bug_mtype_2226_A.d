35eefe502a0c4798f116a9b85954961d934eef5b
thomask <devnull@localhost>
2006-06-29 13:26:02 +0000

    Bruno Medeiros <brunodomedeirosATgmail@SPAM.com> 2006-06-24 news:e7hus2$1egl$1@digitaldaemon.com

---
+++ run/b/bug_mtype_2226_A.d
@@ -0,0 +1,35 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Bruno Medeiros <brunodomedeirosATgmail@SPAM.com>
+// @date@	2006-06-24
+// @uri@	news:e7hus2$1egl$1@digitaldaemon.com
+
+module dstress.run.b.bug_mtype_2226_A;
+
+int status;
+
+class C{
+	void bar(){
+		status++;
+	}
+}
+
+int main(){
+	typeof( {} ) foo;
+	
+	C c = new C();
+
+	foo = &c.bar;
+
+	if(status != 0){
+		assert(0);
+	}
+	foo();
+	if(status != 1){
+		assert(0);
+	}
+
+	return 0;
+}
