44396aca90538aba4580b2e9ed21597a9b38d0bd
thomask <devnull@localhost>
2006-06-29 13:25:53 +0000

    <jarrett.billingsley@gmail.com> 2006-06-23 news:bug-222-3@http.d.puremagic.com/issues/

---
+++ nocompile/b/bug_cod1_1656_H.d
@@ -0,0 +1,22 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<jarrett.billingsley@gmail.com>
+// @date@	2006-06-23
+// @uri@	news:bug-222-3@http.d.puremagic.com/issues/
+
+// __DSTRESS_ELINE__ 18
+
+module dstress.nocompile.b.bug_cod1_1656_H;
+
+void f(){
+}
+
+int main(){
+	int x;
+	x = 0 && f();
+
+	return 0;
+}
+
