e91ad485c65ba606acea77e01d37f164db4703a1
thomask <devnull@localhost>
2006-07-02 15:43:04 +0000

    <larsivar@igesund.net> 2006-06-27 news:bug-229-31@http.d.puremagic.com/issues/

---
+++ compile/b/bug_dcodegen_2933_A.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<larsivar@igesund.net>
+// @date@	2006-06-27
+// @uri@	news:bug-229-31@http.d.puremagic.com/issues/
+
+module dstress.compile.b.bug_d_codegen_2933_A;
+
+class Foo{
+	struct Bar{
+	}
+	
+	private Bar bar;
+}
