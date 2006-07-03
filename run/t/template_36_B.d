63db4376e098121c3be0db7317895bf24d3e9a78
thomask <devnull@localhost>
2006-07-03 04:00:41 +0000

    <h3r3tic@mat.uni.torun.pl> 2006-06-22 news:bug-216-3@http.d.puremagic.com/issues/

---
+++ run/t/template_36_B.d
@@ -0,0 +1,54 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<h3r3tic@mat.uni.torun.pl>
+// @date@	2006-06-22
+// @uri@	news:bug-216-3@http.d.puremagic.com/issues/
+
+module dstress.run.t.template_36_B;
+
+int status;
+
+struct S{
+	int i;
+}
+
+template t(){
+	template foo(){
+		void foo(S s){
+			status = s.i + 1;
+		}
+	}
+
+	void bar(S s){
+		foo!()(s);
+	}
+}
+
+
+class Parent {
+	this() {
+	}
+}
+
+
+class Child : Parent {
+	mixin t!() mixed;
+}
+
+int main(){
+	Child c = new Child();
+
+	S s;
+	s.i = 3;
+
+	c.mixed.bar(s);
+
+	if(status != 4){
+		assert(0);
+	}
+
+	return 0;
+}
+	
