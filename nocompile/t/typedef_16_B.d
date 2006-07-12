8021962f9c7230e7b4fdb958c1492a8b3fac10a3
thomask <devnull@localhost>
2006-07-12 14:02:29 +0000

    <jpelcis@gmail.com> 2006-07-11 news:bug-249-3@http.d.puremagic.com/issues/

---
+++ nocompile/t/typedef_16_B.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<jpelcis@gmail.com>
+// @date@	2006-07-11
+// @uri@	news:bug-249-3@http.d.puremagic.com/issues/
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.t.typedef_16_B;
+
+typedef foo bar;
+typedef bar foo;
+
+foo x;
---
+++ nocompile/t/typedef_16_C.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<jpelcis@gmail.com>
+// @date@	2006-07-11
+// @uri@	news:bug-249-3@http.d.puremagic.com/issues/
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.t.typedef_16_C;
+
+typedef foo bar;
+typedef bar foo;
+
+bar x;
