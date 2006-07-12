7e61533f68e8f35e4a33948d0efbc7af9ac65737
thomask <devnull@localhost>
2006-07-12 10:56:41 +0000

    <smjg@iname.com> 2006-07-10 news:bug-248-3@http.d.puremagic.com/issues/

---
+++ nocompile/v/version_37_A.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<smjg@iname.com>
+// @date@	2006-07-10
+// @uri@	news:bug-248-3@http.d.puremagic.com/issues/
+
+// __DSTRESS_ELINE__ 13
+
+module /*dstress.*/nocompile.v.version_37_A;
+
+version{
+}
---
+++ nocompile/v/version_37_B.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<smjg@iname.com>
+// @date@	2006-07-10
+// @uri@	news:bug-248-3@http.d.puremagic.com/issues/
+
+// __DSTRESS_ELINE__ 13
+
+module /*dstress.*/nocompile.v.version_37_B;
+
+version;
