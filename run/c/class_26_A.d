d407d37aab16f205edba9b492b4ec90c9f5267bc
thomask <devnull@localhost>
2006-08-15 09:45:21 +0000

    [Issue 236] Class literal expression always says "base classes expected"
    Jarrett Billingsley <jarrett.billingsley@gmail.com>
    2006-07-02
    news:bug-236-3@http.d.puremagic.com/issues/

---
+++ run/c/class_26_A.d
@@ -0,0 +1,43 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Jarrett Billingsley <jarrett.billingsley@gmail.com>
+// @date@	2006-07-02
+// @uri@	news:bug-236-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 236] Class literal expression always says "base classes expected"
+
+module dstress.run.c.class_26_A;
+
+int main(){
+	int status;
+
+	int delegate() foo(){
+		class C{
+			int dg(){
+				return ++status;
+			}
+		}
+	
+		C c = new C();
+		
+		return &c.dg;
+	}
+
+	int delegate() bar = foo();
+
+	if(status != 0){
+		assert(0);
+	}
+
+	if(bar() != 1){
+		assert(0);
+	}
+
+	if(status != 1){
+		assert(0);
+	}
+
+	return 0;
+}
+
---
+++ run/c/class_26_B.d
@@ -0,0 +1,41 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Jarrett Billingsley <jarrett.billingsley@gmail.com>
+// @date@	2006-07-02
+// @uri@	news:bug-236-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 236] Class literal expression always says "base classes expected"
+
+module dstress.run.c.class_26_B;
+
+
+int main(){
+	int status;
+
+	int delegate() foo(){
+		return &(new class
+			{
+				int dg(){
+					return ++status;
+				}
+			}
+		).dg;
+	}
+
+	int delegate() bar = foo();
+	
+	if(status != 0){
+		assert(0);
+	}
+
+	if(bar() != 1){
+		assert(0);
+	}
+
+	if(status != 1){
+		assert(0);
+	}
+
+	return 0;
+}
