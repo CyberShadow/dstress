b840165f785193e350efa031877bb39423186804
thomask <devnull@localhost>
2006-08-18 07:04:34 +0000

    [Issue 291] assertion
    Lionello Lunesu <lio@lunesu.com>
    2006-08-18
    news:bug-291-3@http.d.puremagic.com/issues/

---
+++ run/f/float_28_B.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Lionello Lunesu <lio@lunesu.com>
+// @date@	2006-08-18
+// @uri@	news:bug-291-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 291] assertion
+
+module dstress.run.f.float_28_B;
+
+int main(){
+	float r;
+
+	if(r == 0.0){
+		assert(0);
+	}
+
+	return 0;
+}
---
+++ run/r/real_27_D.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Lionello Lunesu <lio@lunesu.com>
+// @date@	2006-08-18
+// @uri@	news:bug-291-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 291] assertion
+
+module dstress.run.r.real_27_D;
+
+int main(){
+	const real r = real.nan;
+
+	static if(r == 0.0){
+		assert(0);
+	}
+
+	return 0;
+}
