17dc27d40d1a44ed4d87e62364d94f8a920e52cb
thomask <devnull@localhost>
2006-08-26 11:05:02 +0000

    [Issue 287] DMD optimization bug arround dynamic array length
    Deewiant <deewiant@gmail.com>
    2006-08-19
    news:ec6kit$560$1@digitaldaemon.com

---
+++ run/o/odd_bug_07_E.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @auhtor@	 Deewiant <deewiant@gmail.com>
+// @date@	2006-08-19
+// @uri@	news:ec6kit$560$1@digitaldaemon.com
+// @desc@	[Issue 287] DMD optimization bug arround dynamic array length
+
+module dstress.run.o.odd_bug_07_E;
+
+int main() {
+	int lng, lngnew;
+
+	int[] arr = new int[1];
+
+	for (int i = 10; i--;) {
+		lngnew = lng + arr.length;
+		lng = lngnew;
+
+		lngnew = lng + arr.length;
+
+		if(lng + 1 != lngnew){
+			assert(0);
+		}
+		
+		lng = lng + arr.length;
+	}
+
+	return 0;
+}
