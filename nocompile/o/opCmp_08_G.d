d5b2cc2f8994ef1596e93c7650ec45ba6c0a3b0b
thomask <devnull@localhost>
2006-08-26 11:05:24 +0000

    [Issue 259] New: Comparing signed to unsigned does not generate an error
    Lionello Lunesu <lio@lunesu.com>
    2006-07-20
    news:bug-259-3@http.d.puremagic.com/issues/

---
+++ nocompile/o/opCmp_08_G.d
@@ -0,0 +1,22 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Lionello Lunesu <lio@lunesu.com>
+// @date@	2006-07-20
+// @uri@	news:bug-259-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 259] New: Comparing signed to unsigned does not generate an error
+
+// __DSTRESS_ELINE__ 18
+
+module dstress.nocompile.o.opCmp_08_G;
+
+void foo(){
+	short a = 1;
+	ushort b = 1;
+
+	if(a > b){
+		a++;
+	}
+}
+
---
+++ nocompile/o/opCmp_08_P.d
@@ -0,0 +1,22 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Lionello Lunesu <lio@lunesu.com>
+// @date@	2006-07-20
+// @uri@	news:bug-259-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 259] New: Comparing signed to unsigned does not generate an error
+
+// __DSTRESS_ELINE__ 18
+
+module dstress.nocompile.o.opCmp_08_P;
+
+void foo(){
+	long a = 1;
+	ulong b = 1;
+
+	if(a < b){
+		a++;
+	}
+}
+
