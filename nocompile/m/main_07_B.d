e507d746727c6a4ba8e3dda25c9a26a0758e77b2
thomask <devnull@localhost>
2006-09-07 07:51:45 +0000

    lazy in main
    Carlos Santander <csantander619@gmail.com>
    2006-09-05
    news:edk63e$2a8n$1@digitaldaemon.com

---
+++ nocompile/m/main_07_B.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Carlos Santander <csantander619@gmail.com>
+// @date@	2006-09-05
+// @uri@	news:edk63e$2a8n$1@digitaldaemon.com
+// @desc@	lazy in main
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.m.main_07_B;
+
+void main (lazy char [][] args){
+	size_t l = args.length;
+}
