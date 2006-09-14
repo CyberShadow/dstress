cab11e122b46bf98a5d45a68088ddbd4591ce37e
thomask <devnull@localhost>
2006-09-14 07:15:39 +0000

    name resolution and static function
    Carlos Santander <csantander619@gmail.com>
    2006-07-09
    news:e8sdrv$2k9c$1@digitaldaemon.com

---
+++ run/s/static_37_G.d
@@ -0,0 +1,32 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Carlos Santander <csantander619@gmail.com>
+// @date@	2006-07-09
+// @uri@	news:e8sdrv$2k9c$1@digitaldaemon.com
+// @desc@	name resolution and static function
+
+module dstress.run.s.static_37_G;
+
+class A{
+	private static int foo (int i){
+		return i + 1;
+	}
+}
+
+class B{
+	int foo (int x){
+		return A.foo(x) * 2;
+	}
+}
+
+int main(){
+	B b = new B();
+
+	if(b.foo(2) != 6){
+		assert(0);
+	}
+
+	return 0;
+}
