26ba14b96fbdf69d25e7f274adb3f025c54f73b6
thomask <devnull@localhost>
2006-09-14 07:15:31 +0000

    [Issue 221] New: Inconsistent name mangling of bool (a relic of 'bit')
     Don Clugston <clugdbug@yahoo.com.au>
    2006-06-23
    news:bug-221-3@http.d.puremagic.com/issues/

---
+++ compile/b/bool_07_C.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don Clugston <clugdbug@yahoo.com.au>
+// @date@	2006-06-23
+// @uri@	news:bug-221-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 221] New: Inconsistent name mangling of bool (a relic of 'bit')
+
+module dstress.compile.b.bool_07_C;
+
+static assert((void delegate(bool)).mangleof == "DFbZv");
+
