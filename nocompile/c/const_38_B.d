174eba39421a18994cb6e64eaa1b15067d045b32
thomask <devnull@localhost>
2006-09-20 19:11:23 +0000

    [Issue 223] Error message for unset constants doesn't specify error location
    Lionello Lunesu <lio@lunesu.com>
    2006-09-15
    news:eeek2t$1kkv$1@digitaldaemon.com

---
+++ nocompile/c/const_38_B.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Lionello Lunesu <lio@lunesu.com>
+// @date@	2006-09-15
+// @uri@	news:eeek2t$1kkv$1@digitaldaemon.com
+// @desc@	[Issue 223] Error message for unset constants doesn't specify error location
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.c.const_38_B;
+
+void foo(){
+	const int x;
+}
+
