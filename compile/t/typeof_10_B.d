09f0121288ca48823539726de97cbee33479f842
thomask <devnull@localhost>
2006-09-29 06:55:39 +0000

    [Issue 380] New: cannot use typeof(*this) in a static context
    Tomasz Stachowiak <bugzilla@digitalmars.com>
    2006-09-27
    news:bug-380-3@http.d.puremagic.com/issues/

---
+++ compile/t/typeof_10_B.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Tomasz Stachowiak <bugzilla@digitalmars.com>
+// @date@	2006-09-27
+// @uri@	news:bug-380-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 380] New: cannot use typeof(*this) in a static context
+
+module dstress.compile.t.typeof_10_B;
+
+struct Foo{
+        typeof(*this) x;
+}
+
+static assert(is(Foo == typeof(Foo.x)));
