0668a4737f14a9daa5b03a8ff10da93f6e0f5016
thomask <devnull@localhost>
2006-10-02 08:02:50 +0000

    [Issue 389] New: Cannot link to std.path.altsep
    Bradley Smith <digitalmars-com@baysmith.com>
    2006-09-30
    news:bug-389-3@http.d.puremagic.com/issues/

---
+++ run/c/const_39_C.d
@@ -0,0 +1,19 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Bradley Smith <digitalmars-com@baysmith.com>
+// @date@	2006-09-30
+// @uri@	news:bug-389-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 389] New: Cannot link to std.path.altsep
+
+module dstress.run.c.const_39_C;
+
+const dchar[0] array;
+
+int main(){
+	if(array.length != 0){
+		assert(0);
+	}
+	return 0;
+}
