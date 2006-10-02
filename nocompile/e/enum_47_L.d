9e86f65015f57c9d91f054e8168b2adcfffeb73d
thomask <devnull@localhost>
2006-10-02 08:02:59 +0000

    [Issue 355] ICE from enum : nonexistent type
    Deewiant <deewiant@gmail.com>
    2006-09-30
    news:eflvsd$29iq$1@digitaldaemon.com

---
+++ nocompile/e/enum_47_L.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Deewiant <deewiant@gmail.com>
+// @date@	2006-09-30
+// @uri@	news:eflvsd$29iq$1@digitaldaemon.com
+// @desc@	[Issue 355] ICE from enum : nonexistent type
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.e.enum_47_L;
+
+enum E : void{
+	A,
+	B
+}
+
