807bd892a95897e02f90ffb21badb702388ca493
thomask <devnull@localhost>
2006-10-06 06:56:44 +0000

    [Issue 369] New: "immediate"-function types allowed as part of delegate/function types.
    Bruno Medeiros <brunodomedeiros+bugz@gmail.com>
    2006-09-26
    news:bug-369-3@http.d.puremagic.com/issues/

---
+++ nocompile/t/typeof_12_C.d
@@ -0,0 +1,21 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Bruno Medeiros <brunodomedeiros+bugz@gmail.com>
+// @date@	2006-09-26
+// @uri@	news:bug-369-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 369] New: "immediate"-function types allowed as part of delegate/function types.
+
+// __DSTRESS_ELINE__ 17
+
+module dstress.nocompile.t.typeof_12_C;
+
+void func() {
+}
+
+typeof(func) delegate() dg2;
+
+void test() {
+	 dg2();
+}
