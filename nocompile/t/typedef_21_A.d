888111e8786798641e5138258321bf0ff9e48727
thomask <devnull@localhost>
2006-10-06 06:57:10 +0000

    [Issue 289] New: Compiler allows (and crashes on) dynamic arrays of typedefs of "immediate"-function types
    Bruno Medeiros <brunodomedeiros+bugz@gmail.com>
    2006-09-16
    news:bug-289-3@http.d.puremagic.com/issues/

---
+++ nocompile/t/typedef_21_A.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Bruno Medeiros <brunodomedeiros+bugz@gmail.com>
+// @date@	2006-09-16
+// @uri@	news:bug-289-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 289] New: Compiler allows (and crashes on) dynamic arrays of typedefs of "immediate"-function types
+
+// __DSTRESS_ELINE__ 16
+
+module dstress.nocompile.t.typedef_21_A;
+
+typedef int ft(int);
+
+ft[] x;
+
+void test() {
+	x.length = 2;
+}
