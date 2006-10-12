39c796683cc4343c4a3db5e78ff9426b5962afe7
thomask <devnull@localhost>
2006-10-12 15:01:53 +0000

    [Issue 427] New: source files starting with a number are accepted by dmd
    Thomas Kuehne <thomas-dloop@kuehne.cn>
    2006-10-11
    news:bug-427-3@http.d.puremagic.com/issues/

---
+++ nocompile/2_source.d
@@ -0,0 +1,10 @@
+// $HeadUrl$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2006-10-11
+// @uri@	news:bug-427-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 427] New: source files starting with a number are accepted by dmd
+
+module dstress.nocompile.2_source;
