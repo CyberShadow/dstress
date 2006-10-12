44eb3931c501b028760e087c4c1bdc8d1aa7c381
thomask <devnull@localhost>
2006-10-12 15:01:48 +0000

    [Issue 422] New: -version=X and -debug=X accept identifiers containing illegal characters
    Thomas Kuehne <thomas-dloop@kuehne.cn>
    2006-10-10
    news:bug-422-3@http.d.puremagic.com/issues/

---
+++ nocompile/d/debug_13_A.d
@@ -0,0 +1,12 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kuehne <thomas-dloop@kuehne.cn>
+// @date@	2006-10-10
+// @uri@	news:bug-422-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 422] New: -version=X and -debug=X accept identifiers containing illegal characters
+
+// __DSTRESS_DFLAGS__ -debug=a.b
+
+module dstress.nocompile.d.debug_13_A;
