c63a64340fb2cc4dd631c8d5710b7acddd511e8c
thomask <devnull@localhost>
2006-10-21 13:28:10 +0000

    [Issue 419] New: Anonymous classes are not working.
    Frank Benoit <benoit@tionex.de>
    2006-10-09
    news:bug-419-3@http.d.puremagic.com/issues/

---
+++ run/t/this_13_C.d
@@ -0,0 +1,29 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Frank Benoit <benoit@tionex.de>
+// @date@	2006-10-09
+// @uri@	news:bug-419-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 419] New: Anonymous classes are not working.
+
+module dstress.run.t.this_13_C;
+
+class C{
+	void init(){
+		I i = new class() {
+			void get( char[] s ){
+				func();
+			}
+		};
+	}
+	void func( ){ }
+}
+
+int main(){
+	C c = new C();
+	c.init();
+
+	return 0;
+}
+
