8dfd2a275e12e30eadf9d3ce68723fc3ab37dd48
thomask <devnull@localhost>
2006-11-08 15:24:16 +0000

    [Issue 461] New: Constant not understood to be constant when circular module dependency exists.
    <digitalmars-com@baysmith.com>
    2006-10-26
    news:bug-461-3@http.d.puremagic.com/issues/

---
+++ compile/c/const_40_B.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<digitalmars-com@baysmith.com>
+// @date@	2006-10-26
+// @uri@	news:bug-461-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 461] New: Constant not understood to be constant when circular module dependency exists.
+
+module /*dstress.*/compile.c.const_40_B;
+
+import /*dstress.*/compile.c.const_40_A;
+
+char[A] s;
