1088060b28aaaf7fcc46a4ec165bf310b3143e59
thomask <devnull@localhost>
2006-11-25 17:53:28 +0000

    [Issue 595] New: can't append to array/arrayliteral statically
    Nazo Humei <lovesyao@hotmail.com>
    2006-11-25
    news:bug-595-3@http.d.puremagic.com/issues/

---
+++ compile/o/opCat_25_B.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Nazo Humei <lovesyao@hotmail.com>
+// @date@	2006-11-25
+// @uri@	news:bug-595-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 595] New: can't append to array/arrayliteral statically
+
+module dstress.compile.o.opCat_25_B;
+
+const char[] a = null;
+const char[] b = a ~ ['d'];
+
+static assert(b == "d");
