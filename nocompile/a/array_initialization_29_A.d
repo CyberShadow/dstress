9c5f1a5fda8a4e6f3e96d9129ede3ba2c100bd66
thomask <devnull@localhost>
2006-11-25 13:36:49 +0000

    [Issue 587] New: DMD crashes compiling char[][] initialization
    Samuel Martin Valentin <samuel@jxdesigner.com>
    2006-11-23
    news:bug-587-3@http.d.puremagic.com/issues/

---
+++ nocompile/a/array_initialization_29_A.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Samuel Martin Valentin <samuel@jxdesigner.com>
+// @date@	2006-11-23
+// @uri@	news:bug-587-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 587] New: DMD crashes compiling char[][] initialization
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.a.array_initialization_29_A;
+
+int[] x = {1, 2, 3};
