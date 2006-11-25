ad631d4c01e49d17cd148dac91649486381b5c89
thomask <devnull@localhost>
2006-11-25 13:37:24 +0000

    [Issue 532] New: Wrong name mangling for template alias params of local vars
    Don Clugston <clugdbug@yahoo.com.au>
    2006-11-16
    news:bug-532-3@http.d.puremagic.com/issues/

---
+++ compile/m/mangleof_19_B.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don Clugston <clugdbug@yahoo.com.au>
+// @date@	2006-11-16
+// @uri@	news:bug-532-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 532] New: Wrong name mangling for template alias params of local vars
+
+module dstress.compile.m.mangleof_19_B;
+
+template Apple(alias F){
+	struct Basket {
+	}
+
+	alias void function (Basket) H;
+
+	const char [] mangledname = typeof(H).mangleof;
+}
+
+template Peach(alias A){
+	const char [] root = Apple!(A).mangledname;
+	static assert(Apple!(A).mangledname == root);
+}
+
+int main(){
+	int tree;
+	const char[] s = Peach!(tree).root;
+
+	return 0;
+}
