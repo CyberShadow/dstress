fdc8b00baf86653cfbc840371d7e06ffcaaed2e9
thomask <devnull@localhost>
2006-11-29 21:19:57 +0000

    [Issue 618] New: The following program crashes dmd.exe 0.175
    Boris Kolar <boris.kolar@globera.com>
    2006-11-29
    news:<bug-618-3@http.d.puremagic.com/issues/

---
+++ run/b/bug_optimize_294_C.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Boris Kolar <boris.kolar@globera.com>
+// @date@	2006-11-29
+// @uri@	news:<bug-618-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 618] New: The following program crashes dmd.exe 0.175
+
+module dstress.run.b.bug_optimize_294_C;
+
+struct Foo(T) {
+}
+
+int main() {
+	if ((Foo!(char)).init == (Foo!(byte)).init){
+		return 1;
+	}
+	return 0;
+}
---
+++ run/b/bug_optimize_294_G.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Boris Kolar <boris.kolar@globera.com>
+// @date@	2006-11-29
+// @uri@	news:<bug-618-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 618] New: The following program crashes dmd.exe 0.175
+
+module dstress.run.b.bug_optimize_294_G;
+
+class Foo(T) {
+}
+
+int main() {
+	if ((Foo!(char)).init == (Foo!(char)).init){
+		return 1;
+	}
+	return 0;
+}
