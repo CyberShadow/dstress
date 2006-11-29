1d0d6f553c6073a83dd192b1511f5e8da37a2956
thomask <devnull@localhost>
2006-11-29 21:20:44 +0000

    [Issue 611] New: IsExpression fails when inside implemented interface
    Michael Arntzenius <daekharel@gmail.com>
    2006-11-27
    news:bug-611-3@http.d.puremagic.com/issues/

---
+++ compile/i/is_15_K.d
@@ -0,0 +1,21 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Michael Arntzenius <daekharel@gmail.com>
+// @date@	2006-11-27
+// @uri@	news:bug-611-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 611] New: IsExpression fails when inside implemented interface
+
+module dstress.compile.i.is_15_K;
+
+template I(T){
+	interface I{
+	}
+
+	static assert(is(C : .I!(C)));
+}
+
+class C : I!(C){
+}
+
