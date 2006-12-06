0596336dba5526303b609e526131518197d5b3e5
thomask <devnull@localhost>
2006-12-06 14:21:13 +0000

    [Issue 653] New: AAs are slightly broken
    Mitja Slenc <mslenc@gmail.com>
    2006-12-05
    news:bug-653-3@http.d.puremagic.com/issues/

---
+++ run/a/associative_array_20_A.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Mitja Slenc <mslenc@gmail.com>
+// @date@	2006-12-05
+// @uri@	news:bug-653-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 653] New: AAs are slightly broken
+
+module dstress.run.a.associative_array_20_A;
+
+int main() {
+        int[uint] aa;
+
+        aa[1236448822] = 0;
+        aa[2716102924] = 1;
+        aa[ 315901071] = 2;
+
+        aa.remove(1236448822);
+
+	int* x = 2716102924 in aa;
+	if(!x){
+		assert(0);
+	}
+	if(*x != 1){
+		assert(0);
+	}
+
+	return 0;
+}
+
