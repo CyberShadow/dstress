681ec79e9a334e60000cb2029c9a2a9c5232a2e2
thomask <devnull@localhost>
2006-12-07 10:51:13 +0000

    [Issue 642] New: error: mixin "static this" into where it cannot be
    Kazuhiro Inaba <kinaba@is.s.u-tokyo.ac.jp>
    2006-12-04
    news:bug-642-3@http.d.puremagic.com/issues/

---
+++ nocompile/t/this_14_E.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kazuhiro Inaba <kinaba@is.s.u-tokyo.ac.jp>
+// @date@	2006-12-04
+// @uri@	news:bug-642-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 642] New: error: mixin "static this" into where it cannot be
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.t.this_14_E;
+
+void test(){
+	static ~this(){
+	}
+}
---
+++ nocompile/t/this_14_G.d
@@ -0,0 +1,21 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kazuhiro Inaba <kinaba@is.s.u-tokyo.ac.jp>
+// @date@	2006-12-04
+// @uri@	news:bug-642-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 642] New: error: mixin "static this" into where it cannot be
+
+// __DSTRESS_ELINE__ 20
+
+module dstress.nocompile.t.this_14_G;
+
+template T(){
+	this(){
+	}
+}
+
+void test(){
+	mixin T!();
+}
