d6f51a0dcc107ef4d40bf3a7fdf5e1543d85bb35
thomask <devnull@localhost>
2006-12-10 16:50:24 +0000

    [Issue 639] New: Escaped tuple parameter ICEs dmd
    Kazuhiro Inaba <kinaba@is.s.u-tokyo.ac.jp>
    2006-12-03
    news:bug-639-3@http.d.puremagic.com/issues/

---
+++ nocompile/t/tuple_06_B.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kazuhiro Inaba <kinaba@is.s.u-tokyo.ac.jp>
+// @date@	2006-12-03
+// @uri@	news:bug-639-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 639] New: Escaped tuple parameter ICEs dmd
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.t.tuple_06_B;
+
+T[] foo(T...)(T x) {
+	return null;
+} 
+
+void bar(){
+	foo(2,3,4);
+} 
