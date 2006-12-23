09811e469c2591ef8e5e9279391c4d2ec0635928
thomask <devnull@localhost>
2006-12-23 19:47:13 +0000

    [Issue 494] New: template's variadic argument can't use as type
    Nazo Humei <lovesyao@hotmail.com>
    2006-11-12
    news:bug-494-3@http.d.puremagic.com/issues/

---
+++ compile/t/tuple_07_C.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Nazo Humei <lovesyao@hotmail.com>
+// @date@	2006-11-12
+// @uri@	news:bug-494-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 494] New: template's variadic argument can't use as type
+
+module dstress.compile.t.tuple_07_C;
+
+template test(TA...){
+	const TA[0] test = TA[0].init;
+}
+
+static assert(test!(X) == cast(X)4);
+
+typedef int X = 4;
