a8f5eee5c53cb9c548dcee75c29439020825be1c
thomask <devnull@localhost>
2006-12-23 19:47:07 +0000

    [Issue 490] New: Static struct initializer without static attribute aborts dmd with assertion
    Tom <chievo3@gmail.com>
    2006-11-09
    news:bug-490-3@http.d.puremagic.com/issues/

---
+++ nocompile/s/struct_initialization_09_A.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Tom <chievo3@gmail.com>
+// @date@	2006-11-09
+// @uri@	news:bug-490-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 490] New: Static struct initializer without static attribute aborts dmd with assertion
+
+// __DSTRESS_ELINE__ 19
+
+module dstress.nocompile.s.struct_initialization_09_A;
+
+struct S {
+	int a;
+}
+
+void foo(){
+ 	S s = {1};
+}
