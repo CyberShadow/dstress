e57ab2aca3f62e0e7804c881bd01294542a2e006
thomask <devnull@localhost>
2006-12-23 16:12:28 +0000

    Error: (dg).ptr is not an lvalue
    Kirk McDonald <kirklin.mcdonald@gmail.com>
    2006-12-17
    http://d.puremagic.com/issues/show_bug.cgi?id=696

---
+++ run/p/ptr_12_A.d
@@ -0,0 +1,39 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kirk McDonald <kirklin.mcdonald@gmail.com>
+// @date@	2006-12-17
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=696
+// @desc@	Error: (dg).ptr is not an lvalue
+
+module dstress.run.p.ptr_12_A;
+
+uint status;
+
+class A {
+	void foo(){
+		status += 4;
+	}
+}
+
+int main() {
+	A a = new A();
+	void delegate() dg;
+
+	dg.ptr = a;
+	dg.funcptr = &A.foo;
+
+	if(status != 0){
+		assert(0);
+	}
+
+	dg();
+
+	if(status != 4){
+		assert(0);
+	}
+
+	return 0;
+}
+
