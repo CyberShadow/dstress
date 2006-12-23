023f0c4393c2fb7e78d2ca848f964b4fe73abb7d
thomask <devnull@localhost>
2006-12-23 14:13:51 +0000

    fixed opAssign_21 -> opMulAssign_21

--- nocompile/o/opMulAssign_21_B.d
+++ nocompile/o/opMulAssign_21_B.d
@@ -4,6 +4,8 @@
 
 // @author@	Thomas Kühne <thomas-dloop@kuehne.cn>
 // @date@	2006-12-23
+// @uri@	news:bug-730-3@http.d.puremagic.com/issues/
+// @desc@	[Issue 730] New: broken operator handling of ifloat/idouble/ireal
 
 // __DSTRESS_ELINE__ 16
 
