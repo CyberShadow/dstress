bdf3076143943b911ae71c432a17673fcfdf023d
thomask <devnull@localhost>
2006-12-26 10:46:25 +0000

    [Issue 734] New: Multidimensional arrays use incorrect initializer
    David Friedman <dvdfrdmn@users.sf.net>
    2006-12-23
    http://d.puremagic.com/issues/show_bug.cgi?id=734

---
+++ run/a/array_initialization_30_B.d
@@ -0,0 +1,34 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	David Friedman <dvdfrdmn@users.sf.net>
+// @date@	2006-12-23
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=734
+// @desc@	[Issue 734] New: Multidimensional arrays use incorrect initializer
+
+module dstress.run.a.array_initialization_30_B;
+
+typedef int Xint = 42;
+
+int main(){
+	auto x = new Xint[3][4];
+	
+	x.length = 200;
+
+	if(x.length != 20){
+		assert(0);
+	}
+	foreach(i; x) {
+		if(i.length != 3){
+			assert(0);
+		}
+		foreach (j; i){
+			if(j != j.init){
+				assert(0);
+			}
+		}
+	}
+
+	return 0;
+}
