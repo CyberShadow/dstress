afcdb16a210f8cab6210d1ede7322bff9e40b26a
thomask <devnull@localhost>
2006-12-30 15:39:14 +0000

    [Issue 688] Implicit function template match doesn't work for classes
    Bill Baxter <wbaxter@gmail.com>
    2006-12-14
    http://d.puremagic.com/issues/show_bug.cgi?id=688

---
+++ compile/t/template_53_B.d
@@ -0,0 +1,22 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Bill Baxter <wbaxter@gmail.com>
+// @date@	2006-12-14
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=688
+// @desc@	[Issue 688] Implicit function template match doesn't work for classes
+
+module dstress.compile.t.template_53_B;
+
+class Foo(T) {
+	T data;
+}
+
+void bar(T)(Foo!(T) f){
+}
+
+void test(){
+	auto x = new Foo!(double);
+	bar!(double)(x);
+}
---
+++ compile/t/template_53_C.d
@@ -0,0 +1,22 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Bill Baxter <wbaxter@gmail.com>
+// @date@	2006-12-14
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=688
+// @desc@	[Issue 688] Implicit function template match doesn't work for classes
+
+module dstress.compile.t.template_53_C;
+
+struct Foo(T) {
+	T data;
+}
+
+void bar(T)(Foo!(T) f){
+}
+
+void test(){
+	Foo!(double) x;
+	bar!(double)(x);
+}
