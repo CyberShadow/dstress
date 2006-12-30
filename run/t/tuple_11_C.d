611595d9ec36033f9520def207cc0345fe454e1e
thomask <devnull@localhost>
2006-12-30 15:39:56 +0000

    [Issue 684] dmd should compile this
    david <davidl@126.com>
    2006-12-13
    http://d.puremagic.com/issues/show_bug.cgi?id=684

---
+++ run/t/tuple_11_C.d
@@ -0,0 +1,38 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	david <davidl@126.com>
+// @date@	2006-12-13
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=684
+// @desc@	[Issue 684] dmd should compile this
+
+module dstress.run.t.tuple_11_C;
+
+uint t_length = uint.max;
+uint m_length = uint.max;
+
+class X{
+	static template a(T...){
+		template a(M...){
+			void a(M m){
+				t_length = T.length;
+				m_length = M.length;
+			}
+		}
+	}
+}
+
+int main(){
+	auto x = new X();
+	x.a!()(1);
+
+	if(t_length != 0){
+		assert(0);
+	}
+	if(m_length != 1){
+		assert(0);
+	}
+
+	return 0;
+}
