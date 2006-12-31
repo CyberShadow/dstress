8fcd92c34059bcf8fae2fea421b233c65b45d4b7
thomask <devnull@localhost>
2006-12-31 14:28:54 +0000

    [Issue 765] ArrayBoundsError when assigning slice of pointer
    Frits van Bommel <fvbommel@wxs.nl>
    2006-12-28
    http://d.puremagic.com/issues/show_bug.cgi?id=765

---
+++ run/n/new_28_C.d
@@ -0,0 +1,22 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Frits van Bommel <fvbommel@wxs.nl>
+// @date@	2006-12-28
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=765
+// @desc@	[Issue 765] ArrayBoundsError when assigning slice of pointer
+
+module dstress.run.n.new_28_C;
+
+int main(){
+	uint* x = (new uint[](5)).ptr;
+
+	x[0 .. 0] = 1;
+
+	if(x[0] != 0){
+		assert(0);
+	}
+
+	return 0;
+}
