b04d8eb0e200e4d7b5efc579bcd2057f9e928eee
thomask <devnull@localhost>
2006-12-31 01:59:44 +0000

    [Issue 530] segfault assigning array literal to a non-array const
    Don <clugdbug@yahoo.com.au>
    2006-11-16
    http://d.puremagic.com/issues/show_bug.cgi?id=530

---
+++ nocompile/a/array_initialization_31_C.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don <clugdbug@yahoo.com.au>
+// @date@	2006-11-16
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=530
+// @desc@	[Issue 530] segfault assigning array literal to a non-array const
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.a.array_initialization_31_C;
+
+int a = [2];
