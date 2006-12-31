68565a88a3d28d105180b5999066a7a4bd1b827e
thomask <devnull@localhost>
2006-12-31 01:59:23 +0000

    [Issue 534] IPF trying to initialize an associative array
    Stewart Gordon <smjg@iname.com>
    2006-11-16
    http://d.puremagic.com/issues/show_bug.cgi?id=534

---
+++ nocompile/a/associative_array_22_A.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg@iname.com>
+// @date@	2006-11-16
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=534
+// @desc@	[Issue 534] IPF trying to initialize an associative array
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.a.associative_array_22_A;
+
+static int[int] yuiop = [ 4: 5, 10: 6, 69: 30 ];
