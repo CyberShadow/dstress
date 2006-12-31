4aa576d5e9ce78e6d51426990730a9d5b1133830
thomask <devnull@localhost>
2006-12-31 02:01:18 +0000

    [Issue 747] undefined reference to `.LDASM1'
    Thomas Kühne <thomas-dloop@kuehne.cn>
    2006-12-25
    http://d.puremagic.com/issues/show_bug.cgi?id=747

---
+++ run/a/asm_jmp_02_B.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kühne <thomas-dloop@kuehne.cn>
+// @date@	2006-12-25
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=747
+// @desc@	[Issue 747] undefined reference to `.LDASM1'
+
+module dstress.run.a.asm_jmp_02_B;
+
+version(D_InlineAsm_X86){
+	version = runTest;
+}else version(D_InlineAsm_X86_64){
+	version = runTest;
+}
+
+version(runTest){
+	int main(){
+		asm{
+			jmp label;
+		}
+
+		assert(0);
+	label:
+		return 0;
+	}
+}else{
+	pragma(msg, "DSTRESS{XFAIL}: no inline ASM support");
+	static assert(0);
+}
