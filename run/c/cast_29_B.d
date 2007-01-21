c3ddefc81a06dafd155f9df399086370e256196e
thomask <devnull@localhost>
2007-01-21 23:29:42 +0000

    added bugzilla urls

--- run/c/cast_29_B.d
+++ run/c/cast_29_B.d
@@ -5,6 +5,7 @@
 // @author@	xs0 <xs0@xs0.com>
 // @date@	2005-08-10
 // @uri@	news:dddlpj$q05$1@digitaldaemon.com
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=856
 // @desc@	foreach doesn't work when accessing elements as supertypes
 
 module dstress.run.c.cast_29_B;
