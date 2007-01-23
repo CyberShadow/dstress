8f068c3642d34420f3713612947119acb306b90e
thomask <devnull@localhost>
2007-01-23 12:03:26 +0000

    adde bugilla URL

--- run/s/switch_22_A.d
+++ run/s/switch_22_A.d
@@ -5,6 +5,7 @@
 // @author@	<BCS@pathlink.com>
 // @date@	2006-06-16
 // @uri@	news:e6umde$2ok0$2@digitaldaemon.com
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=729
 
 module dstress.run.s.switch_22_A;
 
