3256c1ccdc973218da46e5aecd15cefc4fcca8a0
thomask <devnull@localhost>
2007-01-23 13:46:14 +0000

    [Issue 873] Scope dependant compilation error
    Boris Kolar <boris.kolar@globera.com>
    2007-01-23
    http://d.puremagic.com/issues/show_bug.cgi?id=873

---
+++ run/m/mixin_26_B.d
@@ -0,0 +1,32 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Boris Kolar <boris.kolar@globera.com>
+// @date@	2007-01-23
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=873
+// @desc@	[Issue 873] Scope dependant compilation error
+
+module dstress.run.m.mixin_26_B;
+
+template Factory() {
+	Foo bar() {
+		return Foo.init;
+	}
+}
+
+int main() {
+	struct Foo {
+		mixin Factory!();
+		int i = 0x12_34_FF_AA;
+	}
+
+	Foo f;
+	if(f.bar() != Foo.init){
+		assert(0);
+	}
+
+	if(f.bar().i == 0x12_34_FF_AA){
+		return 0;
+	}
+}
