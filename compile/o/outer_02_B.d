8da1bb130c0fd318b3afb35e6f12c4bdcc4268d9
thomask <devnull@localhost>
2007-01-23 13:46:39 +0000

    [Issue 872] Assertion in expression.c caused by taking typeof of "this.outer" in nested classes.
    Tyler Knott <tknott@gmail.com>
    2007-01-22
    http://d.puremagic.com/issues/show_bug.cgi?id=872

---
+++ compile/o/outer_02_B.d
@@ -0,0 +1,19 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Tyler Knott <tknott@gmail.com>
+// @date@	2007-01-22
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=872
+// @desc@	[Issue 872] Assertion in expression.c caused by taking typeof of "this.outer" in nested classes.
+
+module dstress.compile.o.outer_02_B;
+
+class Outer{
+	class Inner{
+		void foo() {
+			typeof(this.outer) x;
+			static assert(Outer == typeof(x));
+		}
+	}
+}
---
+++ compile/o/outer_02_G.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Tyler Knott <tknott@gmail.com>
+// @date@	2007-01-22
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=872
+// @desc@	[Issue 872] Assertion in expression.c caused by taking typeof of "this.outer" in nested classes.
+
+module dstress.compile.o.outer_02_G;
+
+class Outer{
+	class Inner{
+		void foo() {
+			Outer b = this.outer;
+		}
+	}
+}
