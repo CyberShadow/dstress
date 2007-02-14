ec7ec574f7cef9ec200c4e36b5e0c3777153283b
thomask <devnull@localhost>
2007-02-14 10:11:43 +0000

    [Issue 861] Using two typedefs of an undefined name in a class in a different module with import cycles causes DMD to hang
    Matti Niemenmaa <deewiant@gmail.com>
    2007-01-20
    http://d.puremagic.com/issues/show_bug.cgi?id=861

---
+++ nocompile/i/import_18_C.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Matti Niemenmaa <deewiant@gmail.com>
+// @date@	2007-01-20
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=861
+// @desc@	[Issue 861] Using two typedefs of an undefined name in a class in a different module with import cycles causes DMD to hang
+
+module /*dstress.*/nocompile.i.import_18_C;
+
+import /*dstress.*/nocompile.i.import_18_D;
+
+typedef NONEXISTENT_NAME
+	myTypedef,
+	myTypedef2;
