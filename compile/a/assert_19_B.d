d8585736db99875a5971ca2ab2d7fc99156755f8
thomask <devnull@localhost>
2007-02-14 10:08:05 +0000

    [Issue 901] Comparison of array literals fails
    Elmar Zander <elmar@zandere.de>
    2007-01-30
    http://d.puremagic.com/issues/show_bug.cgi?id=901

---
+++ compile/a/assert_19_B.d
@@ -0,0 +1,12 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Elmar Zander <elmar@zandere.de>
+// @date@	2007-01-30
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=901
+// @desc@	[Issue 901] Comparison of array literals fails
+
+module dstress.compile.a.assert_19_B;
+
+static assert( !([2,3]==[2,4]) );
---
+++ run/a/assert_19_D.d
@@ -0,0 +1,15 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Elmar Zander <elmar@zandere.de>
+// @date@	2007-01-30
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=901
+// @desc@	[Issue 901] Comparison of array literals fails
+
+module dstress.run.a.assert_19_D;
+
+int main(){
+	assert([2,3] != [2,4]);
+	return 0;
+}
