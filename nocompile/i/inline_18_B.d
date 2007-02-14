205dd419c208616293235c18370da051cc8e0e02
thomask <devnull@localhost>
2007-02-14 10:07:40 +0000

    [Issue 908] compiler dies trying to inline static method call to nonstatic method in template code.
    Kevin Bealer <kevinbealer@gmail.com>
    2007-01-30
    http://d.puremagic.com/issues/show_bug.cgi?id=908

---
+++ nocompile/i/inline_18_B.d
@@ -0,0 +1,33 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kevin Bealer <kevinbealer@gmail.com>
+// @date@	2007-01-30
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=908
+// @desc@	[Issue 908] compiler dies trying to inline static method call to nonstatic method in template code.
+
+// __DSTRESS_ELINE__ 22
+
+module dstress.nocompile.i.inline_18_B;
+
+class C {
+	final uint next(){
+		return 1;
+	}
+}
+
+template Foo(T) {
+	void bar(){
+		int r = C.next;
+	}
+}
+
+int main(){
+	C prng = new C();
+	alias Foo!(int).bar baz;
+
+	baz();
+
+	return 0;
+}
