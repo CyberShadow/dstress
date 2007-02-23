9822feed8a5f09a51cc73f56aed29f1a55d96818
thomask <devnull@localhost>
2007-02-23 22:34:10 +0000

    [Issue 858] Forward reference to struct inside class crashes the compiler
    Georg Ramme <georg.ramme@gmail.com>
    2007-01-19
    http://d.puremagic.com/issues/show_bug.cgi?id=858

---
+++ run/f/forward_reference_17_A.d
@@ -0,0 +1,29 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Georg Ramme <georg.ramme@gmail.com>
+// @date@	2007-01-19
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=858
+// @desc@	[Issue 858] Forward reference to struct inside class crashes the compiler
+
+module dstress.run.f.forward_reference_17_A;
+
+class Outer{
+	this(){
+		Inner i = new Inner();
+	}
+
+	class Inner{
+		Strct str;
+	}
+}
+
+struct Strct{
+	int i;
+}
+
+int main(){
+	Outer o = new Outer();
+	return 0;
+}
