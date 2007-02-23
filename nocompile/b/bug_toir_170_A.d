27899e698806851fdfe68ba51f7e0dffa736eb8d
thomask <devnull@localhost>
2007-02-23 22:33:02 +0000

    [Issue 852] ICE in dmd with anon. delegate in static class ctor in unittest
    Daniel Keep <daniel.keep+d.puremagic.com@gmail.com>
    2007-01-17
    http://d.puremagic.com/issues/show_bug.cgi?id=852

---
+++ nocompile/b/bug_toir_170_A.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Daniel Keep <daniel.keep+d.puremagic.com@gmail.com>
+// @date@	2007-01-17
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=852
+// @desc@	[Issue 852] ICE in dmd with anon. delegate in static class ctor in unittest
+
+module dstress.nocompile.b.bug_toir_170_A;
+
+void test(){
+	class Foo{
+		static this(){
+			auto i = {
+				return new Foo;
+			};
+		}
+	}
+}
