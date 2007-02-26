3960ddc847fe1edd00b3a0d53edc3a2505c13c35
thomask <devnull@localhost>
2007-02-26 11:31:35 +0000

    [Issue 997] [Regression] Struct-returning function that conditionally passes the result of another function straight through doesn't work (NRVO bug?)
    Stewart Gordon <smjg@iname.com>
    2007-02-22
    http://d.puremagic.com/issues/show_bug.cgi?id=997

---
+++ run/s/struct_28_D.d
@@ -0,0 +1,34 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Stewart Gordon <smjg@iname.com>
+// @date@	2007-02-22
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=997
+// @desc@	[Issue 997] [Regression] Struct-returning function that conditionally passes the result of another function straight through doesn't work (NRVO bug?)
+
+module dstress.run.s.struct_28_D;
+
+struct Rect {
+    int left, top, right, bottom;
+}
+
+Rect foo(bool indirect) {
+	if(indirect){
+		Rect r = defaultRect();
+		return r;
+	}else{
+		return Rect.init;
+	}
+}
+
+Rect defaultRect(){
+	return Rect.init;
+}
+
+int main() {
+	if(foo(false) != foo(true)){
+		assert(0);
+	}
+	return 0;
+}
