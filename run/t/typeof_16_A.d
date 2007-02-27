b5fc7fb94fafa376a23cd6527286e75c6a468065
thomask <devnull@localhost>
2007-02-27 16:42:51 +0000

    [Issue 764] (typeof(o)).classinfo causes parser error
    Dan <ddaglas@gmail.com>
    2006-12-28
    http://d.puremagic.com/issues/show_bug.cgi?id=764

---
+++ run/t/typeof_16_A.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Dan <ddaglas@gmail.com>
+// @date@	2006-12-28
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=764
+// @desc@	[Issue 764] (typeof(o)).classinfo causes parser error
+
+module dstress.run.t.typeof_16_A;
+
+int main() {
+	Object o = new Object();
+	char[] name = (typeof(o)).classinfo.name;
+
+	if("object.Object" != name){
+		assert(0);
+	}
+	return 0;
+}
