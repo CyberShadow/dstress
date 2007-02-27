a4ca834d2e134516f1325baf2f7cf9e1fe92f757
thomask <devnull@localhost>
2007-02-27 16:42:03 +0000

    [Issue 877] forward references in interdependent template/struct definitions since v1.0
    Manuel König <manuelk89@gmx.net>
    2007-01-23
    http://d.puremagic.com/issues/show_bug.cgi?id=877

---
+++ compile/f/forward_reference_18_B.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Manuel König <manuelk89@gmx.net>
+// @date@	2007-01-23
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=877
+// @desc@	[Issue 877] forward references in interdependent template/struct definitions since v1.0
+
+module dstress.compile.f.forward_reference_18_B;
+
+struct T(FOO){
+	S s;
+}
+
+struct S{
+	T!(int)* pt;
+}
