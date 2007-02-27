0bc5edfb744089b10fbaf10695c35f785d01ed6f
thomask <devnull@localhost>
2007-02-27 16:41:48 +0000

    [Issue 853] Internal error: toir.c 182
    Johan Granberg <lijat.me@gmail.com>
    2007-01-17
    http://d.puremagic.com/issues/show_bug.cgi?id=853

---
+++ run/b/bug_toir_182_B.d
@@ -0,0 +1,38 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Johan Granberg <lijat.me@gmail.com>
+// @date@	2007-01-17
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=853
+// @desc@	[Issue 853] Internal error: toir.c 182
+
+module dstress.run.b.bug_toir_182_B;
+
+class StackContext{
+	this(void delegate() dg){
+	}
+}
+
+class Test{
+	StackContext context;
+
+	void init(){
+		context = new StackContext({});
+	}
+	
+	this(){
+		init();
+	}
+}
+
+int main(){
+	Test t = new Test();
+	if(!t){
+		assert(0);
+	}
+	if(!t.context){
+		assert(0);
+	}
+	return 0;
+}
---
+++ run/b/bug_toir_182_E.d
@@ -0,0 +1,34 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Johan Granberg <lijat.me@gmail.com>
+// @date@	2007-01-17
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=853
+// @desc@	[Issue 853] Internal error: toir.c 182
+
+module dstress.run.b.bug_toir_182_E;
+
+class StackContext{
+	this(void delegate() dg){
+	}
+}
+
+class Test{
+	StackContext context;
+
+	private this(){
+		context = new StackContext({});
+	}
+}
+
+int main(){
+	Test t = new Test();
+	if(!t){
+		assert(0);
+	}
+	if(!t.context){
+		assert(0);
+	}
+	return 0;
+}
