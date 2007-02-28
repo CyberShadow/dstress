72a8013a77e769b2a27d1f690cb0a417cc16979d
thomask <devnull@localhost>
2007-02-28 00:30:28 +0000

    added bugzilla uri

--- run/s/static_37_J.d
+++ run/s/static_37_J.d
@@ -5,6 +5,7 @@
 // @author@	Carlos Santander <csantander619@gmail.com>
 // @date@	2006-07-09
 // @uri@	news:e8sdrv$2k9c$1@digitaldaemon.com
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=660
 // @desc@	name resolution and static function
 
 module dstress.run.s.static_37_J;
