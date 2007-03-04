d07205c0336abc0ca2acf666d8c20df4bb3eca8d
thomask <devnull@localhost>
2007-03-04 13:06:49 +0000

    added bugzilla url

--- nocompile/o/opMod_01_A.d
+++ nocompile/o/opMod_01_A.d
@@ -2,7 +2,10 @@
 // $Date$
 // $Author$
 
-// __DSTRESS_ELINE__ 11
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1018
+// @desc@	[Issue 1018] regression: Error: divide by 0
+
+// __DSTRESS_ELINE__ 14
 
 module dstress.nocompile.o.opMod_01_A;
 
