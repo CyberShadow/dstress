ac5177de05455e8880e6288c186169da17a2fd4c
thomask <devnull@localhost>
2007-03-04 13:07:05 +0000

    added bugzilla uri

--- nocompile/bounds_checking_02.d
+++ nocompile/bounds_checking_02.d
@@ -2,7 +2,10 @@
 // $Date$
 // $Author$
 
-// __DSTRESS_ELINE__ 11
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1019
+// @desc@	[Issue 1019] regression: Error: array index X is out of bounds [0 .. Y]
+
+// __DSTRESS_ELINE__ 14
 
 module dstress.nocompile.bounds_checking_02;
 
--- nocompile/bounds_checking_04.d
+++ nocompile/bounds_checking_04.d
@@ -2,7 +2,10 @@
 // $Date$
 // $Author$
 
-// __DSTRESS_ELINE__ 12
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1019
+// @desc@	[Issue 1019] regression: Error: array index X is out of bounds [0 .. Y]
+
+// __DSTRESS_ELINE__ 15
 
 module dstress.nocompile.bounds_checking_04;
 
