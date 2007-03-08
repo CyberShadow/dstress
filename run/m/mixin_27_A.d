e530010a48b1381017fea3d971766c930bc15328
thomask <devnull@localhost>
2007-03-08 16:25:57 +0000

    [Issue 1025] mixin("scope(exit) call();"); is broken
    Tomas Lindquist Olsen <tomas@famolsen.dk>
    2007-03-05
    http://d.puremagic.com/issues/show_bug.cgi?id=1025

---
+++ run/m/mixin_27_A.d
@@ -0,0 +1,26 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Tomas Lindquist Olsen <tomas@famolsen.dk>
+// @date@	2007-03-05
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1025
+// @desc@	[Issue 1025] mixin("scope(exit) call();"); is broken
+
+module dstress.run.m.mixin_27_A;
+
+int status;
+
+void test(){
+	mixin("status += 1;");
+	mixin("scope(exit) status *= 2;");
+	mixin("status += 5;");
+}
+
+int main(){
+	test();
+	if(12 != status){
+		assert(0);
+	}
+	return 0;
+}
---
+++ run/m/mixin_27_E.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Tomas Lindquist Olsen <tomas@famolsen.dk>
+// @date@	2007-03-05
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1025
+// @desc@	[Issue 1025] mixin("scope(exit) call();"); is broken
+
+module dstress.run.m.mixin_27_E;
+
+int status;
+
+void test(){
+	mixin("status += 1;");
+	mixin("scope(success) status *= 2;");
+	throw new Exception("foo");
+	mixin("status += 5;");
+}
+
+int main(){
+	try{
+		test();
+	}catch{
+		if(6 != status){
+			assert(0);
+		}
+		return 0;
+	}
+	assert(0);
+}
