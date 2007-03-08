b8ef10d2f26c94871c8f20cf87d435925adc5048
thomask <devnull@localhost>
2007-03-08 16:29:23 +0000

    [Issue 980] If a function tries to concatenate a char to a empty array, dmd complains that the function can't be evaluated at compile time
    John S. Skogtvedt <teleledningsanka@gmail.com>
    2007-02-18
    http://d.puremagic.com/issues/show_bug.cgi?id=980

---
+++ compile/o/opCatAssign_22_B.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	John S. Skogtvedt <teleledningsanka@gmail.com>
+// @date@	2007-02-18
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=980
+// @desc@	[Issue 980] If a function tries to concatenate a char to a empty array, dmd complains that the function can't be evaluated at compile time
+
+module dstress.compile.o.opCatAssign_22_B;
+
+char[] test(){
+	char[] s = "a";
+	return s ~= "o";
+}
+
+const x = test();
+static assert(x == "ao");
