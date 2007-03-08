7173bd62de7855336d002325ec10b00572de4f9c
thomask <devnull@localhost>
2007-03-08 16:27:25 +0000

    [Issue 1011] illegal import declaration causes compile time segfault
    Thomas Kühne <thomas-dloop@kuehne.cn>
    2007-02-28
    http://d.puremagic.com/issues/show_bug.cgi?id=1011

---
+++ nocompile/i/import_19_B.d
@@ -0,0 +1,12 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kühne <thomas-dloop@kuehne.cn>
+// @date@	2007-02-28
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1011
+// @desc@	[Issue 1011] illegal import declaration causes compile time segfault
+
+module /*dstress.*/nocompile.i.import_19_B;
+
+import /*dstress.*/nocompile.i.import_19_A;
