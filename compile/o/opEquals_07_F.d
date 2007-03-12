571cc9954507765c02c3534951efe0f97b48afec
thomask <devnull@localhost>
2007-03-12 06:20:35 +0000

    fixed @desc@ data

--- compile/o/opEquals_07_F.d
+++ compile/o/opEquals_07_F.d
@@ -5,7 +5,7 @@
 // @author@	Reiner Pope <reiner.pope@gmail.com>
 // @date@	2007-03-03
 // @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1017
-// @desc@	[Issue 1027] CTFE doesn't support (string == string)
+// @desc@	[Issue 1017] CTFE doesn't support (string == string)
 
 module dstress.compile.o.opEquals_07_F;
 
