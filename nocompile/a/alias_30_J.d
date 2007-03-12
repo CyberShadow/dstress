7f7801f8043ce6a68e80b1eb85c47ca50e5df51a
thomask <devnull@localhost>
2007-03-12 06:21:06 +0000

    extended circular alias tests

---
+++ nocompile/a/alias_30_J.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Deewiant <deewiant.doesnotlike.spam@gmail.com>
+// @date@	2006-02-11
+// @uri@	news:dskdle$1838$1@digitaldaemon.com
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.a.alias_30_J;
+
+alias a b;
+alias b a;
+
+a x;
