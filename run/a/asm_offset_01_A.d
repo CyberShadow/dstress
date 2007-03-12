e63939d3a453c89fcf2761859c0f501246d68628
thomask <devnull@localhost>
2007-03-12 05:30:13 +0000

    fixed #1042

--- run/a/asm_offset_01_A.d
+++ run/a/asm_offset_01_A.d
@@ -6,12 +6,16 @@
 // @date@	2006-04-28
 // @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=116
 
+// __DSTRESS_TORTURE_BLOCK__ -fPIC
+
 module dstress.run.a.asm_offset_01_A;
 
 version(D_InlineAsm_X86){
 	version = runTest;
 }else version(D_InlineAsm_X86_64){
 	version = runTest;
+}else{
+	static assert(0, "DSTRESS{XFAIL}: no inline x86 asm support");
 }
 
 version(runTest){
@@ -26,6 +30,10 @@ version(runTest){
 				mov EAX, offset b;
 				mov y, EAX;
 			}
+		}else static if(size_t.sizeof == 8){
+			asm{
+				mov RAX, offset b;
+				mov y, RAX;
 		}else{
 			pragma(msg, "DSTRESS{ERROR}: unsupported pointer size");
 			static assert(0);
@@ -37,7 +45,4 @@ version(runTest){
 
 		return 0;
 	}
-}else{
-	pragma(msg, "DSTRESS{XFAIL}: no inline ASM support");
-	static assert(0);
 }
--- run/a/asm_offset_01_B.d
+++ run/a/asm_offset_01_B.d
@@ -6,29 +6,33 @@
 // @date@	2006-04-28
 // @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=116
 
-// __DSTRESS_ELINE__ 24
+// __DSTRESS_TORTURE_BLOCK__ -fPIC
 
-module dstress.nocompile.a.asm_offset_01_B;
+module dstress.run.a.asm_offset_01_B;
 
 version(D_InlineAsm_X86){
 	version = runTest;
+}else version(D_InlineAsm_X86_64){
+	version = runTest;
+}else{
+	static assert(0, "DSTRESS{XFAIL}: no inline x86 asm support");
 }
 
 version(runTest){
 	byte b;
 
 	void main(){
-		
 		static if(size_t.sizeof == 4){
 			asm{
 				mov EAX, offsetof b;
+			}
+		}else static if(size_t.sizeof == 8){
+			asm{
+				mov RAX, offsetof b;
 			}
 		}else{
 			pragma(msg, "DSTRESS{ERROR}: unsupported pointer size");
 			static assert(0);
 		}
 	}
-}else{
-	pragma(msg, "DSTRESS{XFAIL}: no inline x86 asm support");
-	static assert(0);
 }
