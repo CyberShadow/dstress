69b7139ba1e23baeb62108e3010b6f6b36905234
thomask <devnull@localhost>
2007-03-12 05:29:36 +0000

    fixed floating point comparison

--- run/a/array_initialization_26_E.d
+++ run/a/array_initialization_26_E.d
@@ -2,6 +2,11 @@
 // $Date$
 // $Author$
 
+// @author@	Thomas Kühne <thomas-dloop@kuehne.cn>
+// @date@	2006-12-25
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=746
+// @desc@	[Issue 746] incorrect code generation for "new Type[][](1, 1)"
+
 module dstress.run.a.array_initialization_26_E;
 
 int main(){
@@ -11,8 +16,8 @@ int main(){
 		assert(0);
 	}
 	if(a[0][0] !<>= 0.0){
-		assert(0);
+		return 0;
 	}
 
-	return 0;
+	assert(0);
 }
