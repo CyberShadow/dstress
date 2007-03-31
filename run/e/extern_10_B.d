75ffcff4a74436127fc535c8f264cfbe4f2fbd19
thomask <devnull@localhost>
2007-03-31 08:26:20 +0000

    r7390@birke:  tk | 2007-03-29 16:07:56 +0200  [Issue 957] linkage attributes for function level declarations are ignored
     Thomas Kühne <thomas-dloop@kuehne.cn>
     2007-02-13
     http://d.puremagic.com/issues/show_bug.cgi?id=957

---
+++ run/e/extern_10_B.d
@@ -0,0 +1,26 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kühne <thomas-dloop@kuehne.cn>
+// @date@	2007-02-13
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=957
+// @desc@	[Issue 957] linkage attributes for function level declarations are ignored
+
+module dstress.run.e.extern_10_B;
+
+extern(Pascal) long foo(int a, int b){
+	return (a + 1) * b;
+}
+
+extern(Pascal) long function(int, int) bar;
+
+int main(){
+	bar = &foo;
+
+	if(20 != bar(3, 5)){
+		assert(0);
+	}
+
+	return 0;
+}
