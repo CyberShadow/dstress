bf379f8183d043fd5cee79ee548c9453225c9185
thomask <devnull@localhost>
2007-03-31 08:25:16 +0000

    r7386@birke:  tk | 2007-03-29 15:38:42 +0200  [Issue 1055] union forward reference "overlapping initialization" error
     Bill Baxter <wbaxter@gmail.com>
     2007-03-12
     http://d.puremagic.com/issues/show_bug.cgi?id=1055

---
+++ run/f/forward_reference_20_B.d
@@ -0,0 +1,33 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Bill Baxter <wbaxter@gmail.com>
+// @date@	2007-03-12
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1055
+// @desc@	[Issue 1055] union forward reference "overlapping initialization" error
+
+module dstress.run.f.forward_reference_20_B;
+
+
+struct Z {
+	union {
+		S s;
+	}
+	int x = 0x9ABC_DEF0;
+}
+
+struct S{
+	int y = 0x1234_5678;
+}
+
+int main(){
+	Z z;
+	if(0x9ABC_DEF0 != z.x){
+		assert(0);
+	}
+	if(0x1234_5678 != z.s.y){
+		assert(0);
+	}
+	return 0;
+}
