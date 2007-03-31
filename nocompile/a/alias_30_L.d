1092c85794d4438256bd4a8a45c074f71ef43959
thomask <devnull@localhost>
2007-03-31 08:23:34 +0000

    r7383@birke:  tk | 2007-03-29 15:17:51 +0200  [Issue 1063] Assertion failure: 'global.errors' on line 444 in file 'declaration.c'
     <torhu@yahoo.com>
     2007-03-15
     http://d.puremagic.com/issues/show_bug.cgi?id=1063

---
+++ nocompile/a/alias_30_L.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<torhu@yahoo.com>
+// @date@	2007-03-15
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1063
+// @desc@	[Issue 1063] Assertion failure: 'global.errors' on line 444 in file 'declaration.c'
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.a.alias_30_L;
+
+alias a a;
