b1b31e029359e23a72d33b2df64f80eb67537dda
thomask <devnull@localhost>
2007-03-31 08:24:38 +0000

    r7384@birke:  tk | 2007-03-29 15:24:43 +0200  [Issue 1061] "asm inc [;" segfaults compiler.
     Don Clugston <clugdbug@yahoo.com.au>
     2007-03-14
     http://d.puremagic.com/issues/show_bug.cgi?id=1061

---
+++ nocompile/a/asm_02_F.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don Clugston <clugdbug@yahoo.com.au>
+// @date@	2007-03-14
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1061
+// @desc@	[Issue 1061] "asm inc [;" segfaults compiler.
+
+// __DSTRESS_ELINE__ 23
+
+module dstress.nocompile.a.asm_02_F;
+
+version(D_InlineASM_X86){
+	version = test;
+}else version(D_InlineAsm_X86_64){
+	version = test;
+}
+
+version(test){
+	void foo() {
+		asm {
+			inc };
+		}
+	}
+}else{
+	static assert(0, "DSTRESS{XFAIL}: no X86 inline ASM support");
+}
