900a4db8b966381153f3c25a4050e3595a2592f8
thomask <devnull@localhost>
2007-04-04 20:29:33 +0000

    [Issue 1089] Unsafe pointer comparison in TypeInfo_Pointer.compare
    Madeleine Freudenberg <madou@madou.org>
    2007-04-01
    http://d.puremagic.com/issues/show_bug.cgi?id=1089

---
+++ run/t/typeinfo_03_D.d
@@ -0,0 +1,29 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Madeleine Freudenberg <madou@madou.org>
+// @date@	2007-04-01
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1089
+// @desc@	[Issue 1089] Unsafe pointer comparison in TypeInfo_Pointer.compare
+
+module dstress.run.t.typeinfo_03_D;
+
+int main(){
+	auto t = typeid(int*);
+
+	int* a = new int;
+	int* b = new int;
+	*a = -10;
+	*b = 10;
+
+	if(-1 < t.compare(a, b)){
+		assert(0);
+	}
+	if(t.compare(b, a) < 1){
+		assert(0);
+	}
+
+	return 0;
+}
+
