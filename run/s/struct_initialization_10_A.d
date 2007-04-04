a7690b3f9de3bf8fd7bb8997e47817a0406b5e6e
thomask <devnull@localhost>
2007-04-04 20:33:42 +0000

    [Issue 1088] structs allocated with a struct allocator will not have default initializer values assigned
    Mark Guidarelli <guido@grumpy-cat.com>
    2007-03-31
    http://d.puremagic.com/issues/show_bug.cgi?id=1088

---
+++ run/s/struct_initialization_10_A.d
@@ -0,0 +1,27 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Mark Guidarelli <guido@grumpy-cat.com>
+// @date@	2007-03-31
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1088
+// @desc@	[Issue 1088] structs allocated with a struct allocator will not have default initializer values assigned
+
+module dstress.run.s.struct_initialization_10_A;
+
+struct S{
+	new(size_t size){
+		return (new void[size]).ptr;
+	}
+
+	int i = 0x12_AB_CD_00;
+}
+
+int main(){
+	S* s = new S();
+	if(0x12_AB_CD_00 != s.i){
+		assert(0);
+	}
+
+	return 0;
+}
