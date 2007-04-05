5f6d18f87e96048c98c8150e98bfc64ac1ed5248
thomask <devnull@localhost>
2007-04-05 16:37:14 +0000

    [Issue 933] Literal tuple parameters fails to compile when inout is mentioned
    Nick <korslund@gmail.com>
    2007-02-06
    http://d.puremagic.com/issues/show_bug.cgi?id=933

---
+++ run/t/tuple_20_B.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Nick <korslund@gmail.com>
+// @date@	2007-02-06
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=933
+// @desc@	[Issue 933] Literal tuple parameters fails to compile when inout is mentioned
+
+module dstress.run.t.tuple_20_B;
+
+int x;
+
+struct Foo(T...){
+	void set(T t){
+		x += t[0];
+	}
+}
+
+int main(){
+	Foo!(int) foo;
+	foo.set(5);
+	if(5 != x){
+		assert(0);
+	}
+
+	return 0;
+}
