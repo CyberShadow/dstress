1fa14fb5801eeb01ab11cf5321287e78de07f3aa
thomask <devnull@localhost>
2007-04-05 16:32:01 +0000

    [Issue 991] expression.c:4257: virtual Expression* DotIdExp::semantic(Scope*): Assertion `0' failed.
    Frits van Bommel <fvbommel@wxs.nl>
    2007-01-31
    http://d.puremagic.com/issues/show_bug.cgi?id=991

---
+++ compile/b/bug_expression_4420_C.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Frits van Bommel <fvbommel@wxs.nl>
+// @date@	2007-01-31
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=991
+// @desc@	[Issue 991] expression.c:4257: virtual Expression* DotIdExp::semantic(Scope*): Assertion `0' failed.
+
+module dstress.compile.b.bug_expression_4420_C;
+
+template Template(Ts...) {
+	alias Ts tuple;
+}
+
+static if(!is(typeof(Template!().tuple))){
+	static assert(0);
+}
