fb695df42da508a20233ca43d6712ebf6426acbb
thomask <devnull@localhost>
2007-04-05 10:31:32 +0000

    [Issue 884] Segfault in recursive template
    Frits van Bommel <fvbommel@wxs.nl>
    2007-01-24
    http://d.puremagic.com/issues/show_bug.cgi?id=884

---
+++ nocompile/t/templatde_58_B.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Frits van Bommel <fvbommel@wxs.nl>
+// @date@	2007-01-24
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=884
+// @desc@	[Issue 884] Segfault in recursive template
+
+module dstress.nocompile.t.template_58_B;
+
+template Inner(Ts...) {
+	const Inner = .Inner!(Ts);
+}
+
+auto x = Inner!();
