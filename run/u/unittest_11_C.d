b15e33ef5f8310f633ee371370f6629681081b06
thomask <devnull@localhost>
2007-04-05 16:35:18 +0000

    [Issue 928] nested struct definition in unittest section of a templated class, hangs DMD
    Daniel (Zuu) <dkm4i1@gmail.com>
    2007-02-03
    http://d.puremagic.com/issues/show_bug.cgi?id=928

---
+++ run/u/unittest_11_C.d
@@ -0,0 +1,23 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Daniel (Zuu) <dkm4i1@gmail.com>
+// @date@	2007-02-03
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=928
+// @desc@	[Issue 928] nested struct definition in unittest section of a templated class, hangs DMD
+
+module dstress.run.u.unittest_11_C;
+
+class MinHeap(NodeType){
+	unittest{
+		struct TestType {
+		}
+		MinHeap!(TestType) foo = new MinHeap!(TestType)();
+	}
+}
+
+int main(){
+	MinHeap!(int) foo = new MinHeap!(int)();
+	return 0;
+}
