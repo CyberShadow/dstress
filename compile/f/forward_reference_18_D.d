d20d1f134f1aac7b699d6ae2774b3a6b97c547a8
thomask <devnull@localhost>
2007-04-05 10:19:38 +0000

    [Issue 810] Cannot forward reference template
    Matti Niemenmaa <deewiant@gmail.com>
    2007-01-07
    http://d.puremagic.com/issues/show_bug.cgi?id=810

---
+++ compile/f/forward_reference_18_D.d
@@ -0,0 +1,13 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Matti Niemenmaa <deewiant@gmail.com>
+// @date@	2007-01-07
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=810
+// @desc@	[Issue 810] Cannot forward reference template
+
+module dstress.compile.f.forward_reference_18_D;
+
+template Template(T) {}
+Template!(int) stack;
