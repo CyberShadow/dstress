bf80e074c0f1106a798a6c4fb856f726c64145f6
thomask <devnull@localhost>
2007-04-05 10:19:08 +0000

    [Issue 803] incorrect error message and location for repeated aliases
    Thomas Kühne <thomas-dloop@kuehne.cn>
    2007-01-06
    http://d.puremagic.com/issues/show_bug.cgi?id=803

---
+++ nocompile/a/alias_41_A.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Thomas Kühne <thomas-dloop@kuehne.cn>
+// @date@	2007-01-06
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=803
+// @desc@	[Issue 803] incorrect error message and location for repeated aliases
+
+// __DSTRESS_ELINE__ 17
+
+module dstress.nocompile.a.alias_41_A;
+
+struct Foo{
+}
+
+alias Foo Bar;
+alias Foo Bar;
