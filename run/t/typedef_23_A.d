0b89b02a16420edbba80adc132da0edd69ca289c
thomask <devnull@localhost>
2007-04-05 10:29:26 +0000

    [Issue 850] we need (*type).property to refer to property if we use typedef
    davidl <david@126.com>
    2007-01-14
    http://d.puremagic.com/issues/show_bug.cgi?id=850

---
+++ run/t/typedef_23_A.d
@@ -0,0 +1,29 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	davidl <david@126.com>
+// @date@	2007-01-14
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=850
+// @desc@	[Issue 850] we need (*type).property to refer to property if we use typedef
+
+module dstress.run.t.typedef_23_A;
+
+class C{
+	int myMember;
+}
+
+void foo(C* x){
+	x.myMember = 0xDEAD_DEED;
+}
+
+int main(){
+	C c = new C();
+	foo(&c);
+
+	if(0xDEAD_DEED != c.myMember){
+		assert(0);
+	}
+
+	return 0;
+}
---
+++ run/t/typedef_23_D.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	davidl <david@126.com>
+// @date@	2007-01-14
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=850
+// @desc@	[Issue 850] we need (*type).property to refer to property if we use typedef
+
+module dstress.run.t.typedef_23_D;
+
+class C{
+	int myMember;
+}
+
+typedef C* X;
+
+void foo(X x){
+	(*x).myMember = 0xDEAD_DEED;
+}
+
+int main(){
+	C c = new C();
+	foo(cast(X) &c);
+
+	if(0xDEAD_DEED != c.myMember){
+		assert(0);
+	}
+
+	return 0;
+}
