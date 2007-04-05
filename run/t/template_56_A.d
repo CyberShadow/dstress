0763a5e822d89ca5a6494b06071080403bce62a6
thomask <devnull@localhost>
2007-04-05 10:18:52 +0000

    [Issue 769] Property not properly compiled - (error on valid code)
    Marcin Kuszczak <aarti@interia.pl>
    2006-12-29
    http://d.puremagic.com/issues/show_bug.cgi?id=769

---
+++ run/t/template_56_A.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Marcin Kuszczak <aarti@interia.pl>
+// @date@	2006-12-29
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=769
+// @desc@	[Issue 769] Property not properly compiled - (error on valid code)
+
+module dstress.run.t.template_56_A;
+
+class DataManager {
+	static DataManager instance() {
+		return new DataManager();
+	}
+
+	T get(T)(int row) {
+		return T.init;
+	}
+}
+
+int main(){
+	if(0xFF != DataManager.instance().get!(char)(1)){
+		assert(0);
+	}
+
+	return 0;
+}
