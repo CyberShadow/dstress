42b12cbf9704012db5fd9bab6fed4dc88e828cdf
thomask <devnull@localhost>
2007-04-06 09:14:45 +0000

    [Issue 1100] Alias parameters don't accept primitive types.
    Reiner Pope <reiner.pope@gmail.com>
    2007-04-06
    http://d.puremagic.com/issues/show_bug.cgi?id=1100

---
+++ compile/a/alias_42_A.d
@@ -0,0 +1,18 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Reiner Pope <reiner.pope@gmail.com>
+// @date@	2007-04-06
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1100
+// @desc@	[Issue 1100] Alias parameters don't accept primitive types.
+
+module dstress.compile.a.alias_42_A;
+
+template Alias(alias A){
+	alias A Alias;
+}
+
+Alias!(int) x;
+
+static assert(is(typeof(x) == int));
---
+++ compile/a/alias_42_B.d
@@ -0,0 +1,22 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Reiner Pope <reiner.pope@gmail.com>
+// @date@	2007-04-06
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1100
+// @desc@	[Issue 1100] Alias parameters don't accept primitive types.
+
+module dstress.compile.a.alias_42_B;
+
+struct S{
+	int i;
+}
+
+template Alias(alias A){
+	alias A Alias;
+}
+
+Alias!(S) x;
+
+static assert(is(typeof(x) == S));
