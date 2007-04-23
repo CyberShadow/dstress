f394d0c2caceaaa0f0cb02866576fd214cb66849
thomask <devnull@localhost>
2007-04-23 17:58:22 +0000

    [Issue 1155] GDC crash (internal compiler error: in mostly_copy_tree_r, at gimplify.c:678)
    Frits van Bommel <fvbommel@wxs.nl>
    2007-04-16
    http://d.puremagic.com/issues/show_bug.cgi?id=1155

---
+++ run/b/bug_gimplify_678_A.d
@@ -0,0 +1,21 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Frits van Bommel <fvbommel@wxs.nl>
+// @date@	2007-04-16
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1155
+// @desc@	[Issue 1155] GDC crash (internal compiler error: in mostly_copy_tree_r, at gimplify.c:678)
+
+module dstress.run.b.bug_gimplify_678_A;
+
+int main() {
+	int[] arr = new int[1];
+	arr[$-1] -= 1;
+
+	if(-1 != arr[0]){
+		assert(0);
+	}
+
+	return 0;
+}
