51bec718dc869e8051e39b430ac16368fbe2cf2b
thomask <devnull@localhost>
2007-04-23 17:56:46 +0000

    [Issue 1159] Various mixins cause "CompileExp::semantic" message, some crash DMD
     Matti Niemenmaa <deewiant@gmail.com>
    2007-04-17
    http://d.puremagic.com/issues/show_bug.cgi?id=1159

---
+++ nocompile/m/mixin_31_A.d
@@ -0,0 +1,16 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	 Matti Niemenmaa <deewiant@gmail.com>
+// @date@	2007-04-17
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1159
+// @desc@	[Issue 1159] Various mixins cause "CompileExp::semantic" message, some crash DMD
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.m.mixin_31_A;
+
+void foo(){
+	mixin(2);
+}
