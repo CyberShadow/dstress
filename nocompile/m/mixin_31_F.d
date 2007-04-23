e791a5d26167ea2575edc0b8005ed388def0ad60
thomask <devnull@localhost>
2007-04-23 17:57:32 +0000

    [Issue 1158] Missing line number: invalid mixin outside function scope
    Matti Niemenmaa <deewiant@gmail.com>
    2007-04-17
    http://d.puremagic.com/issues/show_bug.cgi?id=1158

---
+++ nocompile/m/mixin_31_F.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	 Matti Niemenmaa <deewiant@gmail.com>
+// @date@	2007-04-17
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1158
+// @desc@	[Issue 1158] Missing line number: invalid mixin outside function scope
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.m.mixin_31_F;
+
+mixin("1");
