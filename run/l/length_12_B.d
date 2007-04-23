ef317248d9d31f44dc3d2b768230a0861e629658
thomask <devnull@localhost>
2007-04-23 17:55:28 +0000

    [Issue 1172] Inline assembler: cannot access member of templated aggregate directly
    Matti Niemenmaa <deewiant@gmail.com>
    2007-04-21
    http://d.puremagic.com/issues/show_bug.cgi?id=1172

---
+++ run/l/length_12_B.d
@@ -0,0 +1,22 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Matti Niemenmaa <deewiant@gmail.com>
+// @date@	2007-04-21
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1171
+// @desc@	[Issue 1171] Cannot create a static array with the length of a const static array in function scope
+
+module dstress.run.l.length_12_B;
+
+const int[2] foo;
+
+int main() {
+	int[foo.length] bar;
+
+	if(2 != bar.length){
+		assert(0);
+	}
+
+	return 0;
+}
