9c0e6fa2e9c77102b05a60c15ef97258df78e455
thomask <devnull@localhost>
2007-04-23 17:56:20 +0000

    [Issue 1163] Can't initialize multiple variables with void.
    <aziz.kerim@gmail.com>
    2007-04-18
    http://d.puremagic.com/issues/show_bug.cgi?id=1163

---
+++ compile/v/void_07_A.d
@@ -0,0 +1,12 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<aziz.kerim@gmail.com>
+// @date@	2007-04-18
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1163
+// @desc@	[Issue 1163] Can't initialize multiple variables with void.
+
+module dstress.compile.v.void_07_A;
+
+int* p = void, c = void;
