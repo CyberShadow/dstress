0a45f2964e8fc94d7ec4bc90aedfbffa5f4f5017
thomask <devnull@localhost>
2007-04-27 17:32:31 +0000

    [Issue 1178] templates - deduction + specialization won't compile
    Leo Dahlmann <leo.dahlmann@gmail.com>
    2007-04-23
    http://d.puremagic.com/issues/show_bug.cgi?id=1178

---
+++ run/t/template_63_C.d
@@ -0,0 +1,24 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Leo Dahlmann <leo.dahlmann@gmail.com>
+// @date@	2007-04-23
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1178
+// @desc@	[Issue 1178] templates - deduction + specialization won't compile
+
+module dstress.run.t.template_63_C;
+
+template outer(O){
+	O inner(I)(float x, I i){
+		return cast(O) x;
+	}
+}
+
+int main(){
+	if(5.0L != outer!(double).inner(5.0, "hi")){
+		assert(0);
+	}
+
+	return 0;
+}
