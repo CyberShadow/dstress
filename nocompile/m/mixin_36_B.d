2fe00cb690c674893e71d8c1c454576a7af746a9
thomask <devnull@localhost>
2007-04-27 17:31:50 +0000

    [Issue 1187] Segfault with syntax error in two-level mixin.
    Don Clugston <clugdbug@yahoo.com.au>
    2007-04-24
    http://d.puremagic.com/issues/show_bug.cgi?id=1187

---
+++ nocompile/m/mixin_36_B.d
@@ -0,0 +1,14 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Don Clugston <clugdbug@yahoo.com.au>
+// @date@	2007-04-24
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1187
+// @desc@	[Issue 1187] Segfault with syntax error in two-level mixin.
+
+// __DSTRESS_ELINE__ 14
+
+module dstress.nocompile.m.mixin_36_B;
+
+mixin(`mixin`);
