a590ff8413048005ca2214c02d442e6784026141
thomask <devnull@localhost>
2007-04-27 17:35:09 +0000

    updated to DMD-1.013

---
+++ run/b/bug_expression_5981_B.d
@@ -0,0 +1,20 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Kazuhiro Inaba <kinaba@is.s.u-tokyo.ac.jp>
+// @date@	2006-12-03
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=641
+// @desc@	[Issue 641] New: Complex string operations in template argument ICEs dmd
+
+module dstress.run.b.bug_expression_5981_B;
+
+int main(){
+	auto x = ([""]~[])[0];
+
+	if(is(typeof(x) : char[])){
+		return 0;
+	}
+
+	assert(0);
+}
