1f1ad54f3b193a6a02589fa11882007cfc08ac71
thomask <devnull@localhost>
2007-04-27 17:35:45 +0000

    fixed ModuleInfo dependency

--- norun/u/unittest_10_H.d
+++ norun/u/unittest_10_H.d
@@ -7,7 +7,7 @@
 // @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=776
 // @desc@	[Issue 776] Unittest section inside a template does not alway execute.
 
-// __DSTRESS_DFLAGS__ -unittest
+// __DSTRESS_DFLAGS__ -unittest compile/u/unittest_10_C.d
 
 module /*dstress.*/norun.u.unittest_10_H;
 import /*dstress.*/compile.u.unittest_10_C;
