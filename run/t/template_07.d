37d0ec5e5079daa5f5ec339883e77281a81918f0
thomask <devnull@localhost>
2007-04-27 17:22:49 +0000

    updated to DMD-1.013

--- run/t/template_07.d
+++ run/t/template_07.d
@@ -7,10 +7,7 @@
 // @uri@	news:cpk4vq$1m5a$1@digitaldaemon.com
 // @uri@	http://www.digitalmars.com/pnews/read.php?server=news.digitalmars.com&group=digitalmars.D.bugs&artnum=2546
 
-
-// __DSTRESS_ERROR__ 18
-
-module dstress.nocompile.template_07;
+module dstress.run.t.template_07;
 
 template T(){
 	class A {
