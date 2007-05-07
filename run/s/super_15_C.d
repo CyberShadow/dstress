8dab20ccd239e72683f606c4cb1e8fd78bfaefc7
thomask <devnull@localhost>
2007-05-07 05:21:43 +0000

    [Issue 1203] Cannot create Anonclass in loop
    Frank Benoit <benoit@tionex.de>
    2007-04-30
    http://d.puremagic.com/issues/show_bug.cgi?id=1203

---
+++ run/s/super_15_C.d
@@ -0,0 +1,28 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Frank Benoit <benoit@tionex.de>
+// @date@	2007-04-30
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1203
+// @desc@	[Issue 1203] Cannot create Anonclass in loop
+
+module dstress.run.s.super_15_C;
+
+class C {
+	this(){
+	}
+}
+
+int main(char[][] args){
+	foreach(char[] arg; args){
+		auto c = new class(arg) C {
+				this(char[] arg){
+					super();
+				}
+			};
+		assert(c);
+	}
+
+	return 0;
+}
