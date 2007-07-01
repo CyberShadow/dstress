385cff4b95be9dcff1bdefd8a9a54387bb607919
thomask <devnull@localhost>
2007-07-01 13:19:50 +0000

    [Issue 1151] Final template class w/ constructor and invariant causes compile error
    <dhaffey@gmail.com>
    2007-05-31
    http://d.puremagic.com/issues/show_bug.cgi?id=1151

---
+++ run/f/final_13_D.d
@@ -0,0 +1,25 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<dhaffey@gmail.com>
+// @date@	2007-05-31
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1151
+// @desc@	[Issue 1151] Final template class w/ constructor and invariant causes compile error
+
+module dstress.run.f.final_13_D;
+
+final class Foo{
+	this(){
+	}
+
+	invariant {
+	}
+}
+
+int main(){
+	auto f = new Foo();
+	assert(f);
+
+	return 0;
+}
