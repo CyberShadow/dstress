20ec5b30961b47293a9878472df65be18cda812a
thomask <devnull@localhost>
2007-07-01 13:19:07 +0000

    [Issue 1152] Inline assembler: cannot use properties of types
    Matti Niemenmaa (Deewiant) <deewiant@gmail.com>
    2007-05-31
    http://d.puremagic.com/issues/show_bug.cgi?id=1152

---
+++ run/a/asm_05_B.d
@@ -0,0 +1,34 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Matti Niemenmaa (Deewiant) <deewiant@gmail.com>
+// @date@	2007-05-31
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1152
+// @desc@	[Issue 1152] Inline assembler: cannot use properties of types
+
+module dstress.run.a.asm_05_B;
+
+version(D_InlineAsm_X86){
+	version = runTest;
+}else version(D_InlineAsm_X86_64){
+	version = runTest;
+}
+
+version(runTest){
+	int main(){
+		int i = 1;
+
+		asm{
+			mov i, int.max; 
+		}
+
+		if(0x7FFF_FFFF != i){
+			assert(0);
+		}
+
+		return 0;
+	}
+}else{
+	static assert(0, "DSTRESS{XFAIL}: no inline x86 ASM support");
+}
