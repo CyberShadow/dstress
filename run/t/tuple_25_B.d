ee54480c45779a615104315aeb53a5072d80a789
thomask <devnull@localhost>
2007-07-01 13:17:51 +0000

    [Issue 1259] Inline build triggers an illegal error msg "Error: S() is not an lvalue"
    <onlystupidspamhere@yahoo.se>
    2007-06-06
    http://d.puremagic.com/issues/show_bug.cgi?id=1259

---
+++ run/t/tuple_25_B.d
@@ -0,0 +1,31 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<onlystupidspamhere@yahoo.se>
+// @date@	2007-06-06
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1259
+// @desc@	[Issue 1259] Inline build triggers an illegal error msg "Error: S() is not an lvalue"
+
+module dstress.run.t.tuple_25_B;
+
+struct S(T...) {
+	S opMul_r(double r) {
+		return S();
+	}
+	
+	S!(1) opDiv(R)(R r) {
+		return S!(1)();
+	}
+}
+
+int main() {
+	S!(1) a;
+	S!(0) b;
+	auto c = 1.0 * a;
+
+	if(S!(1)() != c){
+		assert(0);
+	}
+	return 0;
+}
