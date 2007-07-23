eb067e36bae81bf159ffb2df1c662f92dde2a9e8
thomask <devnull@localhost>
2007-07-23 18:45:03 +0000

    DMD-1.017: __VENDOR__ and __VERSION__

---
+++ compile/v/VENDOR_01_A.d
@@ -0,0 +1,9 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+module dstress.compile.v.VENDOR_01_A;
+
+const char[] vendor = __VENDOR__;
+
+static assert(0 < vendor.length);
---
+++ compile/v/VERSION_01_A.d
@@ -0,0 +1,9 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+module dstress.compile.v.VERSION_01_A;
+
+const int vers = __VERSION__;
+
+static assert(vers);
