efdbf9ac27ee1bf1ae0141e788b56c2debd76e9b
thomask <devnull@localhost>
2007-07-23 18:41:15 +0000

    Matti Niemenmaa <deewiant@gmail.com> 2006-11-18 http://d.puremagic.com/issues/show_bug.cgi?id=559

--- nocompile/final_01.d
+++ nocompile/final_01.d
@@ -2,7 +2,11 @@
 // $Date$
 // $Author$
 
-// __DSTRESS_ELINE__ 15
+// @author@	Matti Niemenmaa <deewiant@gmail.com>
+// @date@	2006-11-18
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=559
+
+// __DSTRESS_ELINE__ 19
 
 module dstress.nocompile.final_01;
 
