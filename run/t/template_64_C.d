dd5ac47291dcb989bc5ec80b41499c4bb7391b1f
thomask <devnull@localhost>
2007-07-23 18:40:50 +0000

    Christian Kamm <kamm@incasoftware.de> 2007-04-24 http://d.puremagic.com/issues/show_bug.cgi?id=540

---
+++ run/t/template_64_C.d
@@ -0,0 +1,27 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	Christian Kamm <kamm@incasoftware.de>
+// @date@	2007-04-24
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=540
+
+
+module dstress.run.t.template_64_C;
+
+template A(){
+	static T foo(T)(T t){
+		return t + 1;
+	}
+}
+
+struct Bar{
+	mixin A!() a;
+}
+
+int main(){
+	if(2 != Bar.a.foo(1)){
+		assert(0);
+	}
+	return 0;
+}
