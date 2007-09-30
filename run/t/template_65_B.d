20abee9cbb3e32bdc781035f6439fbbba7ebb799
thomask <devnull@localhost>
2007-09-30 09:46:24 +0000

    r8717@birke:  tk | 2007-09-30 10:45:56 +0200  [Issue 1532] Template instance cannot use class locals as template parameters
     <samukha@voliacable.com>
     2007-09-07
     http://d.puremagic.com/issues/show_bug.cgi?id=1532

---
+++ run/t/template_65_B.d
@@ -0,0 +1,30 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<samukha@voliacable.com>
+// @date@	2007-09-07
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1532
+// @desc@	[Issue 1532] Template instance cannot use class locals as template parameters
+
+module dstress.run.t.template_65_B;
+
+class C{
+	template T(alias local){
+		int T(){
+			return local;
+		}
+	}
+
+	int foo = 0x12_AB_34_EF;
+
+	alias T!(foo) bar;
+}
+
+int main(){
+	C c = new C();
+	if(0x12_AB_34_EF != c.bar()){
+		assert(0);
+	}
+	return 0;
+}
