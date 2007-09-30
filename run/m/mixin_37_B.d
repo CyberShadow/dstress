1acd1e4da94db4a2197b878897b2915a619531f9
thomask <devnull@localhost>
2007-09-30 09:44:14 +0000

    r8715@birke:  tk | 2007-09-29 23:52:45 +0200  [Issue 1534] Can't mix in a case statement.
     <aziz.kerim@gmail.com>
     2007-09-26
     http://d.puremagic.com/issues/show_bug.cgi?id=1534

---
+++ run/m/mixin_37_B.d
@@ -0,0 +1,33 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<aziz.kerim@gmail.com>
+// @date@	2007-09-26
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1534
+// @desc@	[Issue 1534] Can't mix in a case statement.
+
+module dstress.run.m.mixin_37_B;
+
+int check(int i){
+	switch(i){
+		case 0:{ return 1; }
+		case 1, 2:
+			return 2;
+		default:
+			return 3;
+	}
+}
+
+int main(){
+	if(1 != check(0)){
+		assert(0);
+	}
+	if(2 != check(1)){
+		assert(0);
+	}
+	if(3 != check(-1)){
+		assert(0);
+	}
+	return 0;
+}
