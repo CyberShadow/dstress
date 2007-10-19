8d129e2739e35f31c2d7612dadca6482d312c6f1
thomask <devnull@localhost>
2007-10-19 17:19:41 +0000

    [Issue 1586] DMD and GDC segfaults on incomplete code segment.
    <hoytak@cs.ubc.ca>
    2007-10-07
    http://d.puremagic.com/issues/show_bug.cgi?id=1586

---
+++ nocompile/t/template_66_C.d
@@ -0,0 +1,17 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<hoytak@cs.ubc.ca>
+// @date@	2007-10-07
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1586
+// @desc@	[Issue 1586] DMD and GDC segfaults on incomplete code segment.
+
+// __DSTRESS_ELINE__ 15
+
+module dstress.nocompile.t.template_66_C;
+
+void myFunc(){
+	auto func = &NotHere!().a!();
+}
+
