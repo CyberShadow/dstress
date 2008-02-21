cf350ea3cafa4211de08894ce374adcae355b7e4
thomask <devnull@localhost>
2008-02-21 15:19:32 +0000

    [Issue 1264] New: anon class in anon delegate in private member == ICE
    <default_357-line@yahoo.de>
    2007-06-13
    http://d.puremagic.com/issues/show_bug.cgi?id=1264

---
+++ run/i/interface_27_B.d
@@ -0,0 +1,37 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	<default_357-line@yahoo.de>
+// @date@	2007-06-13
+// @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=1264
+// @desc@	[Issue 1264] New: anon class in anon delegate in private member == ICE
+
+module dstress.run.i.interface_27_B;
+
+abstract class X{
+	int foo();
+}
+
+int main() {
+	int check = 0x1234ABCD;
+
+	X o = null;
+	
+	(){
+		o = new class X{
+			int foo() {
+				return 1 + check;
+			}
+		};
+	}();
+
+	if(0x1234ABCE != o.foo()){
+		assert(0);
+	}
+	if(0x1234ABCD != check){
+		assert(0);
+	}
+
+	return 0;
+}
