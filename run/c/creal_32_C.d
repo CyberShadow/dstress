a2a62c673182167afffa684ec22d3b4e44eea5bf
Christian Kamm <kamm incasoftware de>
2009-01-04 22:25:02 +0100

    Allow for tango math in creal tests.

--- run/c/creal_32_C.d
+++ run/c/creal_32_C.d
@@ -5,11 +5,14 @@
 // @author@	Don Clugston <dac@nospam.com.au>
 // @date@	2006-03-02
 // @uri@	http://www.digitalmars.com/pnews/read.php?server=news.digitalmars.com&group=digitalmars.D.bugs&artnum=6425
-// @WARNING@	direct use of phobos
 
 module dstress.run.c.creal_32_C;
 
+version(Tango) {
+import tango.math.IEEE;
+} else {
 import std.math;
+}
 
 int main(){
 	creal a = 0.0L - 0.0Li;
--- run/c/creal_34_B.d
+++ run/c/creal_34_B.d
@@ -5,11 +5,14 @@
 // @author@	Don Clugston <dac@nospam.com.au>
 // @date@	2006-03-02
 // @uri@	http://www.digitalmars.com/pnews/read.php?server=news.digitalmars.com&group=digitalmars.D.bugs&artnum=6425
-// @WARNING@	direct use of phobos
 
 module dstress.run.c.creal_34_B;
 
+version(Tango) {
+import tango.math.IEEE;
+} else {
 import std.math;
+}
 
 int main(){
 	real re = 0.0L;
