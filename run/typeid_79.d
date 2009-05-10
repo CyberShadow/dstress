3c03c967975c3631063989ffbae5212471940e5c
Christian Kamm <kamm incasoftware de>
2009-05-10 11:55:53 +0200

    Fix typo in testcase run/typeid_79

--- run/typeid_79.d
+++ run/typeid_79.d
@@ -14,13 +14,13 @@ int main(){
 	assert(!(ti is null));
 	assert(ti);
 	assert(ti.tsize==(MyUnion[]).sizeof);
-	assert(ti.toString()=="dstress.run.typeid_78.MyUnion[]");
+	assert(ti.toString()=="dstress.run.typeid_79.MyUnion[]");
 	
 	TypeInfo_Array ta = cast(TypeInfo_Array) ti;
 	assert(!(ta is null));
 	assert(ta);
 	assert(ta.tsize==(MyUnion[]).sizeof);
-	assert(ta.toString()=="dstress.run.typeid_78.MyUnion[]");
+	assert(ta.toString()=="dstress.run.typeid_79.MyUnion[]");
 
 	return 0;
 }
