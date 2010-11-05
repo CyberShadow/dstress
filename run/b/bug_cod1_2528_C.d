1dd9748074ad547da5fcd811b3763cf043cc8935
Leandro Lucarella <llucax@gmail.com>
2010-11-05 10:10:51 -0300

    Fix dstress.run.b.bug_cod1_2528_C as reported by Don Clugston
    Quoting Don's words from Ticket #8:
    
    	Fails because it returns 3 instead of 0.
    
    	The DStress test case is incorrect. The arrray literal is an int, so
    	it's 4 bytes. Casting it to void[] should give a length of 4 -- and
    	that's exactly what it does. Should be "return x.length - 4;".

--- run/b/bug_cod1_2528_C.d
+++ run/b/bug_cod1_2528_C.d
@@ -2,7 +2,7 @@
 // $Date$
 // $Author$
 
-// @author@	Matti Niemenmaa <deewiant@gmail.com>
+// @author@	Matti Niemenmaa <deewiant@gmail.com> (fixed by Don Clugston)
 // @date@	2007-02-03
 // @uri@	http://d.puremagic.com/issues/show_bug.cgi?id=925
 // @desc@	[Issue 925] ICE on casting array literal to void[]
@@ -11,5 +11,5 @@ module dstress.run.b.bug_cod1_2528_C;
 
 int main(){
 	auto x = cast(void[])[1];
-	return x.length - 1;
+	return x.length - 4;
 }
