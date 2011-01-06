9f44f31e278572cd1c3fdc48cbc7f8368e175827
Moritz Warning <moritzwarning@web.de>
2011-01-06 15:52:55 +0100

    add test

---
+++ run/class_cast.d
@@ -0,0 +1,24 @@
+// $HeadURL$
+// $Date$
+// $Author$
+
+// @author@	SiegeLoard 
+// @date@	2010-06-01
+// @uri@	http://dsource.org/projects/ldc/ticket/450
+
+interface A
+{
+	
+}
+
+class B : A
+{
+	byte[2] a;
+	real b;
+}
+
+void main()
+{
+	A a = new B();
+	B b = cast(B) a;
+}
